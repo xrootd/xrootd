025ae034b9b37c3bd2d3e268557e085a270fcff3
Andrew Hanushevsky <abh@slac.stanford.edu>
2004-06-02 22:04:19 +0000

    Fix debug options. Create objects in the right order.

--- src/XrdAcc/XrdAccConfig.cc
+++ src/XrdAcc/XrdAccConfig.cc
@@ -131,8 +131,9 @@ int XrdAccConfig::Configure(XrdOucError &Eroute, const char *cfn) {
 // Process the configuration file and authorization database
 //
    if (!(NoGo = ConfigFile(Eroute, cfn))
-   &&  !(NoGo = ConfigDB(0, Eroute))
-   &&  !(Authorization = new XrdAccAccess(&Eroute))) NoGo = 1;
+   &&  !(Authorization = new XrdAccAccess(&Eroute))
+   &&  !(NoGo = ConfigDB(0, Eroute)))
+       {delete Authorization, Authorization = 0; NoGo = 1;}
 
 // Start a refresh thread unless this was a refresh thread call
 //
