593fe695a45062b1c652d36f1974ba1ab12fe26f
Peter Elmer <Peter.Elmer@cern.ch>
2004-12-04 20:55:01 +0000

    Reset version to Unknown

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION     "20041204-1254"
+#define XrdVERSION     "Unknown"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
