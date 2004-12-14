7a782fcf721a34b1515c95cc84586a5c77f67f73
Andrew Hanushevsky <abh@slac.stanford.edu>
2004-12-14 01:17:00 +0000

    New file for fast-path async I/O. Handle appendage files in migratable space.

---
+++ src/XrdSfs/XrdSfsAio.hh
@@ -0,0 +1,63 @@
+#ifndef __SFS_AIO_H__
+#define __SFS_AIO_H__
+/******************************************************************************/
+/*                                                                            */
+/*                          X r d S f s A i o . h h                           */
+/*                                                                            */
+/* (c) 2004 by the Board of Trustees of the Leland Stanford, Jr., University  */
+/*       All Rights Reserved. See XrdInfo.cc for complete License Terms       */
+/*   Produced by Andrew Hanushevsky for Stanford University under contract    */
+/*              DE-AC03-76-SFO0515 with the Department of Energy              */
+/******************************************************************************/
+
+//        $Id$
+
+#include <signal.h>
+#include <sys/types.h>
+#ifdef _POSIX_ASYNCHRNOUS_IO
+#include <aio.h>
+#else
+struct aiocb {           // Minimal structure to avoid compiler errors
+       int    aio_fildes;
+       void  *aio_buf;
+       size_t aio_nbytes;
+       off_t  aio_offset;
+       int    aio_reqprio;
+       struct sigevent aio_sigevent;
+      };
+#endif
+
+// The XrdSfsAIO class is meant to be derived. This object provides the
+// basic interface to handle AIO control block queues not processing.
+//
+class XrdSfsAio
+{
+public:
+
+struct aiocb sfsAio;
+
+ssize_t      Result; // If >= 0 valid result; else is -errno
+
+char        *TIdent; // Trace information (optional)
+
+// Method to handle completed reads
+//
+virtual void doneRead() = 0;
+
+// Method to hand completed writes
+//
+virtual void doneWrite() = 0;
+
+// Method to recycle free object
+//
+virtual void Recycle() = 0;
+
+             XrdSfsAio()
+                      {sfsAio.aio_sigevent.sigev_value.sival_ptr = (void *)this;
+                       sfsAio.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+                       sfsAio.aio_reqprio = 0;
+                       TIdent = (char *)"";
+                      }
+virtual     ~XrdSfsAio() {}
+};
+#endif
