81e549d9a92a07000daf2432051d79cfba8b6b2a
Gerardo Ganis <gerardo.ganis@cern.ch>
2005-01-05 09:38:39 +0000

    Use #ifdef XrdDEBUG instead of #if XrdDEBUG (from F. Rademakers)

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -2,7 +2,7 @@
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
 #define XrdVERSION     "Unknown"
-#if XrdDEBUG
+#ifdef XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
 #define XrdVSTRING XrdVERSION
