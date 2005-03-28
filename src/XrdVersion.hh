7a4e03357c1d349db3b03a456be48405761dc37d
Peter Elmer <Peter.Elmer@cern.ch>
2005-03-28 16:34:50 +0000

    Update to version 20050328-0834

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,8 +1,8 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION     "Unknown"
-#if XrdDEBUG
+#define XrdVERSION     "20050328-0834"
+#ifdef XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
 #define XrdVSTRING XrdVERSION
