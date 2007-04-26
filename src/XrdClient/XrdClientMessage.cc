88db41a85925c4e8561a68b9776962bc1a2032b3
Fabrizio Furano <furano@cern.ch>
2007-04-26 14:22:50 +0000

    Usage of posix_memalign instead of malloc for big blocks

--- src/XrdClient/XrdClientMessage.cc
+++ src/XrdClient/XrdClientMessage.cc
@@ -26,187 +26,194 @@
 //__________________________________________________________________________
 XrdClientMessage::XrdClientMessage(struct ServerResponseHeader header)
 {
-   // Constructor
+  // Constructor
 
-   fStatusCode = kXrdMSC_ok;
-   memcpy((void *)&fHdr, (const void*)&header, sizeof(ServerResponseHeader));
-   fData = 0;
-   fMarshalled = false;
-   if (!CreateData()) {
-      Error("XrdClientMessage", 
-            "Error allocating " << fHdr.dlen << " bytes.");
-      fAllocated = false;
-   } else 
-      fAllocated = true;
+  fStatusCode = kXrdMSC_ok;
+  memcpy((void *)&fHdr, (const void*)&header, sizeof(ServerResponseHeader));
+  fData = 0;
+  fMarshalled = false;
+  if (!CreateData()) {
+    Error("XrdClientMessage", 
+	  "Error allocating " << fHdr.dlen << " bytes.");
+    fAllocated = false;
+  } else 
+    fAllocated = true;
 }
 
 //__________________________________________________________________________
 XrdClientMessage::XrdClientMessage()
 {
-   // Default constructor
+  // Default constructor
 
-   memset(&fHdr, 0, sizeof(fHdr));
-   fStatusCode = kXrdMSC_ok;
-   fData = 0;
-   fMarshalled = false;
-   fAllocated = false;
+  memset(&fHdr, 0, sizeof(fHdr));
+  fStatusCode = kXrdMSC_ok;
+  fData = 0;
+  fMarshalled = false;
+  fAllocated = false;
 }
 
 //__________________________________________________________________________
 XrdClientMessage::~XrdClientMessage()
 {
-   // Destructor
+  // Destructor
 
-   if (fData)
-      free(fData);
+  if (fData)
+    free(fData);
 }
 
 //__________________________________________________________________________
 void *XrdClientMessage::DonateData()
 {
-   // Unlink the owned data in order to pass them elsewhere
+  // Unlink the owned data in order to pass them elsewhere
 
-   void *res = fData;
-   fData = 0;
-   fAllocated = false;
+  void *res = fData;
+  fData = 0;
+  fAllocated = false;
   
-   return (res);
+  return (res);
 }
 
 //__________________________________________________________________________
 bool XrdClientMessage::CreateData()
 {
-   // Allocate data
+  // Allocate data
 
-   if (!fAllocated) {
-      if (fHdr.dlen) {
-         fData = malloc(fHdr.dlen+1);
-         if (!fData) {
-            Error("XrdClientMessage::CreateData","Fatal ERROR *** malloc failed."
-                  " Probable system resources exhausted.");
-            abort();
-         }
-         char *tmpPtr = (char *)fData;
+  if (!fAllocated) {
+    if (fHdr.dlen) {
 
-	 // Useful to get always 0-terminated strings
-         tmpPtr[fHdr.dlen] = '\0';
-      }
-      if (!fData)
-         return FALSE;
+      long pgsz = sysconf(_SC_PAGESIZE);
+      int memtrbl = 0;
+      if ((pgsz > 0) && (fHdr.dlen+1 > pgsz))
+	memtrbl = posix_memalign(&fData, pgsz, fHdr.dlen+1);
       else
-         return TRUE;
-   } else
+	fData = malloc(fHdr.dlen+1);
+
+      if (!fData || memtrbl) {
+	Error("XrdClientMessage::CreateData","Fatal ERROR *** malloc failed."
+	      " Probable system resources exhausted.");
+	abort();
+      }
+      char *tmpPtr = (char *)fData;
+
+      // Useful to get always 0-terminated strings
+      tmpPtr[fHdr.dlen] = '\0';
+    }
+    if (!fData)
+      return FALSE;
+    else
       return TRUE;
+  } else
+    return TRUE;
 }
 
 //__________________________________________________________________________
 void XrdClientMessage::Marshall()
 {
-   // Marshall, i.e. put in network byte order
+  // Marshall, i.e. put in network byte order
 
-   if (!fMarshalled) {
-      ServerResponseHeader2NetFmt(&fHdr);
-      fMarshalled = TRUE;
-   }
+  if (!fMarshalled) {
+    ServerResponseHeader2NetFmt(&fHdr);
+    fMarshalled = TRUE;
+  }
 }
 
 //__________________________________________________________________________
 void XrdClientMessage::Unmarshall()
 {
-   // Unmarshall, i.e. from network byte to normal order
+  // Unmarshall, i.e. from network byte to normal order
 
-   if (fMarshalled) {
-      clientUnmarshall(&fHdr);
-      fMarshalled = FALSE;
-   }
+  if (fMarshalled) {
+    clientUnmarshall(&fHdr);
+    fMarshalled = FALSE;
+  }
 }
 
 //__________________________________________________________________________
 int XrdClientMessage::ReadRaw(XrdClientPhyConnection *phy)
 {
-   // Given a physical connection, we completely build the content
-   // of the message, reading it from the socket of a phyconn
+  // Given a physical connection, we completely build the content
+  // of the message, reading it from the socket of a phyconn
 
-   int readres;
-   int readLen = sizeof(ServerResponseHeader);
-   int usedsubstreamid = 0;
+  int readres;
+  int readLen = sizeof(ServerResponseHeader);
+  int usedsubstreamid = 0;
 
-   phy->ReadLock();
+  phy->ReadLock();
 
-   Info(XrdClientDebug::kDUMPDEBUG,
-	"XrdClientMessage::ReadRaw",
-	"Reading header (" << readLen << " bytes).");
+  Info(XrdClientDebug::kDUMPDEBUG,
+       "XrdClientMessage::ReadRaw",
+       "Reading header (" << readLen << " bytes).");
   
-   // Read a header from any substream and report it
-   readres = phy->ReadRaw((void *)&fHdr, readLen, -1, &usedsubstreamid);
-   phy->PauseSelectOnSubstream(usedsubstreamid);
+  // Read a header from any substream and report it
+  readres = phy->ReadRaw((void *)&fHdr, readLen, -1, &usedsubstreamid);
+  phy->PauseSelectOnSubstream(usedsubstreamid);
 
-   phy->ReadUnLock();
+  phy->ReadUnLock();
 
-   if (readres < 0) {
+  if (readres < 0) {
+
+    if (readres == TXSOCK_ERR_TIMEOUT)
+      SetStatusCode(kXrdMSC_timeout);
+    else {
+      Info(XrdClientDebug::kNODEBUG,"XrdClientMessage::ReadRaw",
+	   "Failed to read header (" << readLen << " bytes). Retrying ...");
+      SetStatusCode(kXrdMSC_readerr);
+
+    }
+    memset(&fHdr, 0, sizeof(fHdr));
+  }
+
+  // the data arrive marshalled from the server (i.e. network byte order)
+  SetMarshalled(TRUE);
+  Unmarshall();
+
+  Info(XrdClientDebug::kDUMPDEBUG,
+       "XrdClientMessage::ReadRaw"," sid: "<<HeaderSID() <<
+       ", IsAttn: " << IsAttn() <<
+       ", substreamid: " << usedsubstreamid);
+
+  if (fHdr.dlen) {
+
+    Info(XrdClientDebug::kDUMPDEBUG,
+	 "XrdClientMessage::ReadRaw",
+	 "Reading data (" << fHdr.dlen << " bytes) from substream " << usedsubstreamid);
+
+    CreateData();
+    if (phy->ReadRaw(fData, fHdr.dlen, usedsubstreamid) < 0) {
+
+      Info(XrdClientDebug::kNODEBUG,"XrdClientMessage::ReadRaw",
+	   "Failed to read data (" << fHdr.dlen << " bytes) from substream " << usedsubstreamid<< ". Retrying ...");
+
+      free( DonateData() );
 
       if (readres == TXSOCK_ERR_TIMEOUT)
-         SetStatusCode(kXrdMSC_timeout);
-      else {
-         Info(XrdClientDebug::kNODEBUG,"XrdClientMessage::ReadRaw",
-	       "Failed to read header (" << readLen << " bytes). Retrying ...");
-         SetStatusCode(kXrdMSC_readerr);
+	SetStatusCode(kXrdMSC_timeout);
+      else
+	SetStatusCode(kXrdMSC_readerr);
 
-      }
       memset(&fHdr, 0, sizeof(fHdr));
-   }
-
-   // the data arrive marshalled from the server (i.e. network byte order)
-   SetMarshalled(TRUE);
-   Unmarshall();
-
-   Info(XrdClientDebug::kDUMPDEBUG,
-        "XrdClientMessage::ReadRaw"," sid: "<<HeaderSID() <<
-	", IsAttn: " << IsAttn() <<
-	", substreamid: " << usedsubstreamid);
-
-   if (fHdr.dlen) {
-
-      Info(XrdClientDebug::kDUMPDEBUG,
-	   "XrdClientMessage::ReadRaw",
-	   "Reading data (" << fHdr.dlen << " bytes) from substream " << usedsubstreamid);
-
-      CreateData();
-      if (phy->ReadRaw(fData, fHdr.dlen, usedsubstreamid) < 0) {
-
-         Info(XrdClientDebug::kNODEBUG,"XrdClientMessage::ReadRaw",
-	      "Failed to read data (" << fHdr.dlen << " bytes) from substream " << usedsubstreamid<< ". Retrying ...");
-
-         free( DonateData() );
-
-	 if (readres == TXSOCK_ERR_TIMEOUT)
-	     SetStatusCode(kXrdMSC_timeout);
-         else
-	     SetStatusCode(kXrdMSC_readerr);
-
-         memset(&fHdr, 0, sizeof(fHdr));
-      }
-   }
-   phy->RestartSelectOnSubstream(usedsubstreamid);
-   return 1;
+    }
+  }
+  phy->RestartSelectOnSubstream(usedsubstreamid);
+  return 1;
 }
 
 //___________________________________________________________________________
 void XrdClientMessage::Int2CharStreamid(kXR_char *charstreamid, short intstreamid)
 {
-   // Converts a streamid given as an integer to its representation
-   // suitable for the streamid inside the messages (i.e. ascii)
+  // Converts a streamid given as an integer to its representation
+  // suitable for the streamid inside the messages (i.e. ascii)
 
-   memcpy(charstreamid, &intstreamid, sizeof(intstreamid));
+  memcpy(charstreamid, &intstreamid, sizeof(intstreamid));
 }
 
 //___________________________________________________________________________
 kXR_unt16 XrdClientMessage::CharStreamid2Int(kXR_char *charstreamid)
 {
-   // Converts a streamid given as an integer to its representation
-   // suitable for the streamid inside the messages (i.e. ascii)
+  // Converts a streamid given as an integer to its representation
+  // suitable for the streamid inside the messages (i.e. ascii)
 
-   kXR_unt16 res = *((short *)charstreamid);
+  kXR_unt16 res = *((short *)charstreamid);
 
-   return res;
+  return res;
 }
