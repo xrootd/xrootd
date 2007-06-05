07b5895ba9660d2159613148cac877f31d26c3b7
Derek Feichtinger <derek.feichtinger@cern.ch>
2007-06-05 10:10:08 +0000

    - replacement macro for posix_memalign on MAC OS (darwin)

--- src/XrdSys/XrdSysPlatform.hh
+++ src/XrdSys/XrdSysPlatform.hh
@@ -104,6 +104,8 @@ extern char *cuserid(char *s);
 #endif
 #define O_LARGEFILE 0
 #define memalign(pgsz,amt) valloc(amt)
+#define posix_memalign(memp, algn, sz) \
+        ((*memp = memalign(algn, sz)) ? 0 : ENOMEM)
 #define SHMDT_t void *
 #ifndef EDEADLOCK
 #define EDEADLOCK EDEADLK
