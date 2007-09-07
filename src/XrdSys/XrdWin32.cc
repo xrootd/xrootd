3b500dcff8793088fd1b5a97557feffd8857c6d0
Gerardo Ganis <gerardo.ganis@cern.ch>
2007-09-07 08:55:09 +0000

    From B. Bellenot: fix a problem in the implementation of posix_memalign

--- src/XrdSys/XrdWin32.cc
+++ src/XrdSys/XrdWin32.cc
@@ -437,24 +437,14 @@ char *cuserid(char * s)
       return strcpy(s, name ? name : "");
    return name;
 } 
-  
-  
+
 int posix_memalign(void **memptr, size_t alignment, size_t size)
 {
-  void *mem;
-
-  /* Test whether the SIZE argument is valid.  It must be a power of
-     two multiple of sizeof (void *).  */
-  if (size % sizeof (void *) != 0 || (size & (size - 1)) != 0)
-    return EINVAL;
-
-  mem = memalign(alignment, size);
-
-  if (mem != NULL) {
-    *memptr = mem;
-    return 0;
-  }
-
-  return ENOMEM;
+   void *mem = malloc(size);
+   if (mem != NULL) {
+      *memptr = mem;
+      return 0;
+   }
+   return ENOMEM;
 }
 
