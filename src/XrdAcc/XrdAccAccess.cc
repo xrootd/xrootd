9469591128bfa3caf8c6d2597c7cb20698d1e388
Andrew Hanushevsky <abh@slac.stanford.edu>
2008-03-26 00:02:46 +0000

    Prvent SEGV if no group privileges defined but a group is present.

--- src/XrdAcc/XrdAccAccess.cc
+++ src/XrdAcc/XrdAccAccess.cc
@@ -116,21 +116,21 @@ XrdAccPrivs XrdAccAccess::Access(const XrdSecEntity    *Entity,
 // credentials, in which case we need not have a username, or from the
 // standard unix-username group mapping.
 //
-   if (Entity->grps)
-      {char gBuff[1024];
-       XrdOucTokenizer gList(gBuff);
-       strlcpy(gBuff, Entity->grps, sizeof(gBuff));
-       gList.GetLine();
-       while((gname = gList.GetToken()))
-            if ((cp = Atab.G_Hash->Find((const char *)gname)))
-               cp->Privs(caps, path, plen, phash);
-      } else if (isuser && Atab.G_Hash
-             && (glp = XrdAccConfiguration.GroupMaster.Groups(id)))
-                {while((gname = (char *)glp->Next()))
-                      if ((cp = Atab.G_Hash->Find((const char *)gname)))
-                         cp->Privs(caps, path, plen, phash);
-                 delete glp;
-                }
+   if (Atab.G_Hash)
+      if (Entity->grps)
+         {char gBuff[1024];
+          XrdOucTokenizer gList(gBuff);
+          strlcpy(gBuff, Entity->grps, sizeof(gBuff));
+          gList.GetLine();
+          while((gname = gList.GetToken()))
+               if ((cp = Atab.G_Hash->Find((const char *)gname)))
+                  cp->Privs(caps, path, plen, phash);
+         } else if (isuser && (glp=XrdAccConfiguration.GroupMaster.Groups(id)))
+                   {while((gname = (char *)glp->Next()))
+                         if ((cp = Atab.G_Hash->Find((const char *)gname)))
+                            cp->Privs(caps, path, plen, phash);
+                    delete glp;
+                   }
 
 // Now add in the netgroup privileges
 //
