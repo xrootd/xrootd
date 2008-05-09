f93b0b3b8e96088558094a8f7bd195d3650c0a7a
Wilko Kroeger <wilko@slac.stanford.edu>
2008-05-09 22:17:31 +0000

    Release version to be used with root for GLAST

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION     "Unknown"
+#define XrdVERSION  "20080508"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
