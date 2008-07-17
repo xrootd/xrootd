cc681fa7285b124f49bc9284a4519ed027294ce1
Wilko Kroeger <wilko@slac.stanford.edu>
2008-07-17 22:14:00 +0000

    revert to unknown version

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION  "20080626-1805p1"
+#define XrdVERSION  "Unknown"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
