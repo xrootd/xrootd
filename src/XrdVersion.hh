10abf3b0cee176bf7412c91db6829b0e0f5e5fb6
Wilko Kroeger <wilko@slac.stanford.edu>
2008-07-25 22:02:46 +0000

    Change version for tag v20080722-1249

--- src/XrdVersion.hh
+++ src/XrdVersion.hh
@@ -1,7 +1,7 @@
 // $Id$
 #ifndef __XRD_VERSION_H__
 #define __XRD_VERSION_H__
-#define XrdVERSION  "Unknown"
+#define XrdVERSION  "20080722-1249"
 #if XrdDEBUG
 #define XrdVSTRING XrdVERSION "_dbg"
 #else
