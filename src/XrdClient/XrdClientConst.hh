122d39b7171081454ece1e40e359ad5cfcdec359
Fabrizio Furano <furano@cern.ch>
2009-10-27 12:59:47 +0000

    Use more friendly values for various timeouts. Add the concept of max allowed time for a transaction.

--- src/XrdClient/XrdClientConst.hh
+++ src/XrdClient/XrdClientConst.hh
@@ -21,36 +21,31 @@
 #define DFLT_CONNECTTIMEOUT     120
 #define NAME_CONNECTTIMEOUT     (char *)"ConnectTimeout"
 
-#define DFLT_CONNECTTIMEOUTWAN  120
-#define NAME_CONNECTTIMEOUTWAN  (char *)"ConnectTimeoutWan"
-
 #define DFLT_REQUESTTIMEOUT     300
 #define NAME_REQUESTTIMEOUT     (char *)"RequestTimeout"
 
-
-#define DFLT_MAXREDIRECTCOUNT   255
+#define DFLT_MAXREDIRECTCOUNT   16
 #define NAME_MAXREDIRECTCOUNT   (char *)"MaxRedirectcount"
 
 #define DFLT_DEBUG              0
 #define NAME_DEBUG              (char *)"DebugLevel"
 
-#define DFLT_RECONNECTTIMEOUT   20
-#define NAME_RECONNECTTIMEOUT   (char *)"ReconnectTimeout"
+#define DFLT_RECONNECTWAIT      5
+#define NAME_RECONNECTWAIT      (char *)"ReconnectWait"
 
-#define DFLT_REDIRCNTTIMEOUT	3600
+#define DFLT_REDIRCNTTIMEOUT	36000
 #define NAME_REDIRCNTTIMEOUT    (char *)"RedirCntTimeout"
 
-#define DFLT_FIRSTCONNECTMAXCNT 150
+#define DFLT_FIRSTCONNECTMAXCNT 8
 #define NAME_FIRSTCONNECTMAXCNT (char *)"FirstConnectMaxCnt"
+
+#define DFLT_TRANSACTIONTIMEOUT 300
+#define NAME_TRANSACTIONTIMEOUT (char *)"TransactionTimeout"
+
 
 #define TXSOCK_ERR_TIMEOUT	-1
 #define TXSOCK_ERR		-2
 #define TXSOCK_ERR_INTERRUPT	-3
-
-// Maybe we don't want to start the garbage collector
-// But the default must be to start it
-#define DFLT_STARTGARBAGECOLLECTORTHREAD  1
-#define NAME_STARTGARBAGECOLLECTORTHREAD  (char *)"StartGarbageCollectorThread"
 
 // the default number of parallel streams PER physical connection
 // 0 means that the multistream support is disabled
