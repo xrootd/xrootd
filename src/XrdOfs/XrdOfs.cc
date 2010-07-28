f8d2fac7395d82d3c2c8e2d6302e8976e938c64d
Andrew Hanushevsky <abh@slac.stanford.edu>
2010-07-28 02:07:18 +0000

    Final phase of removing odc/olb references as they are no longer supported.

--- src/XrdOfs/XrdOfs.cc
+++ src/XrdOfs/XrdOfs.cc
@@ -1856,7 +1856,7 @@ int XrdOfs::Emsg(const char    *pfx,    // Message prefix value
                  const char    *op,     // Operation being performed
                  const char    *target) // The target (e.g., fname)
 {
-   char *etext, buffer[XrdOucEI::Max_Error_Len], unkbuff[64];
+   char *etext, buffer[MAXPATHLEN+80], unkbuff[64];
 
 // If the error is EBUSY then we just need to stall the client. This is
 // a hack in order to provide for proxy support
