be712e5a9ac2ed360fab4d776ab94b0fcbe44eb1
Gerri Ganis <gerardo.ganis@cern.ch>
2010-12-02 19:14:49 +0100

    From B. Bockelman:   Add possibility to load the plugin with the RTLD_GLOBAL option. The default behavior   remains unchanged.

--- src/XrdSys/XrdSysPlugin.cc
+++ src/XrdSys/XrdSysPlugin.cc
@@ -52,14 +52,22 @@ XrdSysPlugin::~XrdSysPlugin()
 /******************************************************************************/
 /*                             g e t P l u g i n                              */
 /******************************************************************************/
-  
+
+
 void *XrdSysPlugin::getPlugin(const char *pname, int errok)
+{
+   return getPlugin(pname, errok, false);
+}
+
+void *XrdSysPlugin::getPlugin(const char *pname, int errok, bool global)
 {
    void *ep;
 
 // Open the plugin library if not already opened
 //
-   if (!libHandle && !(libHandle = dlopen(libPath, RTLD_NOW)))
+   int flags = RTLD_NOW;
+   flags |= global ? RTLD_GLOBAL : RTLD_LOCAL;
+   if (!libHandle && !(libHandle = dlopen(libPath, flags)))
       {eDest->Emsg("getPlugin", "Unable to open", libPath, dlerror());
        return 0;
       }
