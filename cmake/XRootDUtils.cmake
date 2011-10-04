3a047b6b0eac0ddf0e7d54037f84a023aa8acc66
Lukasz Janyst <ljanyst@cern.ch>
2011-10-04 16:34:37 +0200

    [CMake] Disable in-source builds - they fail on MacOS because the filesystem is case insensitive

--- cmake/XRootDUtils.cmake
+++ cmake/XRootDUtils.cmake
@@ -23,3 +23,23 @@ macro( component_status name flag found )
     set( STATUS_${name} "disabled" )
   endif()
 endmacro()
+
+#-------------------------------------------------------------------------------
+# Detect in source builds
+#-------------------------------------------------------------------------------
+function( CheckBuildDirectory )
+
+  # Get Real Paths of the source and binary directories
+  get_filename_component( srcdir "${CMAKE_SOURCE_DIR}" REALPATH )
+  get_filename_component( bindir "${CMAKE_BINARY_DIR}" REALPATH )
+
+  # Check for in-source builds
+  if( ${srcdir} STREQUAL ${bindir} )
+    message( FATAL_ERROR "XRootD cannot be built in-source! "
+                         "Please run cmake <src-dir> outside the "
+                         "source directory and be sure to remove "
+                         "CMakeCache.txt or CMakeFiles if they "
+                         "exist in the source directory." )
+  endif()
+
+endfunction()
