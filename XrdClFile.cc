fd4e97a8d8e3322f91b9e8397a457e22a97ae2ee
Lukasz Janyst <ljanyst@cern.ch>
2011-11-11 18:27:11 +0100

    [XrdCl] Update license header

--- XrdClFile.cc
+++ XrdClFile.cc
@@ -1,5 +1,7 @@
 //------------------------------------------------------------------------------
+// Copyright (c) 2011 by European Organization for Nuclear Research (CERN)
 // Author: Lukasz Janyst <ljanyst@cern.ch>
+// See the LICENCE file for details.
 //------------------------------------------------------------------------------
 
 #include "XrdCl/XrdClLog.hh"
