164077f998cd7a6fde96d8339b2289f318b4dfe2
Lukasz Janyst <ljanyst@cern.ch>
2012-01-31 17:55:39 +0100

    [XrdCl] Make Utils::splitString templated in order to put the result in any container

--- XrdClUtils.cc
+++ XrdClUtils.cc
@@ -8,30 +8,4 @@
 
 namespace XrdClient
 {
-  //----------------------------------------------------------------------------
-  // Split string
-  //----------------------------------------------------------------------------
-  void Utils::splitString( std::vector<std::string> &result,
-                           const std::string  &input,
-                           const std::string  &delimiter )
-  {
-    size_t start  = 0;
-    size_t end    = 0;
-    size_t length = 0;
-
-    do
-    {
-      end = input.find( delimiter, start );
-
-      if( end != std::string::npos )
-        length = end - start;
-      else
-        length = std::string::npos;
-
-      result.push_back( input.substr( start, length ) );
-
-      start = end + delimiter.size();
-    }
-    while( end != std::string::npos );
-  }
 }
--- XrdClUtils.hh
+++ XrdClUtils.hh
@@ -21,9 +21,30 @@ namespace XrdClient
       //------------------------------------------------------------------------
       //! Split a string
       //------------------------------------------------------------------------
-      static void splitString( std::vector<std::string> &result,
-                               const std::string        &input,
-                               const std::string        &delimiter );
+      template<class Container>
+      static void splitString( Container         &result,
+                               const std::string &input,
+                               const std::string &delimiter )
+      {
+        size_t start  = 0;
+        size_t end    = 0;
+        size_t length = 0;
+
+        do
+        {
+          end = input.find( delimiter, start );
+
+          if( end != std::string::npos )
+            length = end - start;
+          else
+            length = std::string::npos;
+
+          result.push_back( input.substr( start, length ) );
+
+          start = end + delimiter.size();
+        }
+        while( end != std::string::npos );
+      }
   };
 
   //----------------------------------------------------------------------------
