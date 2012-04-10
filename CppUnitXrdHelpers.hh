4c85f4c162bd11299c09da92f27d675df39cb56f
Lukasz Janyst <ljanyst@cern.ch>
2012-04-10 17:38:46 +0200

    [XrdClTests] Add testing helpers for pthreads and errno

--- CppUnitXrdHelpers.hh
+++ CppUnitXrdHelpers.hh
@@ -7,12 +7,31 @@
 #ifndef __CPPUNIT_XRD_HELPERS_HH__
 #define __CPPUNIT_XRD_HELPERS_HH__
 
+#include <XrdCl/XrdClXRootDResponses.hh>
+#include <errno.h>
+#include <string.h>
+
 #define CPPUNIT_ASSERT_XRDST( x )                    \
 {                                                    \
-  XRootDStatus st = x;                               \
+  XrdClient::XRootDStatus st = x;                    \
   std::string msg = "["; msg += #x; msg += "]: ";    \
   msg += st.ToStr();                                 \
   CPPUNIT_ASSERT_MESSAGE( msg, st.IsOK() );   \
+}
+
+#define CPPUNIT_ASSERT_ERRNO( x )                    \
+{                                                    \
+  std::string msg = "["; msg += #x; msg += "]: ";    \
+  msg += strerror( errno );                          \
+  CPPUNIT_ASSERT_MESSAGE( msg, x );                  \
+}
+
+#define CPPUNIT_ASSERT_PTHREAD( x )                  \
+{                                                    \
+  errno = x;                                         \
+  std::string msg = "["; msg += #x; msg += "]: ";    \
+  msg += strerror( errno );                          \
+  CPPUNIT_ASSERT_MESSAGE( msg, errno == 0 );         \
 }
 
 #endif // __CPPUNIT_XRD_HELPERS_HH__
