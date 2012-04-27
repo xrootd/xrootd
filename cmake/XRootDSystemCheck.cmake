4e342d7fa5c753e94c9ebb7c4281df3117cc37cd
Lukasz Janyst <ljanyst@cern.ch>
2012-04-27 17:15:19 +0200

    Disable sendfile on Mac - it gets detected and compiled correctly but doesn't work

--- cmake/XRootDSystemCheck.cmake
+++ cmake/XRootDSystemCheck.cmake
@@ -63,15 +63,17 @@ endif()
 #-------------------------------------------------------------------------------
 # Sendfile
 #-------------------------------------------------------------------------------
-check_function_exists( sendfile HAVE_SENDFILE )
-compiler_define_if_found( HAVE_SENDFILE HAVE_SENDFILE )
-set( SENDFILE_LIBRARY "" )
-if( NOT HAVE_SENDFILE )
-  check_library_exists( sendfile sendfile "" HAVE_SENDFILE_IN_SENDFILE )
-  compiler_define_if_found( HAVE_SENDFILE_IN_SENDFILE HAVE_SENDFILE )
+if( NOT MacOSX )
+  check_function_exists( sendfile HAVE_SENDFILE )
+  compiler_define_if_found( HAVE_SENDFILE HAVE_SENDFILE )
+  set( SENDFILE_LIBRARY "" )
+  if( NOT HAVE_SENDFILE )
+    check_library_exists( sendfile sendfile "" HAVE_SENDFILE_IN_SENDFILE )
+    compiler_define_if_found( HAVE_SENDFILE_IN_SENDFILE HAVE_SENDFILE )
 
-  if( HAVE_SENDFILE_IN_SENDFILE )
-    set( SENDFILE_LIBRARY "sendfile" )
+    if( HAVE_SENDFILE_IN_SENDFILE )
+      set( SENDFILE_LIBRARY "sendfile" )
+    endif()
   endif()
 endif()
 
