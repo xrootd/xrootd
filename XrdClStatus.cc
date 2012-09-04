d926c17c6b88b12bbb20f9b9a8e8ca5c3741fe32
Lukasz Janyst <ljanyst@cern.ch>
2012-09-04 10:22:14 +0200

    [XrdCl] Change 'This program' to 'XRootD' in the license header

--- XrdClStatus.cc
+++ XrdClStatus.cc
@@ -2,18 +2,18 @@
 // Copyright (c) 2011-2012 by European Organization for Nuclear Research (CERN)
 // Author: Lukasz Janyst <ljanyst@cern.ch>
 //------------------------------------------------------------------------------
-// This program is free software: you can redistribute it and/or modify
+// XRootD is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// XRootD is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
 #include "XrdCl/XrdClStatus.hh"
