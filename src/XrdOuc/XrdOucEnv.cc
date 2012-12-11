1401d29289f421a72a9a4505550bb17317af5f2b
Andrew Hanushevsky <abh@stanford.edu>
2012-12-11 07:39:40 -0800

    Avoid lost memory and correct computation of env length. This also fixes but 99375.

--- src/XrdOuc/XrdOucEnv.cc
+++ src/XrdOuc/XrdOucEnv.cc
@@ -46,18 +46,18 @@ XrdOucEnv::XrdOucEnv(const char *vardata, int varlen,
 
    if (!vardata) {global_env = 0; global_len = 0; return;}
 
-// Allocate storage for the global information (don't rely on its being correct)
+// Get the length of the global information (don't rely on its being correct)
 //
    if (!varlen) varlen = strlen(vardata);
-   global_env = (char *)malloc(varlen+2); global_len = varlen;
 
 // We want our env copy to start with a single ampersand
 //
    while(*vardata == '&' && varlen) {vardata++; varlen--;}
    if (!varlen) {global_env = 0; global_len = 0; return;}
+   global_env = (char *)malloc(varlen+2);
    *global_env = '&'; vdp = global_env+1;
    memcpy((void *)vdp, (const void *)vardata, (size_t)varlen);
-   *(vdp+varlen) = '\0';
+   *(vdp+varlen) = '\0'; global_len = varlen+1;
 
 // scan through the string looking for '&'
 //
