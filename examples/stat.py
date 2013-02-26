d62b2b9a894fae86e68ac0f0ee51599ffbee7600
Justin Salmon <jsalmon@cern.ch>
2013-02-26 10:08:07 +0100

    Add async blocking example

--- examples/stat.py
+++ examples/stat.py
@@ -2,27 +2,56 @@ from XRootD import client
 
 myclient = client.Client("root://localhost")
 print 'URL:', myclient.url
-
+ 
 #-------------------------------------------------------------------------------
 # Synchronous example
 #-------------------------------------------------------------------------------
 status, response = myclient.stat("/tmp")
 print "Status:", status
 print "Modification time:", response.GetModTimeAsString()
-
+ 
 #-------------------------------------------------------------------------------
-# Asynchronous example
+# Asynchronous non-waiting example
 #-------------------------------------------------------------------------------
 def callback(status, response, hostList):
   print 'Status:', status
   print 'Response:', response
   print 'Modification time:', response.GetModTimeAsString()
-  
+   
   for host in hostList:
     print "Host:", host.url
-
+ 
 myclient.stat("/tmp", callback)
-
+ 
 # Halt script (todo: implement callback class w/semaphore and/or callback 
 # decorator w/generator)
-raw_input()
+#x = raw_input()
+
+#-------------------------------------------------------------------------------
+# Asynchronous waiting example
+#-------------------------------------------------------------------------------
+from threading import BoundedSemaphore, Thread
+
+class AsyncStatHandler(Thread):
+  def __init__(self, client, path):
+    self.client = client
+    self.path   = path
+    self.sem    = BoundedSemaphore(1)
+    Thread.__init__(self)
+    
+  def run(self):
+    self.sem.acquire()
+    status, response = self.client.stat(self.path)
+    print 'Status:', status
+    print 'Response:', response
+    print 'Modification time:', response.GetModTimeAsString()
+    self.sem.release()
+    
+  def startAndWait(self):
+    handler.start()
+    handler.join()
+
+myclient = client.Client('root://localhost')
+handler = AsyncStatHandler(myclient, '/tmp')
+handler.startAndWait()
+
