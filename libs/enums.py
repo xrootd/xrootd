62f30035390487b91264a8d23c6d5d90b5102e2b
Justin Lewis Salmon <jsalmon@cern.ch>
2013-03-18 00:00:27 +0000

    Add remaining enums

--- libs/enums.py
+++ libs/enums.py
@@ -4,7 +4,20 @@ def enum(**enums):
   reverse = dict((value, key) for key, value in enums.iteritems())
   enums['reverse_mapping'] = reverse
   return type('Enum', (), enums)
-  
+
+QueryCode = enum(
+  STATS          = 1,
+  PREPARE        = 2,
+  CHECKSUM       = 3,
+  XATTR          = 4,
+  SPACE          = 5,
+  CHECKSUMCANCEL = 6,
+  CONFIG         = 7,
+  VISA           = 8,
+  OPAQUE         = 16,
+  OPAQUEFILE     = 32
+)
+
 OpenFlags = enum(
 #  COMPRESS  = 1,
    DELETE    = 2,
@@ -21,4 +34,42 @@ OpenFlags = enum(
    POSC      = 4096,
    NOWAIT    = 8192,
    SEQIO     = 16384
+)
+
+AccessMode = enum(
+  UR = 0x100,
+  UW = 0x080, 
+  UX = 0x040,
+  GR = 0x020,
+  GW = 0x010,
+  GX = 0x008,
+  OR = 0x004,
+  OW = 0x002,
+  OX = 0x001
+)
+
+MkDirFlags = enum(
+  NONE     = 0,
+  MAKEPATH = 1
+)
+
+DirListFlags = enum(
+  NONE   = 0,
+  STAT   = 1,
+  LOCALE = 2
+)
+
+StatFlags = enum(
+  OBJECT = 0,
+  VFS    = 1
+)
+
+PrepareFlags = enum(
+# CANCEL    = 1,
+# NOTIFY    = 2,
+# NOERRS    = 4,
+  STAGE     = 8,
+  WRITEMODE = 16,
+  COLOCATE  = 32,
+  FRESH     = 64
 )
