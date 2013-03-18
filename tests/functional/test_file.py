0b29734d22acf9575abc45bee8657e8303b953f1
Justin Salmon <jsalmon@cern.ch>
2013-03-18 10:48:43 +0100

    Update tests

--- tests/functional/test_file.py
+++ tests/functional/test_file.py
@@ -4,30 +4,24 @@ from XRootD.enums import OpenFlags
 
 import pytest
 
-def test_file():
+def test_write():
     with client.File() as f:
-        assert not f.is_open()
-        status, response = f.open('root://localhost//tmp/spam', OpenFlags.READ)
+        buffer = 'eggs and ham'
+
+        with pytest.raises(ValueError):
+            f.write(buffer)
+
+        status, response = f.open('root://localhost//tmp/spam', OpenFlags.DELETE)
         assert status['isOK']
-        assert f.is_open()
 
-        with pytest.raises(TypeError):
-            f.enable_read_recovery(enable="foo")
-            
-        f.enable_read_recovery(enable=True)
+        status, response = f.write(buffer)
+        assert status['isOK']
 
-        with pytest.raises(TypeError):
-            f.enable_write_recovery(enable="foo")
-            
-        f.enable_write_recovery(enable=True)
+        status, response = f.read()
+        assert status['isOK']
+        assert len(response) == len(buffer)
 
-        assert f.get_data_server()
-
-        with pytest.raises(TypeError):
-            f.get_data_server("invalid_arg")
-
-    # testing context manager
-    assert not f.is_open()
+        # Test write offsets and sizes
 
 def test_read():
     with client.File() as f:
@@ -51,40 +45,21 @@ def test_vector_read():
 
         status, response = f.open('root://localhost//tmp/spam', OpenFlags.READ)
         assert status['isOK']
-        
+
         status, response = f.vector_read()
         assert status['isOK']
 
         assert response
-    
-def test_write():
-    with client.File() as f:
-        buffer = 'eggs and ham'
-
-        with pytest.raises(ValueError):
-            f.write(buffer)
-
-        status, response = f.open('root://localhost//tmp/spam', OpenFlags.DELETE)
-        assert status['isOK']
-
-        status, response = f.write(buffer)
-        assert status['isOK']
-
-        status, response = f.read()
-        assert status['isOK']
-        assert len(response) == len(buffer)
-
-        # Test write offsets and sizes
 
 def test_stat():
     with client.File() as f:
-        
+
         with pytest.raises(ValueError):
             f.stat()
 
         status, response = f.open('root://localhost//tmp/spam')
         assert status['isOK']
-        
+
         # status, response = f.stat()
         assert 0  # TODO: Fix stat() with force=False
         assert status['isOK']
@@ -95,7 +70,7 @@ def test_sync():
         
         with pytest.raises(ValueError):
             f.sync()
-            
+
         status, response = f.open('root://localhost//tmp/spam')
         assert status['isOK']
         
@@ -107,10 +82,35 @@ def test_truncate():
         
         with pytest.raises(ValueError):
             f.truncate()
-            
+
         status, response = f.open('root://localhost//tmp/spam')
         assert status['isOK']
-        
+
         status, response = f.truncate(size=1000) # TODO: Fix EINVAL
         print status
         assert status['isOK']
+
+def test_misc():
+    with client.File() as f:
+        assert not f.is_open()
+        status, response = f.open('root://localhost//tmp/spam', OpenFlags.READ)
+        assert status['isOK']
+        assert f.is_open()
+
+        with pytest.raises(TypeError):
+            f.enable_read_recovery(enable="foo")
+
+        f.enable_read_recovery(enable=True)
+
+        with pytest.raises(TypeError):
+            f.enable_write_recovery(enable="foo")
+
+        f.enable_write_recovery(enable=True)
+
+        assert f.get_data_server()
+
+        with pytest.raises(TypeError):
+            f.get_data_server("invalid_arg")
+
+    # testing context manager
+    assert not f.is_open()
--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -1,35 +1,41 @@
 from XRootD import client
 from XRootD.handlers import AsyncResponseHandler
-from XRootD.enums import OpenFlags, QueryCode, MkDirFlags, AccessMode
+from XRootD.enums    import OpenFlags, QueryCode, MkDirFlags, AccessMode, \
+                            DirListFlags, PrepareFlags
 import pytest
 
 def test_filesystem():
     c = client.Client("root://localhost")
-    
+
     funcspecs = [(c.locate,     ('/tmp', OpenFlags.REFRESH), True),
                  (c.deeplocate, ('/tmp', OpenFlags.REFRESH), True),
-                #(c.mv,         ('/tmp/spam', '/tmp/ham'), False),
                  (c.query,      (QueryCode.SPACE, '/tmp'), True),
                  (c.truncate,   ('/tmp/spam', 1000), False),
-                #(c.rm,         ('/tmp/ham'), False),
-                #(c.mkdir,      ('/tmp/1/2', MkDirFlags.MAKEPATH), False),
+                 (c.mv,         ('/tmp/spam', '/tmp/ham'), False),
+                 (c.chmod,      ('/tmp/ham', AccessMode.UR | AccessMode.UW), False),
+                 (c.rm,         ('/tmp/ham',), False),
+                 (c.mkdir,      ('/tmp/somedir', MkDirFlags.MAKEPATH), False),
                  (c.rmdir,      ('/tmp/somedir',), False),
-                 (c.chmod,      ('/tmp/eggs', AccessMode.UR), False),
                  (c.ping,       (), False),
                  (c.stat,       ('/tmp',), True),
                  (c.statvfs,    ('/tmp',), True),
                  (c.protocol,   (), True),
-                 (c.dirlist,    ('/tmp',), True),
-                 (c.sendinfo,   (), False),
-                 (c.prepare,    (), True),
+                 (c.dirlist,    ('/tmp', DirListFlags.STAT), True),
+                 (c.sendinfo,   ('important info',), False),
+                 (c.prepare,    (['/tmp/foo'], PrepareFlags.STAGE), True),
                  ]
 
     for func, args, hasReturnObject in funcspecs:
         sync (func, args, hasReturnObject)
+
+    # Create new temp file
+    with client.File() as f:
+        status, response = f.open('root://localhost//tmp/spam', OpenFlags.NEW)
+
+    for func, args, hasReturnObject in funcspecs:
         async(func, args, hasReturnObject)
 
 def sync(func, args, hasReturnObject):
-    c = client.Client("root://localhost")
     status, response = func(*args)
     print status
     assert status['isOK']
@@ -38,7 +44,6 @@ def sync(func, args, hasReturnObject):
         assert response
     
 def async(func, args, hasReturnObject):
-    c = client.Client("root://localhost")
     handler = AsyncResponseHandler()
     status = func(*args, callback=handler)
     print status
@@ -68,3 +73,4 @@ def test_args():
         
     with pytest.raises(TypeError):
         c.locate(path="/tmp", flags=1, foo=0)
+
