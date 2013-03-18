68767c89126f5ce3d3a0ab4b8022e69c217889f2
Justin Lewis Salmon <jsalmon@cern.ch>
2013-03-18 00:00:46 +0000

    Update tests

--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -1,6 +1,6 @@
 from XRootD import client
 from XRootD.handlers import AsyncResponseHandler
-from XRootD.enums import OpenFlags
+from XRootD.enums import OpenFlags, QueryCode, MkDirFlags, AccessMode
 import pytest
 
 def test_filesystem():
@@ -8,9 +8,20 @@ def test_filesystem():
     
     funcspecs = [(c.locate,     ('/tmp', OpenFlags.REFRESH), True),
                  (c.deeplocate, ('/tmp', OpenFlags.REFRESH), True),
-                 (c.mv,         ('/tmp/spam', '/tmp/ham'), False),
+                #(c.mv,         ('/tmp/spam', '/tmp/ham'), False),
+                 (c.query,      (QueryCode.SPACE, '/tmp'), True),
+                 (c.truncate,   ('/tmp/spam', 1000), False),
+                #(c.rm,         ('/tmp/ham'), False),
+                #(c.mkdir,      ('/tmp/1/2', MkDirFlags.MAKEPATH), False),
+                 (c.rmdir,      ('/tmp/somedir',), False),
+                 (c.chmod,      ('/tmp/eggs', AccessMode.UR), False),
                  (c.ping,       (), False),
-                 (c.stat,       ('/tmp'), True)
+                 (c.stat,       ('/tmp',), True),
+                 (c.statvfs,    ('/tmp',), True),
+                 (c.protocol,   (), True),
+                 (c.dirlist,    ('/tmp',), True),
+                 (c.sendinfo,   (), False),
+                 (c.prepare,    (), True),
                  ]
 
     for func, args, hasReturnObject in funcspecs:
@@ -20,21 +31,23 @@ def test_filesystem():
 def sync(func, args, hasReturnObject):
     c = client.Client("root://localhost")
     status, response = func(*args)
-    assert status['isOK']
     print status
+    assert status['isOK']
     if hasReturnObject:
+        print response
         assert response
     
 def async(func, args, hasReturnObject):
     c = client.Client("root://localhost")
     handler = AsyncResponseHandler()
     status = func(*args, callback=handler)
-    assert status['isOK']
     print status
+    assert status['isOK']
     status, response, hostList = handler.waitFor()
     #assert 0
     assert status['isOK']
     if hasReturnObject:
+        print response
         assert response
     #assert hostList
     
