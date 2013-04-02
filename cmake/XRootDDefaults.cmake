51c304172c801f0e3aebea2f8db9989c5ba2cbab
Lukasz Janyst <ljanyst@cern.ch>
2013-04-02 17:43:11 +0200

    [CMake] Remove perl bindings

--- cmake/XRootDDefaults.cmake
+++ cmake/XRootDDefaults.cmake
@@ -1,19 +1,12 @@
 #-------------------------------------------------------------------------------
 # Define the default build parameters
 #-------------------------------------------------------------------------------
-
 if( "${CMAKE_BUILD_TYPE}" STREQUAL "" )
   if( Solaris AND NOT SUNCC_CAN_DO_OPTS )
     set( CMAKE_BUILD_TYPE Debug )
   else()
     set( CMAKE_BUILD_TYPE RelWithDebInfo )
   endif()
-endif()
-
-if( Solaris )
-  define_default( ENABLE_PERL     FALSE )
-else()
-  define_default( ENABLE_PERL     TRUE )
 endif()
 
 define_default( ENABLE_FUSE     TRUE )
--- src/XrdClient.cmake
+++ src/XrdClient.cmake
@@ -114,67 +114,6 @@ target_link_libraries(
   pthread )
 
 #-------------------------------------------------------------------------------
-# Perl bindings
-#-------------------------------------------------------------------------------
-if( BUILD_PERL )
-  include_directories( ${PERL_INCLUDE_PATH} )
-
-  #-----------------------------------------------------------------------------
-  # Check if we have the right version of SWIG
-  #-----------------------------------------------------------------------------
-  set( USE_SWIG FALSE )
-  if( SWIG_FOUND )
-    if( ${SWIG_VERSION} VERSION_GREATER "1.3.33" )
-      set( USE_SWIG TRUE )
-    endif()
-  endif()
-
-  #-----------------------------------------------------------------------------
-  # We have SWIG
-  #-----------------------------------------------------------------------------
-  if( USE_SWIG )
-    add_custom_command(
-      OUTPUT XrdClientAdmin_c_wrap.cc XrdClientAdmin.pm
-      COMMAND
-      ${SWIG_EXECUTABLE} -c++ -perl -o XrdClientAdmin_c_wrap.cc
-      ${CMAKE_SOURCE_DIR}/src/XrdClient/XrdClientAdmin_c.hh
-      MAIN_DEPENDENCY XrdClient/XrdClientAdmin_c.hh )
-
-  #-----------------------------------------------------------------------------
-  # No SWIG
-  #-----------------------------------------------------------------------------
-  else()
-    add_custom_command(
-      OUTPUT XrdClientAdmin_c_wrap.cc XrdClientAdmin.pm
-      COMMAND
-      cp ${CMAKE_SOURCE_DIR}/src/XrdClient/XrdClientAdmin_c_wrap.c
-      XrdClientAdmin_c_wrap.cc
-      COMMAND
-      cp ${CMAKE_SOURCE_DIR}/src/XrdClient/XrdClientAdmin.pm . )
-  endif()
-
-  add_library(
-    XrdClientAdmin
-    MODULE
-    XrdClientAdmin_c_wrap.cc
-    XrdClient/XrdClientAdmin_c.cc XrdClient/XrdClientAdmin_c.hh )
-
-  target_link_libraries(
-    XrdClientAdmin
-    XrdClient
-    XrdUtils
-    pthread
-    ${PERL_LIBRARY} )
-
-  set_target_properties(
-    XrdClientAdmin
-    PROPERTIES
-    PREFIX ""
-    LINK_INTERFACE_LIBRARIES "" )
-
-endif()
-
-#-------------------------------------------------------------------------------
 # Install
 #-------------------------------------------------------------------------------
 install(
@@ -190,27 +129,9 @@ install(
   ${PROJECT_SOURCE_DIR}/docs/man/xrdstagetool.1
   DESTINATION ${CMAKE_INSTALL_MANDIR}/man1 )
 
-#-------------------------------------------------------------------------------
-# Install the perl bindings
-#-------------------------------------------------------------------------------
-if( BUILD_PERL )
-  install(
-    TARGETS XrdClientAdmin
-    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
-
-  install(
-    FILES
-    ${PROJECT_BINARY_DIR}/src/XrdClientAdmin.pm
-    DESTINATION ${CMAKE_INSTALL_LIBDIR} )
-endif()
-
 # FIXME: files
 #-rw-r--r-- 1 ljanyst ljanyst  1643 2011-03-21 16:13 TestXrdClient.cc
 #-rw-r--r-- 1 ljanyst ljanyst 20576 2011-03-21 16:13 TestXrdClient_read.cc
-#-rwxr-xr-x 1 ljanyst ljanyst  1848 2011-03-21 16:13 tinytestXTNetAdmin.pl
 #-rwxr-xr-x 1 ljanyst ljanyst 15314 2011-03-21 16:13 xrdadmin
 #-rw-r--r-- 1 ljanyst ljanyst  1516 2011-03-21 16:13 XrdClientAbsMonIntf.hh
-#-rw-r--r-- 1 ljanyst ljanyst 18822 2011-03-21 16:13 XrdClientAdminJNI.cc
-#-rw-r--r-- 1 ljanyst ljanyst  3239 2011-03-21 16:13 XrdClientAdminJNI.h
-#-rw-r--r-- 1 ljanyst ljanyst  1636 2011-03-21 16:13 XrdClientAdminJNI.java
 #-rw-r--r-- 1 ljanyst ljanyst  1026 2011-03-21 16:13 XrdClientCallback.hh
