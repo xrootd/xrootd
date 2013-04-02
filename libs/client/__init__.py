c7d97100d8cae1c77444e6561696f21d6c53bb92
Justin Salmon <jsalmon@cern.ch>
2013-04-02 11:44:27 +0200

    Add CopyProcess to __init__.py

---
+++ libs/client/__init__.py
@@ -0,0 +1,6 @@
+from client import Client as Client
+from file import File as File
+from url import URL as URL
+from handlers import AsyncResponseHandler as AsyncResponseHandler
+from handlers import CallbackWrapper as CallbackWrapper
+from copyprocess import CopyProcess as CopyProcess
