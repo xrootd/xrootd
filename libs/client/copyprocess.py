6e3d12b6e772390aa1a21d27b11c3f32e0fcec2c
Justin Salmon <jsalmon@cern.ch>
2013-04-03 11:28:19 +0200

    Remove debug prints

--- libs/client/copyprocess.py
+++ libs/client/copyprocess.py
@@ -4,20 +4,16 @@ class CopyProcess(object):
   """TODO: write me"""
   
   def __init__(self):
-    print '+++++ new copyprocess'
     self.__process = client.CopyProcess()
     
   def add_job(self, source='', target=''):
     """Add a job"""
     self.__process.add_job(source, target)
-    print '+++++ job added'
     
   def prepare(self):
     """Prepare the jobs"""
     self.__process.prepare()
-    print '+++++ prepped'
     
   def run(self):
     """Run the jobs"""
-    print '+++++ running'
     self.__process.run()
