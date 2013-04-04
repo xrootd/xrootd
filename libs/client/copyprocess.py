b176c296af55b7d2a9592a1978b8bdd07a47b559
Justin Salmon <jsalmon@cern.ch>
2013-04-04 13:30:11 +0200

    Add copy job parameters and copy process return objects

--- libs/client/copyprocess.py
+++ libs/client/copyprocess.py
@@ -1,19 +1,31 @@
 from pyxrootd import client
+from XRootD.responses import XRootDStatus
 
 class CopyProcess(object):
   """TODO: write me"""
-  
+
   def __init__(self):
     self.__process = client.CopyProcess()
+
+  def add_job(self, source, target, sourcelimit=1, force=False, 
+              posc=False, coerce=False, thirdparty=False, checksumprint=False, 
+              chunksize=4194304, parallelchunks=8):
+    """Add a job
     
-  def add_job(self, source='', target=''):
-    """Add a job"""
-    self.__process.add_job(source, target)
-    
+    :param source: 
+    :type  source:
+    :param target:
+    :type  target:
+    """
+    self.__process.add_job(source, target, sourcelimit, force, posc, coerce, 
+                           thirdparty, checksumprint, chunksize, parallelchunks)
+
   def prepare(self):
     """Prepare the jobs"""
-    self.__process.prepare()
-    
+    status = self.__process.prepare()
+    return XRootDStatus(status)
+
   def run(self):
     """Run the jobs"""
-    self.__process.run()
+    status = self.__process.run()
+    return XRootDStatus(status)
