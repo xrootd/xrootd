80a701c5a7f3c4be831131ccc7b1d1aab42e8623
Andrew Hanushevsky <abh@stanford.edu>
2013-04-12 18:51:07 -0700

    Add new scaled number convertor for future use.

--- src/XrdOuc/XrdOuca2x.hh
+++ src/XrdOuc/XrdOuca2x.hh
@@ -41,12 +41,16 @@ static int a2i( XrdSysError &, const char *emsg, const char *item, int *val, int
 static int a2ll(XrdSysError &, const char *emsg, const char *item, long long *val, long long minv=-1, long long maxv=-1);
 static int a2fm(XrdSysError &, const char *emsg, const char *item, int *val, int minv);
 static int a2fm(XrdSysError &, const char *emsg, const char *item, int *val, int minv, int maxv);
+static int a2sn(XrdSysError &Eroute, const char *emsg, const char *item,
+                int *val, int nScale, int minv=-1, int maxv=-1);
 static int a2sp(XrdSysError &, const char *emsg, const char *item, long long *val, long long minv=-1, long long maxv=-1);
 static int a2sz(XrdSysError &, const char *emsg, const char *item, long long *val, long long minv=-1, long long maxv=-1);
 static int a2tm(XrdSysError &, const char *emsg, const char *item, int *val, int minv=-1, int maxv=-1);
 static int a2vp(XrdSysError &, const char *emsg, const char *item, int *val, int minv=-1, int maxv=-1);
 
 private:
+static int Emsg(XrdSysError &Eroute, const char *etxt1, const char *item,
+                                     const char *etxt2, double    val);
 static int Emsg(XrdSysError &Eroute, const char *etxt1, const char *item,
                                      const char *etxt2, int       val);
 static int Emsg(XrdSysError &Eroute, const char *etxt1, const char *item,
