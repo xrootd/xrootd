7671dfbc260592d41233cc5a6fa49c9c85322bf1
Justin Salmon <jsalmon@cern.ch>
2013-05-02 14:52:09 +0200

    Add CopyProgressHandler example

--- examples/copyprocess.py
+++ examples/copyprocess.py
@@ -1,8 +1,44 @@
 """
-Add a number of copy jobs and run them in parallel
---------------------------------------------------
+Add a number of copy jobs and run them in parallel with a progress handler
+--------------------------------------------------------------------------
+
+Produces output similar to the following::
+
+  id: 1, total: 4
+  source: /tmp/spam
+  target: /tmp/spam1
+  processed: 20, total: 20
+  end status: [SUCCESS] 
+  id: 2, total: 4
+  source: /tmp/spam
+  target: root://localhost//tmp/spam2
+  processed: 20, total: 20
+  end status: [SUCCESS] 
+  id: 3, total: 4
+  source: root://localhost//tmp/spam
+  target: /tmp/spam3
+  processed: 20, total: 20
+  end status: [SUCCESS] 
+  id: 4, total: 4
+  source: root://localhost//tmp/spam
+  target: root://localhost//tmp/spam4
+  processed: 20, total: 20
+  end status: [SUCCESS] 
+
 """
 from XRootD import client
+
+class MyCopyProgressHandler(client.utils.CopyProgressHandler):
+  def begin(self, id, total, source, target):
+    print 'id: %d, total: %d' % (id, total)
+    print 'source: %s' % source
+    print 'target: %s' % target
+
+  def end(self, status):
+    print 'end status:', status
+
+  def update(self, processed, total):
+    print 'processed: %d, total: %d' % (processed, total)
 
 process = client.CopyProcess()
 
@@ -15,5 +51,6 @@ process.add_job( 'root://localhost//tmp/spam', '/tmp/spam3' )
 # From remote to remote
 process.add_job( 'root://localhost//tmp/spam', 'root://localhost//tmp/spam4' )
 
+handler = MyCopyProgressHandler()
 process.prepare()
-process.run()
+process.run(handler)
