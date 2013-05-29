f7fd6cfaec7bb99486e42fcaf2a8731d5d18d267
Andreas Peters <andreas.joachim.peters@cern.ch>
2013-05-29 11:32:03 +0200

    Initialize the 'myFile' variable in the Reset function when a protocol object get's recycled

--- src/XrdXrootd/XrdXrootdProtocol.cc
+++ src/XrdXrootd/XrdXrootdProtocol.cc
@@ -677,6 +677,7 @@ void XrdXrootdProtocol::Reset()
    myIOLen            = 0;
    myStalls           = 0;
    myAioReq           = 0;
+   myFile             = 0;
    numReads           = 0;
    numReadP           = 0;
    numWrites          = 0;
