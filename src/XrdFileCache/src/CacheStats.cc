5ef5be1969ad1f762442c7cf6548418beacf34be
alja <amraktadel@ucsd.edu>
2014-01-02 11:14:43 -0800

    Remove files added by accident.

--- src/XrdFileCache/src/CacheStats.cc
+++
@@ -1,9 +0,0 @@
-#include "CacheStats.hh"
-#include "Context.hh"
-
-using namespace XrdFileCache;
-
-void CacheStats::Dump() const
-{
-   printf("CacheStats::Dump() bCachedPrefetch = %lld, bPrefetch = %lld\n", BytesCachedPrefetch, BytesPrefetch);
-}
