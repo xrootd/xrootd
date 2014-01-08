f7d3d118b25c5afb4af31ca59b3c687cfbd38d68
alja <amraktadel@ucsd.edu>
2014-01-08 13:26:45 -0800

    in Finalize() get stat info before attaching cache.

--- src/XrdPosix/XrdPosixFile.cc
+++ src/XrdPosix/XrdPosixFile.cc
@@ -115,20 +115,23 @@ bool XrdPosixFile::Close(XrdCl::XRootDStatus &Status)
 
 bool XrdPosixFile::Finalize(XrdCl::XRootDStatus &Status)
 {
-// Setup the cache if it is to be used
-//
-   if (cOpt & XrdOucCache::optRW)
-      {    if (CacheW) XCio = CacheW->Attach((XrdOucCacheIO *)this, cOpt);}
-      else if (CacheR) XCio = CacheR->Attach((XrdOucCacheIO *)this, cOpt);
+    // Complete initialization. If the stat() fails, the caller will unwind the
+    // whole open process (ick).
 
-// Indicate that we are at the start of the file
-//
-   currOffset = 0;
+    if (Stat(Status)) {
+        // Setup the cache if it is to be used
+        //
+        if (cOpt & XrdOucCache::optRW)
+        {    if (CacheW) XCio = CacheW->Attach((XrdOucCacheIO *)this, cOpt);}
+        else if (CacheR) XCio = CacheR->Attach((XrdOucCacheIO *)this, cOpt);
 
-// Complete initialization. If the stat() fails, the caller will unwind the
-// whole open process (ick).
-//
-   return Stat(Status);
+        // Indicate that we are at the start of the file
+        //
+        currOffset = 0;
+        return true;
+    }
+    else
+        return false;
 }
   
 /******************************************************************************/
