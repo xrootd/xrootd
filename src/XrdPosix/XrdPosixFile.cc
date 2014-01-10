23b28148457c86a990d49d776831b171b7f1a1fe
alja <amraktadel@ucsd.edu>
2014-01-10 14:51:30 -0800

    Keep same logic as in master branch, except attach cache after Stat is successful.

--- src/XrdPosix/XrdPosixFile.cc
+++ src/XrdPosix/XrdPosixFile.cc
@@ -115,23 +115,24 @@ bool XrdPosixFile::Close(XrdCl::XRootDStatus &Status)
 
 bool XrdPosixFile::Finalize(XrdCl::XRootDStatus &Status)
 {
-   // Complete initialization. If the stat() fails, the caller will unwind the
-   // whole open process (ick).
+// Indicate that we are at the start of the file
+//
+   currOffset = 0;
 
-   if (Stat(Status)) {
-      // Setup the cache if it is to be used
-      //
-      if (cOpt & XrdOucCache::optRW)
-      {    if (CacheW) XCio = CacheW->Attach((XrdOucCacheIO *)this, cOpt);}
-      else if (CacheR) XCio = CacheR->Attach((XrdOucCacheIO *)this, cOpt);
+// Complete initialization. If the stat() fails, the caller will unwind the
+// whole open process (ick).
 
-      // Indicate that we are at the start of the file
-      //
-      currOffset = 0;
-      return true;
-   }
-   else
+   if (!Stat(Status))
       return false;
+
+// Setup the cache if it is to be used
+//
+   if (cOpt & XrdOucCache::optRW)
+   {    if (CacheW) XCio = CacheW->Attach((XrdOucCacheIO *)this, cOpt);}
+   else if (CacheR) XCio = CacheR->Attach((XrdOucCacheIO *)this, cOpt);
+
+
+   return true;
 }
   
 /******************************************************************************/
