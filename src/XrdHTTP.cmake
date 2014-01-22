f6921606ddafebb5876cad6f673a580a18eaf4b2
Lukasz Janyst <ljanyst@cern.ch>
2014-01-22 09:20:08 +0100

    [CMake] Fix various issues in XrdHTTP.cmake

--- src/XrdHTTP.cmake
+++ src/XrdHTTP.cmake
@@ -1,26 +1,36 @@
+
+include( XRootDCommon )
+
 #-------------------------------------------------------------------------------
 # Shared library version
 #-------------------------------------------------------------------------------
 set( XRD_HTTP_VERSION   1.0.0 )
 set( XRD_HTTP_SOVERSION 1 )
 
+#-------------------------------------------------------------------------------
+# The XrdHttp library
+#-------------------------------------------------------------------------------
+include_directories( ${OPENSSL_INCLUDE_DIR} )
 
+add_library(
+  XrdHttp
+  SHARED
+  XrdHTTP/XrdHttpProtocol.cc    XrdHTTP/XrdHttpProtocol.hh
+  XrdHTTP/XrdHttpReq.cc         XrdHTTP/XrdHttpReq.hh
+                                XrdHTTP/XrdHttpSecXtractor.hh
+                                XrdHTTP/XrdHttpStatic.hh
+  XrdHTTP/XrdHttpTrace.cc       XrdHTTP/XrdHttpTrace.hh
+  XrdHTTP/XrdHttpUtils.cc       XrdHTTP/XrdHttpUtils.hh )
 
-# -------------------------
-# Local targets
-# -------------------------
-
-set(XrdHttp_SOURCES XrdHTTP/XrdHttpProtocol.cc XrdHTTP/XrdHttpReq.cc XrdHTTP/XrdHttpTrace.cc XrdHTTP/XrdHttpUtils.cc)
-
-#
-# Our target is a library to be loaded as a plugin by Xrootd
-#
-add_library(XrdHttp SHARED ${XrdHttp_SOURCES})
-set_target_properties(XrdHttp PROPERTIES
- VERSION "${XRD_HTTP_VERSION}"
- SOVERSION "${XRD_HTTP_SOVERSION}")
-
-target_link_libraries(XrdHttp XrdServer XrdXrootd XrdUtils XrdCrypto dl pthread ssl )
+target_link_libraries(
+  XrdHttp
+  XrdServer
+  XrdXrootd
+  XrdUtils
+  XrdCrypto
+  dl
+  pthread
+  ${OPENSSL_LIBRARIES} )
 
 set_target_properties(
   XrdHttp
@@ -30,7 +40,9 @@ set_target_properties(
   INTERFACE_LINK_LIBRARIES ""
   LINK_INTERFACE_LIBRARIES "" )
 
-# Install directive.
-install(TARGETS XrdHttp
+#-------------------------------------------------------------------------------
+# Install
+#-------------------------------------------------------------------------------
+install(
+  TARGETS XrdHttp
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
-
