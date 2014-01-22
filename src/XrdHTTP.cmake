1e6e8ad5d13e1b1c83fca91db694c089f865bd1a
Lukasz Janyst <ljanyst@solbuild01.(none)>
2014-01-22 14:19:12 +0100

    [CMake] Rewrite XrdHTTP.cmake so that the lib can build on every platform

--- src/XrdHTTP.cmake
+++ src/XrdHTTP.cmake
@@ -7,42 +7,46 @@ include( XRootDCommon )
 set( XRD_HTTP_VERSION   1.0.0 )
 set( XRD_HTTP_SOVERSION 1 )
 
-#-------------------------------------------------------------------------------
-# The XrdHttp library
-#-------------------------------------------------------------------------------
-include_directories( ${OPENSSL_INCLUDE_DIR} )
+if( BUILD_CRYPTO )
+  #-----------------------------------------------------------------------------
+  # The XrdHttp library
+  #-----------------------------------------------------------------------------
+  include_directories( ${OPENSSL_INCLUDE_DIR} )
 
-add_library(
-  XrdHttp
-  SHARED
-  XrdHTTP/XrdHttpProtocol.cc    XrdHTTP/XrdHttpProtocol.hh
-  XrdHTTP/XrdHttpReq.cc         XrdHTTP/XrdHttpReq.hh
-                                XrdHTTP/XrdHttpSecXtractor.hh
-                                XrdHTTP/XrdHttpStatic.hh
-  XrdHTTP/XrdHttpTrace.cc       XrdHTTP/XrdHttpTrace.hh
-  XrdHTTP/XrdHttpUtils.cc       XrdHTTP/XrdHttpUtils.hh )
+  add_library(
+    XrdHttp
+    SHARED
+    XrdHTTP/XrdHttpProtocol.cc    XrdHTTP/XrdHttpProtocol.hh
+    XrdHTTP/XrdHttpReq.cc         XrdHTTP/XrdHttpReq.hh
+                                  XrdHTTP/XrdHttpSecXtractor.hh
+                                  XrdHTTP/XrdHttpStatic.hh
+    XrdHTTP/XrdHttpTrace.cc       XrdHTTP/XrdHttpTrace.hh
+    XrdHTTP/XrdHttpUtils.cc       XrdHTTP/XrdHttpUtils.hh )
 
-target_link_libraries(
-  XrdHttp
-  XrdServer
-  XrdXrootd
-  XrdUtils
-  XrdCrypto
-  dl
-  pthread
-  ${OPENSSL_LIBRARIES} )
+  target_link_libraries(
+    XrdHttp
+    XrdServer
+    XrdXrootd
+    XrdUtils
+    XrdCrypto
+    dl
+    pthread
+    ${OPENSSL_LIBRARIES}
+    ${OPENSSL_CRYPTO_LIBRARY} )
 
-set_target_properties(
-  XrdHttp
-  PROPERTIES
-  VERSION   ${XRD_HTTP_VERSION}
-  SOVERSION ${XRD_HTTP_SOVERSION}
-  INTERFACE_LINK_LIBRARIES ""
-  LINK_INTERFACE_LIBRARIES "" )
+  set_target_properties(
+    XrdHttp
+    PROPERTIES
+    VERSION   ${XRD_HTTP_VERSION}
+    SOVERSION ${XRD_HTTP_SOVERSION}
+    INTERFACE_LINK_LIBRARIES ""
+    LINK_INTERFACE_LIBRARIES "" )
 
-#-------------------------------------------------------------------------------
-# Install
-#-------------------------------------------------------------------------------
-install(
-  TARGETS XrdHttp
-  LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
+  #-----------------------------------------------------------------------------
+  # Install
+  #-----------------------------------------------------------------------------
+  install(
+    TARGETS XrdHttp
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
+
+endif()
