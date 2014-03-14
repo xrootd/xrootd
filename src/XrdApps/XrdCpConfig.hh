68ef9e3168ff91b6547568a7fbd471b7680f6c48
Andrew Hanushevsky <abh@stanford.edu>
2014-03-14 16:29:49 -0700

    Change --noszchk to --dynamic-src.

--- src/XrdApps/XrdCpConfig.hh
+++ src/XrdApps/XrdCpConfig.hh
@@ -147,8 +147,8 @@ static const int    OpVersion  =  'V';        // -V | --version
 static const int    OpXrate    =  'X';
 static const int    DoXrate    =  0x00080000; // -X | --xrate
 
-static const int    OpNoSzChk  =  'Z';
-static const int    DoNoSzChk  =  0x00200000; //      --noszchk
+static const int    OpDynaSrc  =  'Z';
+static const int    DoDynaSrc  =  0x00200000; //      --dynamic-src
 
 // Call Config with the parameters passed to main() to fill out this object. If
 // the method returns then no errors have been found. Otherwise, it exits.
