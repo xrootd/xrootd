67b81f2fde4c6320da0198b380ead3a1a6fe31f1
Andrew Hanushevsky <abh@stanford.edu>
2014-03-24 17:38:14 -0700

    Correct handling of a temporary resume.

--- src/XrdCms/XrdCmsAdmin.cc
+++ src/XrdCms/XrdCmsAdmin.cc
@@ -182,7 +182,10 @@ void XrdCmsAdmin::Login(int socknum)
         {DEBUG("received request: '" <<request <<"'");
          if ((tp = Stream.GetToken()))
             {     if (!strcmp("resume",   tp))
-                      CmsState.Update(XrdCmsState::Active, 1);
+                     {if ((tp = Stream.GetToken()) && *tp == 't') sPerm = 0;
+                         else sPerm = 1;
+                      CmsState.Update(XrdCmsState::Active, 1, sPerm);
+                     }
              else if (!strcmp("rmdid",    tp)) do_RmDid();   // via lfn
              else if (!strcmp("newfn",    tp)) do_RmDud();   // via lfn
              else if (!strcmp("suspend",  tp)) 
