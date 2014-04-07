d9798bb42d9d06a92164e5bb530c76f0b5d1b5ef
Lukasz Janyst <ljanyst@cern.ch>
2014-04-07 11:01:49 +0200

    [XrdCl] Fix broke license header

--- src/XrdCl/XrdClCopyProcess.hh
+++ src/XrdCl/XrdClCopyProcess.hh
@@ -19,7 +19,7 @@
 //
 // In applying this licence, CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdicti
+// or submit itself to any jurisdiction.
 //------------------------------------------------------------------------------
 
 #ifndef __XRD_CL_COPY_PROCESS_HH__
