72be0ed34f6cb51d6d0b96d0f071639eda83f13d
Lukasz Janyst <ljanyst@cern.ch>
2015-02-20 12:01:57 +0100

    CMake: Look for CEPH in custom installation directory

--- cmake/Findceph.cmake
+++ cmake/Findceph.cmake
@@ -2,26 +2,42 @@
 #
 # RADOS_INCLUDE_DIR        - location of the ceph-devel header files for rados
 # RADOS_LIBS               - list of rados libraries, with full path
-#
+# RADOS_FOUND
 
-# Be silent if CEPH_INCLUDE_DIR is already cached
-if (RADOS_INCLUDE_DIR)
-  set(RADOS_FIND_QUIETLY TRUE)
-endif (RADOS_INCLUDE_DIR)
-
-find_path (RADOS_INCLUDE_DIR NAMES radosstriper/libradosstriper.hpp
-  PATH_SUFFIXES include/radosstriper
+find_path(
+  RADOS_INCLUDE_DIR
+  radosstriper/libradosstriper.hpp
+  HINTS
+  ${CEPH_DIR}
+  $ENV{CEPH_DIR}
+  /usr
+  /opt
+  PATH_SUFFIXES include
 )
 
-find_library (RADOSSTRIPER_LIB radosstriper)
-find_library (RADOS_LIB rados)
+find_library(
+  RADOSSTRIPER_LIB
+  NAMES radosstriper
+  HINTS
+  ${CEPH_DIR}
+  $ENV{CEPH_DIR}
+  /usr
+  /opt
+  PATH_SUFFIXES lib
+)
+
+find_library(
+  RADOS_LIB
+  NAMES rados
+  HINTS
+  ${CEPH_DIR}
+  $ENV{CEPH_DIR}
+  /usr
+  /opt
+  PATH_SUFFIXES lib
+)
+
 set(RADOS_LIBS ${RADOS_LIB} ${RADOSSTRIPER_LIB})
 
-message (STATUS "RADOS_INCLUDE_DIR        = ${RADOS_INCLUDE_DIR}")
-message (STATUS "RADOS_LIBS               = ${RADOS_LIBS}")
-
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (ceph DEFAULT_MSG
-  RADOS_INCLUDE_DIR
-  RADOS_LIBS)
-
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ceph DEFAULT_MSG RADOS_INCLUDE_DIR RADOS_LIBS)
