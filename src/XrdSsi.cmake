f43a6214d81933eb973eaa8725c28f04870b04f9
Andrew Hanushevsky <abh@stanford.edu>
2015-05-31 19:01:33 -0700

    Add a shared meory package, XrdSsiShMap, to the ssi package.

--- src/XrdSsi.cmake
+++ src/XrdSsi.cmake
@@ -6,6 +6,8 @@ include( XRootDCommon )
 #-------------------------------------------------------------------------------
 set( XRD_SSI_VERSION   1.0.0 )
 set( XRD_SSI_SOVERSION 1 )
+set( XRD_SSI_SHMAP_VERSION   1.0.0 )
+set( XRD_SSI_SHMAP_SOVERSION 1 )
 
 #-------------------------------------------------------------------------------
 # The XrdSsi library
@@ -13,6 +15,7 @@ set( XRD_SSI_SOVERSION 1 )
 add_library(
   XrdSsi
   SHARED
+                                       XrdSsi/XrdSsiAtomics.hh
                                        XrdSsi/XrdSsiBVec.hh
 XrdSsi/XrdSsiClient.cc
                                        XrdSsi/XrdSsiCluster.hh
@@ -55,8 +58,29 @@ set_target_properties(
   LINK_INTERFACE_LIBRARIES "" )
 
 #-------------------------------------------------------------------------------
+# The XrdSsiShMap library
+#-------------------------------------------------------------------------------
+add_library(
+  XrdSsiShMap
+  SHARED
+XrdSsi/XrdSsiShMam.cc                  XrdSsi/XrdSsiShMam.hh
+XrdSsi/XrdSsiShMap.icc                 XrdSsi/XrdSsiShMap.hh
+XrdSsi/XrdSsiShMat.cc                  XrdSsi/XrdSsiShMat.hh)
+
+target_link_libraries(
+  XrdSsiShMap
+  ${ZLIB_LIBRARY} )
+
+set_target_properties(
+  XrdSsiShMap
+  PROPERTIES
+  VERSION   ${XRD_SSI_SHMAP_VERSION}
+  SOVERSION ${XRD_SSI_SHMAP_SOVERSION}
+  LINK_INTERFACE_LIBRARIES "" )
+
+#-------------------------------------------------------------------------------
 # Install
 #-------------------------------------------------------------------------------
 install(
-  TARGETS XrdSsi
+  TARGETS XrdSsi XrdSsiShMap
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
