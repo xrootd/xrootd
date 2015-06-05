b1c608f124165fe4a48e70a282f244ae530c699c
Elvin Sindrilaru <esindril@cern.ch>
2015-06-05 14:37:09 +0200

    Python: Add conversion functions from PyObj to unsigned types
       The conversion functions from PyObj to unsigned types (uint64_t, uint32_t,
       uint16_t) need to be used throughout the code as the PyArg_Parse* does no
       overflow/underflow checks.

--- bindings/python/src/Utils.cc
+++ bindings/python/src/Utils.cc
@@ -27,6 +27,9 @@
 
 namespace PyXRootD
 {
+  //----------------------------------------------------------------------------
+  // Check that the given callback is actually callable.
+  //----------------------------------------------------------------------------
   bool IsCallable( PyObject *callable )
   {
     if ( !PyCallable_Check( callable ) ) {
@@ -39,6 +42,9 @@ namespace PyXRootD
     return true;
   }
 
+  //----------------------------------------------------------------------------
+  // Initialize the Python types for the extension.
+  //----------------------------------------------------------------------------
   int InitTypes()
   {
     URLType.tp_new = PyType_GenericNew;
@@ -47,5 +53,138 @@ namespace PyXRootD
     Py_INCREF( &URLType );
     return 0;
   }
-}
 
+  //----------------------------------------------------------------------------
+  // Convert PyInt to unsigned long (uint64_t)
+  //----------------------------------------------------------------------------
+  int PyIntToUlong(PyObject *py_val, unsigned long *val, const char *name)
+  {
+    const long tmp = PyInt_AsLong(py_val);
+
+    if (tmp == -1 && PyErr_Occurred())
+    {
+      if (PyErr_ExceptionMatches(PyExc_OverflowError))
+        PyErr_Format(PyExc_OverflowError, "%s too big for unsigned long", name);
+      return -1;
+    }
+
+    if (tmp < 0)
+    {
+      PyErr_Format(PyExc_OverflowError,
+                   "negative %s cannot be converted to unsigned long", name);
+      return -1;
+    }
+
+    *val = tmp;
+    return 0;
+  }
+
+  //----------------------------------------------------------------------------
+  // Convert Python object to unsigned long (uint64_t)
+  //----------------------------------------------------------------------------
+  int PyObjToUlong(PyObject *py_val, unsigned long *val, const char *name)
+  {
+    if (PyInt_Check(py_val))
+      return PyIntToUlong(py_val, val, name);
+
+    if (!PyLong_Check(py_val))
+    {
+      PyErr_Format(PyExc_TypeError, "expected integer %s", name);
+      return -1;
+    }
+
+    const unsigned long tmp = PyLong_AsUnsignedLong(py_val);
+
+    if (PyErr_Occurred())
+    {
+      if (PyErr_ExceptionMatches(PyExc_OverflowError))
+        PyErr_Format(PyExc_OverflowError, "%s too big for unsigned long", name);
+      return -1;
+    }
+
+    *val = tmp;
+    return 0;
+  }
+
+  //----------------------------------------------------------------------------
+  // Convert Python object to unsigned int (uint32_t)
+  //----------------------------------------------------------------------------
+  int PyObjToUint(PyObject *py_val, unsigned int *val, const char *name)
+  {
+    unsigned long tmp;
+
+    if (PyObjToUlong(py_val, &tmp, name))
+      return -1;
+
+    if (tmp > UINT_MAX)
+    {
+      PyErr_Format(PyExc_OverflowError, "%s too big for unsigned int (uint32_t)",
+                   name);
+      return -1;
+    }
+
+    *val = tmp;
+    return 0;
+  }
+
+  //----------------------------------------------------------------------------
+  // Convert Python object to unsigned short int (uint16_t)
+  //----------------------------------------------------------------------------
+  int PyObjToUshrt(PyObject *py_val, unsigned short int *val, const char *name)
+  {
+    unsigned int tmp;
+
+    if (PyObjToUint(py_val, &tmp, name))
+      return -1;
+
+    fprintf(stderr, "tmp=%u\n", tmp);
+    if (tmp > USHRT_MAX)
+    {
+      PyErr_Format(PyExc_OverflowError, "%s too big for unsigned short int "
+                   "(uint16_t)", name);
+      return -1;
+    }
+
+    *val = tmp;
+    return 0;
+  }
+
+  //----------------------------------------------------------------------------
+  // Convert Python object to unsigned long long (uint64_t)
+  //----------------------------------------------------------------------------
+  int PyObjToUllong(PyObject *py_val, unsigned PY_LONG_LONG *val,
+                    const char *name)
+  {
+    if (PyInt_Check(py_val))
+    {
+      unsigned long tmp;
+
+      if (!PyIntToUlong(py_val, &tmp, name))
+      {
+        *val = tmp;
+        return 0;
+      }
+
+      return -1;
+    }
+
+    if (!PyLong_Check(py_val))
+    {
+      PyErr_Format(PyExc_TypeError, "integer argument expected for %s", name);
+      return -1;
+    }
+
+    const unsigned PY_LONG_LONG tmp = PyLong_AsUnsignedLongLong(py_val);
+
+    if ((tmp == (unsigned long long) -1) && PyErr_Occurred())
+    {
+      if (PyErr_ExceptionMatches(PyExc_OverflowError))
+        PyErr_Format(PyExc_OverflowError,
+                     "%s too big for unsigned long long", name);
+      return -1;
+    }
+
+    *val = tmp;
+    return 0;
+  }
+}
