87ebf644a6ab7ae96a94b35b80cf8ff89f18bc30
otron <oddweb42+gith@gmail.com>
2015-07-28 16:14:59 +0200

    Python: limited support for osx w/ homebrew
    Looks for xrootd include and library directories where they are found on
    OS X/Darwin systems where xrootd has been installed via homebrew.
    
    Only works if the bindings version match the xrootd version installed
    via homebrew.
    
    Comments on the installation procedure for OSX in README.rst

--- bindings/python/setup_pypi.py
+++ bindings/python/setup_pypi.py
@@ -1,6 +1,7 @@
 from distutils.core import setup, Extension
 from distutils import sysconfig
 from os import getenv, walk, path, path, getcwd, chdir
+from platform import system
 import subprocess
 
 # Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
@@ -19,12 +20,22 @@ for dirname, dirnames, filenames in walk('src'):
     elif filename.endswith('.hh'):
       depends.append(path.join(dirname, filename))
 
-xrdlibdir = getenv( 'XRD_LIBDIR' ) or '/usr/lib'
-xrdincdir = getenv( 'XRD_INCDIR' ) or '/usr/include/xrootd'
-
 # Get package version
 with open ('VERSION_INFO') as verfile:
     version = verfile.read().strip()
+
+def getincdir_osx():
+    # Assume xrootd was installed via homebrew
+    return '/usr/local/Cellar/xrootd/{0}/include/xrootd'.format(version)
+
+def getlibdir():
+    return (system() == 'Darwin' and '/usr/local/lib') or '/usr/lib'
+
+def getincdir():
+    return (system() == 'Darwin' and getincdir_osx()) or '/usr/include/xrootd'
+
+xrdlibdir = getenv( 'XRD_LIBDIR' ) or getlibdir()
+xrdincdir = getenv( 'XRD_INCDIR' ) or getincdir()
 
 print 'XRootD library dir: ', xrdlibdir
 print 'XRootD include dir: ', xrdincdir
