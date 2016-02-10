d0585b5369d9d6162cdc020cbfbe1b0589afeb24
Michal Simon <michal.simon@cern.ch>
2016-02-10 10:36:59 +0100

    [CMake] Treat misleading indentations as warnings (gcc 6.0 and greater only).

--- cmake/XRootDOSDefs.cmake
+++ cmake/XRootDOSDefs.cmake
@@ -17,13 +17,16 @@ set( LIBRARY_PATH_PREFIX "lib" )
 if( CMAKE_COMPILER_IS_GNUCXX )
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror" )
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter" )
-  set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=misleading-indentation" )
   # gcc 4.1 is retarded
   execute_process( COMMAND ${CMAKE_C_COMPILER} -dumpversion
                    OUTPUT_VARIABLE GCC_VERSION )
   if( (GCC_VERSION VERSION_GREATER 4.1 OR GCC_VERSION VERSION_EQUAL 4.1)
       AND GCC_VERSION VERSION_LESS 4.2 )
     set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-strict-aliasing" )
+  endif()
+  # gcc 6.0 is more pedantic
+  if( GCC_VERSION VERSION_GREATER 6.0 OR GCC_VERSION VERSION_EQUAL 6.0 )
+    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=misleading-indentation" )
   endif()
 endif()
 
