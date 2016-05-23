77a49d4d121375df82e5b20d07f9ec0a996f528b
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2016-05-23 10:22:54 -0700

    In hdfs healing mode forward stat query to origin.

--- src/XrdFileCache/XrdFileCacheIOFileBlock.hh
+++ src/XrdFileCache/XrdFileCacheIOFileBlock.hh
@@ -64,6 +64,7 @@ namespace XrdFileCache
          //! Called to check if destruction needs to be done in a separate task.
          virtual bool ioActive();
 
+         virtual int  Fstat(struct stat &sbuff);
       private:
          long long                  m_blocksize; //!< size of file-block
          std::map<int, File*>       m_blocks;    //!< map of created blocks
