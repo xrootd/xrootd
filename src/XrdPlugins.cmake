1cf12a4ba99d134452d891970db5003b0e7e7a1a
Andrew Hanushevsky <abh@stanford.edu>
2016-09-03 17:41:13 -0700

    Change library packaging to follow EPEL rules.

--- src/XrdPlugins.cmake
+++ src/XrdPlugins.cmake
@@ -8,6 +8,7 @@ set( LIB_XRD_BWM        XrdBwm-${PLUGIN_VERSION} )
 set( LIB_XRD_PSS        XrdPss-${PLUGIN_VERSION} )
 set( LIB_XRD_GPFS       XrdOssSIgpfsT-${PLUGIN_VERSION} )
 set( LIB_XRD_ZCRC32     XrdCksCalczcrc32-${PLUGIN_VERSION} )
+set( LIB_XRD_SSI        XrdSsi-${PLUGIN_VERSION} )
 set( LIB_XRD_THROTTLE   XrdThrottle-${PLUGIN_VERSION} )
 
 #-------------------------------------------------------------------------------
@@ -102,6 +103,32 @@ set_target_properties(
   LINK_INTERFACE_LIBRARIES "" )
 
 #-------------------------------------------------------------------------------
+# The XrdSsi lib
+#-------------------------------------------------------------------------------
+add_library(
+  ${LIB_XRD_SSI}
+  SHARED
+  XrdSsi/XrdSsiDir.cc                    XrdSsi/XrdSsiDir.hh
+  XrdSsi/XrdSsiFile.cc                   XrdSsi/XrdSsiFile.hh
+  XrdSsi/XrdSsiFileReq.cc                XrdSsi/XrdSsiFileReq.hh
+  XrdSsi/XrdSsiFileSess.cc               XrdSsi/XrdSsiFileSess.hh
+  XrdSsi/XrdSsiSfs.cc                    XrdSsi/XrdSsiSfs.hh
+  XrdSsi/XrdSsiSfsConfig.cc              XrdSsi/XrdSsiSfsConfig.hh
+  XrdSsi/XrdSsiStat.cc
+)
+
+target_link_libraries(
+  ${LIB_XRD_SSI}
+  XrdSsiLib
+  XrdServer )
+
+set_target_properties(
+  ${LIB_XRD_SSI}
+  PROPERTIES
+  INTERFACE_LINK_LIBRARIES ""
+  LINK_INTERFACE_LIBRARIES "" )
+
+#-------------------------------------------------------------------------------
 # The XrdThrottle lib
 #-------------------------------------------------------------------------------
 add_library(
@@ -130,5 +157,5 @@ set_target_properties(
 # Install
 #-------------------------------------------------------------------------------
 install(
-  TARGETS ${LIB_XRD_PSS} ${LIB_XRD_BWM} ${LIB_XRD_GPFS} ${LIB_XRD_ZCRC32} ${LIB_XRD_THROTTLE}
+  TARGETS ${LIB_XRD_PSS} ${LIB_XRD_BWM} ${LIB_XRD_GPFS} ${LIB_XRD_ZCRC32} ${LIB_XRD_SSI} ${LIB_XRD_THROTTLE}
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
--- src/XrdSsi.cmake
+++ src/XrdSsi.cmake
@@ -13,7 +13,7 @@ set( XRD_SSI_SHMAP_SOVERSION 1 )
 # The XrdSsi library
 #-------------------------------------------------------------------------------
 add_library(
-  XrdSsi
+  XrdSsiLib
   SHARED
                                        XrdSsi/XrdSsiAtomics.hh
                                        XrdSsi/XrdSsiBVec.hh
@@ -21,12 +21,8 @@ XrdSsi/XrdSsiClient.cc
                                        XrdSsi/XrdSsiCluster.hh
 XrdSsi/XrdSsiCms.cc                    XrdSsi/XrdSsiCms.hh
 XrdSsi/XrdSsiDebug.cc                  XrdSsi/XrdSsiDebug.hh
-XrdSsi/XrdSsiDir.cc                    XrdSsi/XrdSsiDir.hh
                                        XrdSsi/XrdSsiErrInfo.hh
 XrdSsi/XrdSsiEvent.cc                  XrdSsi/XrdSsiEvent.hh
-XrdSsi/XrdSsiFile.cc                   XrdSsi/XrdSsiFile.hh
-XrdSsi/XrdSsiFileReq.cc                XrdSsi/XrdSsiFileReq.hh
-XrdSsi/XrdSsiFileSess.cc               XrdSsi/XrdSsiFileSess.hh
 XrdSsi/XrdSsiLogger.cc                 XrdSsi/XrdSsiLogger.hh
 XrdSsi/XrdSsiPacer.cc                  XrdSsi/XrdSsiPacer.hh
                                        XrdSsi/XrdSsiProvider.hh
@@ -40,23 +36,20 @@ XrdSsi/XrdSsiServReal.cc               XrdSsi/XrdSsiServReal.hh
 XrdSsi/XrdSsiSessReal.cc               XrdSsi/XrdSsiSessReal.hh
 XrdSsi/XrdSsiSSRun.cc                  XrdSsi/XrdSsiSSRun.hh
                                        XrdSsi/XrdSsiSession.hh
-XrdSsi/XrdSsiSfs.cc                    XrdSsi/XrdSsiSfs.hh
-XrdSsi/XrdSsiSfsConfig.cc              XrdSsi/XrdSsiSfsConfig.hh
-XrdSsi/XrdSsiStat.cc
                                        XrdSsi/XrdSsiStream.hh
 XrdSsi/XrdSsiTaskReal.cc               XrdSsi/XrdSsiTaskReal.hh
                                        XrdSsi/XrdSsiTrace.hh
 XrdSsi/XrdSsiUtils.cc                  XrdSsi/XrdSsiUtils.hh)
 
 target_link_libraries(
-  XrdSsi
+  XrdSsiLib
   XrdCl
   XrdServer
   XrdUtils
   pthread )
 
 set_target_properties(
-  XrdSsi
+  XrdSsiLib
   PROPERTIES
   VERSION   ${XRD_SSI_VERSION}
   SOVERSION ${XRD_SSI_SOVERSION}
@@ -88,5 +81,5 @@ set_target_properties(
 # Install
 #-------------------------------------------------------------------------------
 install(
-  TARGETS XrdSsi XrdSsiShMap
+  TARGETS XrdSsiLib XrdSsiShMap
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
