f8d9f1f0ff8a04b4be3c22f6d25d909c525e7c4e
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-05-19 10:57:58 +0200

    Make tmoMask unsigned

--- src/XrdSys/XrdSysIOEvents.hh
+++ src/XrdSys/XrdSysIOEvents.hh
@@ -506,7 +506,7 @@ struct          PipeData {char req; char evt; short ent; int fd;
 PipeData        reqBuff;    // Buffer used by poller thread to recv data
 char           *pipeBuff;   // Read resumption point in buffer
 int             pipeBlen;   // Number of outstanding bytes
-char            tmoMask;    // Timeout mask
+unsigned char   tmoMask;    // Timeout mask
 CPP_ATOMIC_TYPE(bool) wakePend;   // Wakeup is effectively pending (don't send)
 bool            chDead;     // True if channel deleted by callback
 
