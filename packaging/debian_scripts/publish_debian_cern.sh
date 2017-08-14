e6e37264ac64135d3c8251c82c451d05d662eae4
Jozsef Makai <jmakai@cern.ch>
2017-08-14 16:53:12 +0200

    [CI] disabling batch mode repo signs

--- packaging/debian_scripts/publish_debian_cern.sh
+++ packaging/debian_scripts/publish_debian_cern.sh
@@ -22,6 +22,6 @@ for dist in artful; do
   components=$(find $prefix/dists/$dist/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | tr '\n' ' ')
   rm $prefix/dists/$dist/Release $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release.gpg
   apt-ftparchive -o APT::FTPArchive::Release::Origin=CERN -o APT::FTPArchive::Release::Label=XrootD -o APT::FTPArchive::Release::Codename=artful -o APT::FTPArchive::Release::Architectures=amd64 -o APT::FTPArchive::Release::Components="$components" release $prefix/dists/$dist/ > $prefix/dists/$dist/Release;
-  gpg --homedir /home/stci/ --batch --yes --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release;
-  gpg --homedir /home/stci/ --batch --yes -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release;
+  gpg --homedir /home/stci/ --clearsign -o $prefix/dists/$dist/InRelease $prefix/dists/$dist/Release;
+  gpg --homedir /home/stci/ -abs -o $prefix/dists/$dist/Release.gpg $prefix/dists/$dist/Release;
 done
