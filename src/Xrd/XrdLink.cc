xrootd/xrootd

commit 908b88afb93a0f484900abdea8b321d5708e5788

XrdLink: Increment the IOSemaphore once for each waiting thread
Depending on timing, there may be more than one thread waiting on the
IOSemaphore. (doPost > 1)

Increment the semaphore once for each waiting thread.

src/Xrd/XrdLink.cc
@@ -1112,9 +1112,10 @@ void XrdLink::setRef(int use)
              XrdLog->Emsg("Link", "Zero use count for", ID);
             }
     else if (InUse == 1 && doPost)
-            {doPost--;
-             IOSemaphore.Post();
-             TRACEI(CONN, "setRef posted link");
+            {while(doPost--)
+                {IOSemaphore.Post();
+                 TRACEI(CONN, "setRef posted link");
+                }
              opMutex.UnLock();
             }
     else if (InUse < 0)

commit bbbc8ef9011e2f4136c0c117404f67c58525680b

[Server] Make sure doPost does not become <0 (error introduced by 908b88af).

src/Xrd/XrdLink.cc
@@ -1112,9 +1112,10 @@ void XrdLink::setRef(int use)
              XrdLog->Emsg("Link", "Zero use count for", ID);
             }
     else if (InUse == 1 && doPost)
-            {while(doPost--)
+            {while(doPost)
                 {IOSemaphore.Post();
                  TRACEI(CONN, "setRef posted link");
+                 doPost--;
                 }
              opMutex.UnLock();
             }