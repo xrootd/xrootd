50ff1d12d73110e1175ef5bfcd249a0282c96d1c
Krzysztof Jamrog <kjamrog@student-pc.dyndns.cern.ch>
2018-08-03 16:50:04 +0200

    File operations workflows - tests, documentation, setting execution status

--- src/XrdCl/XrdClOperationParams.hh
+++ src/XrdCl/XrdClOperationParams.hh
@@ -1,25 +1,45 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2011-2017 by European Organization for Nuclear Research (CERN)
+// Author: Krzysztof Jamrog <krzysztof.piotr.jamrog@cern.ch>
+//------------------------------------------------------------------------------
+// This file is part of the XRootD software suite.
+//
+// XRootD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// XRootD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
+//
+// In applying this licence, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//------------------------------------------------------------------------------
+
 #ifndef __XRD_CL_OPERATION_PARAMS_HH__
 #define __XRD_CL_OPERATION_PARAMS_HH__
 
-#include <iostream>
-
-#define notdef(type) OptionalParam<type>()
-
-using namespace std;
 
 namespace XrdCl {
+
+    class NotDefParam {} notdef;
 
     template <typename T>
     class OptionalParam {
         public:
-            OptionalParam(T val){
-                value = val;
-                empty = false;
+            OptionalParam(T val): empty(false){
+                value = val;    
             }
 
-            OptionalParam(){
-                empty = true;
-            }
+            OptionalParam(): empty(true){}
+
+            OptionalParam(NotDefParam notdef): empty(true){}
 
             bool IsEmpty(){
                 return empty;
@@ -27,7 +47,7 @@ namespace XrdCl {
 
             T GetValue(){
                 if(IsEmpty()){
-                    throw logic_error("Cannot get parameter: value has not been specified");
+                    throw std::logic_error("Cannot get parameter: value has not been specified");
                 }
                 return value;
             }
@@ -37,48 +57,81 @@ namespace XrdCl {
             bool empty;
     };
 
+
+    //--------------------------------------------------------------------
+    //! Container to store file operation parameters
+    //--------------------------------------------------------------------
     class ParamsContainer {
         public:
+            //------------------------------------------------------------
+            //! Get value from container
+            //!
+            //! @param key  key under which the value is stored
+            //! @return     value
+            //------------------------------------------------------------
             template <typename T>
             T GetParam(std::string key){
                 T* valuePtr = GetPtrParam<T*>(key);
                 return *valuePtr;
             }
 
+            //------------------------------------------------------------
+            //! Get pointer from container
+            //!
+            //! @param key  key under which the pointer is stored
+            //! @return     pointer to stored object
+            //------------------------------------------------------------
             template <typename T>
             T GetPtrParam(std::string key){
                 if(!Exists(key)){
-                    throw logic_error(std::string("Parameter ") + key + std::string(" has not been specified"));
+                    throw std::logic_error(std::string("Parameter ") + key + std::string(" has not been specified"));
                 }
                 AnyObject *obj = paramsMap[key];
                 T valuePtr = 0;
                 obj->Get(valuePtr);
-                cout<<"Getting "<<key<<" param"<<endl;
                 return valuePtr;
             }
 
+            //------------------------------------------------------------
+            //! Save value in container
+            //!
+            //! @param key  key under which the value will be saved
+            //------------------------------------------------------------
             template <typename T>
             void SetParam(std::string key, T value){
                 T *valuePtr = new T(value);
                 SetPtrParam(key, valuePtr, true);
             }
 
+            //------------------------------------------------------------
+            //! Save pointer in container
+            //!
+            //! @param key  key under which the pointer will be saved
+            //------------------------------------------------------------
             template <typename T>
             void SetPtrParam(std::string key, T* value, bool passOwnership = false){
                 if(Exists(key)){
-                    throw logic_error(std::string("Parameter ") + key + std::string(" has already been set"));
+                    throw std::logic_error(std::string("Parameter ") + key + std::string(" has already been set"));
                 }
                 AnyObject *obj = new AnyObject();
                 obj->Set(value, passOwnership);
                 paramsMap[key] = obj;
             }     
 
+            //------------------------------------------------------------
+            //! Check whether given key exists in the container
+            //!
+            //! @return true if exists, false if not
+            //------------------------------------------------------------
             bool Exists(std::string key){
                 return paramsMap.find(key) != paramsMap.end();
-            } 
+            }
 
             ~ParamsContainer(){
                 std::map<std::string, AnyObject*>::iterator it = paramsMap.begin();
+                //----------------------------------------------------------------
+                // Clear map
+                //----------------------------------------------------------------
                 while(it != paramsMap.end()){
                     AnyObject* obj = it->second;
                     std::string keyToErase = it->first;
@@ -91,6 +144,7 @@ namespace XrdCl {
         private:
             std::map<std::string, AnyObject*> paramsMap;
     };
+
 }
 
 
--- src/XrdCl/XrdClOperations.cc
+++ src/XrdCl/XrdClOperations.cc
@@ -1,63 +1,86 @@
-#include <iostream>
-#include <XrdCl/XrdClOperations.hh>
-#include <typeinfo>
+//------------------------------------------------------------------------------
+// Copyright (c) 2011-2017 by European Organization for Nuclear Research (CERN)
+// Author: Krzysztof Jamrog <krzysztof.piotr.jamrog@cern.ch>
+//------------------------------------------------------------------------------
+// This file is part of the XRootD software suite.
+//
+// XRootD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// XRootD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
+//
+// In applying this licence, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//------------------------------------------------------------------------------
 
-using namespace std;
+#include <XrdCl/XrdClOperations.hh>
 
 
 namespace XrdCl {
 
-    //////////// OperationHandler
+    //----------------------------------------------------------------------------
+    // OperationHandler
+    //----------------------------------------------------------------------------
 
-    OperationHandler::OperationHandler(ForwardingHandler *handler){
+    OperationHandler::OperationHandler(ForwardingHandler *handler): nextOperation(NULL), semaphore(NULL){
         responseHandler = handler;
         params = handler->GetParamsContainer();
-        nextOperation = NULL;
-        semaphore = NULL;
     }
 
-    void OperationHandler::AddOperation(Operation<Handled> *op){
+    void OperationHandler::AddOperation(Operation<Handled> *operation){
         if(nextOperation){
-            nextOperation->AddOperation(op);
+            nextOperation->AddOperation(operation);
         } else {
-            nextOperation = op;
+            nextOperation = operation;
         }
     }
 
     void OperationHandler::HandleResponseWithHosts(XRootDStatus *status, AnyObject *response, HostList *hostList){
-        bool operationStatus = status->IsOK();
-        if(operationStatus){
-            responseHandler->HandleResponseWithHosts(status, response, hostList);
-            RunNextOperation();
-        } else {
-            cout<<"Operation status = "<<operationStatus
-                <<". Handler and next operations will not be run. Reason: "
-                <<status->ToStr()<<endl;
-            if(semaphore){
-                semaphore->Post();
-            }
+        if(!status->IsOK()){
+            return HandleFailedOperationStatus(status, response);
         }
+        if(nextOperation){
+            responseHandler->HandleResponseWithHosts(status, response, hostList);
+            return RunNextOperation();
+        }
+        // We need to copy status as original status object is destroyed in HandleResponse function
+        auto statusCopy = new XRootDStatus(*status);
+        responseHandler->HandleResponseWithHosts(status, response, hostList);
+        workflow->EndWorkflowExecution(statusCopy);
     }
 
     void OperationHandler::HandleResponse(XRootDStatus *status, AnyObject *response){
-        responseHandler->HandleResponse(status, response);
-        if(status->IsOK()){
-            RunNextOperation();
-        } else {
-            cout<<"Operation status = "<<status->IsOK()<<". Next operation will not be run."<<endl;
-            if(semaphore){
-                semaphore->Post();
-            }
+        if(!status->IsOK()){
+            return HandleFailedOperationStatus(status, response);   
         }
+        if(nextOperation){
+            responseHandler->HandleResponse(status, response);
+            return RunNextOperation();
+        }
+        // We need to copy status as original status object is destroyed in HandleResponse function
+        auto statusCopy = new XRootDStatus(*status);
+        responseHandler->HandleResponse(status, response);
+        workflow->EndWorkflowExecution(statusCopy);
+    }
+
+    void OperationHandler::HandleFailedOperationStatus(XRootDStatus *status, AnyObject *response){
+        if(response){
+            delete response;
+        }
+        workflow->EndWorkflowExecution(status);
     }
 
     void OperationHandler::RunNextOperation(){
-        if(nextOperation){ 
-            cout<<"Running next operation:  "<<nextOperation->GetName()<<endl;
-            nextOperation->Run(params); 
-        } else if (semaphore){
-            semaphore->Post();
-        }
+        nextOperation->Run(params);
     }
 
     OperationHandler::~OperationHandler(){
@@ -66,47 +89,55 @@ namespace XrdCl {
         if(responseHandler) { delete responseHandler; }
     }
 
-    ForwardingHandler* OperationHandler::GetHandler(){
-        return responseHandler;
-    }
-
-    void OperationHandler::SetSemaphore(XrdSysSemaphore *sem){
-        semaphore = sem;
+    void OperationHandler::AssignToWorkflow(Workflow *wf){
+        workflow = wf;
         if(nextOperation){
-            nextOperation->SetSemaphore(sem);
+            nextOperation->AssignToWorkflow(wf);
         }
     }
 
 
-    //////////////// Workflow
+    //----------------------------------------------------------------------------
+    // Workflow
+    //----------------------------------------------------------------------------
 
-    Workflow::Workflow(Operation<Handled> &op){
+    Workflow::Workflow(Operation<Handled> &op): semaphore(NULL), status(NULL){
         firstOperation = &op;
         firstOperationParams = new ParamsContainer();
-        semaphore = NULL;        
     }
 
     Workflow::~Workflow(){
         delete firstOperation;
         delete firstOperationParams;
-        if(semaphore) {
-            delete semaphore;
-        }
+        if(semaphore) { delete semaphore; }
+        if(status) { delete status; }
     }
 
     Workflow& Workflow::Run(){
-        if(!semaphore){
-            semaphore = new XrdSysSemaphore(0);
-            firstOperation->SetSemaphore(semaphore);
-            firstOperation->Run(firstOperationParams);
-        } else {
-            cout<<"Workflow is already running"<<endl;
+        if(semaphore){
+            throw std::logic_error("Workflow is already running");
         }
+        semaphore = new XrdSysSemaphore(0);
+        firstOperation->AssignToWorkflow(this);
+        firstOperation->Run(firstOperationParams);
         return *this;
     }
 
+    void Workflow::EndWorkflowExecution(XRootDStatus *lastOperationStatus){
+        if(semaphore){
+            status = lastOperationStatus;
+            semaphore->Post();
+        }
+    }
+
+    XRootDStatus Workflow::GetStatus(){
+        return status ? *status : XRootDStatus();
+    }
+
     void Workflow::Wait(){
-        semaphore->Wait();
+        if(semaphore){
+            semaphore->Wait();
+        }
         return;
     }
 
