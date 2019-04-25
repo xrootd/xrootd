a22f92489a3078ea1d662e0fb1f9131643bef1e9
Ben Krikler <bek07@ic.ac.uk>
2019-04-25 02:38:31 +0200

    Test glob on realy xrootd files

--- bindings/python/tests/test_glob.py
+++ bindings/python/tests/test_glob.py
@@ -17,20 +17,27 @@ def tmptree(tmpdir):
     return tmpdir
 
 
-@pytest.mark.parametrize("prefix", ["", r"root://localhost:"])
-def test_glob(tmptree, prefix):
-    tmptree = str(tmptree)
-    if prefix:
-        tmptree = prefix + tmptree
-    normal_glob_result = norm_glob.glob(os.path.join(tmptree, "not-there"))
-    assert glob.glob(os.path.join(tmptree, "not-there")) == normal_glob_result
-    assert len(glob.glob(os.path.join(tmptree, "not-there"))) == 0
-    assert len(glob.glob(os.path.join(tmptree, "not-there*"))) == 0
-    assert len(glob.glob(os.path.join(tmptree, "sub*"))) == 2
-    assert len(glob.glob(os.path.join(tmptree, "subdir1", "*txt"))) == 3
-    assert len(glob.glob(os.path.join(tmptree, "subdir*", "*txt"))) == 3
+def test_glob_local(tmptree):
+    normal_glob_result = norm_glob.glob(str(tmptree / "not-there"))
+    assert glob.glob(str(tmptree / "not-there")) == normal_glob_result
+    assert len(glob.glob(str(tmptree / "not-there"))) == 0
+    assert len(glob.glob(str(tmptree / "not-there*"))) == 0
+    assert len(glob.glob(str(tmptree / "sub*"))) == 2
+    assert len(glob.glob(str(tmptree / "subdir1" / "*txt"))) == 3
+    assert len(glob.glob(str(tmptree / "subdir*" / "*txt"))) == 3
 
     with pytest.raises(RuntimeError) as excinfo:
-        glob.glob(os.path.join(tmptree, "not-there"), raise_error=True)
+        glob.glob(str(tmptree / "not-there"), raise_error=True)
     assert "[ERROR]" in str(excinfo.value)
     assert str(tmptree) in str(excinfo.value)
+
+
+def test_glob_remote(tmptree):
+    assert len(glob.glob("root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoad/")) == 0
+    assert len(glob.glob("root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoa*")) == 1
+    assert len(glob.glob("root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/*")) > 0
+    assert len(glob.glob("root://eospublic.cern.ch//eos/root-*/cms_opendata_2012_nanoaod/*")) > 0
+
+    with pytest.raises(RuntimeError) as excinfo:
+        glob.glob("root://eospublic.cern.ch//eos/root-NOTREAL/cms_opendata_2012_nanoaod/*", raise_error=True)
+    assert "[ERROR]" in str(excinfo.value)
