xrootd/xrootd

commit 95e2d5cb90e26cadd2546f7c9c2c4eeb3727ed56

[XrdCl] Make sure the protocol (root vs roots) is preserved across
redirections. fixes #1145

src/XrdCl/XrdClXRootDMsgHandler.cc
@@ -625,7 +625,8 @@ namespace XrdCl
         //----------------------------------------------------------------------
         // Check if we need to return the URL as a response
         //----------------------------------------------------------------------
-        if( newUrl.GetProtocol() != "root" && newUrl.GetProtocol() != "xroot" &&
+        if( newUrl.GetProtocol() != "root"  && newUrl.GetProtocol() != "xroot"  &&
+            newUrl.GetProtocol() != "roots" && newUrl.GetProtocol() != "xroots" &&
             !newUrl.IsLocalFile() )
           pRedirectAsAnswer = true;
 
@@ -648,6 +649,11 @@ namespace XrdCl
           HandleResponse();
           return;
         }
+
+        //----------------------------------------------------------------------
+        // Make sure we don't change the protocol by accident (root vs roots)
+        //----------------------------------------------------------------------
+        newUrl.SetProtocol( pUrl.GetProtocol() );
 
         //----------------------------------------------------------------------
         // Send the request to the new location
@@ -2220,7 +2226,7 @@ namespace XrdCl
     // Set up a redirect entry
     //--------------------------------------------------------------------------
     if( pRdirEntry ) pRedirectTraceBack.push_back( std::move( pRdirEntry ) );
-    pRdirEntry.reset( new RedirectEntry( pUrl.GetHostId(), url.GetHostId(), entryType ) );
+    pRdirEntry.reset( new RedirectEntry( pUrl.GetLocation(), url.GetLocation(), entryType ) );
 
     if( pUrl.GetLocation() != url.GetLocation() )
     {

src/XrdCl/XrdClFileStateHandler.cc
@@ -1418,11 +1418,14 @@ namespace XrdCl
     //--------------------------------------------------------------------------
     if( !status->IsOK() && status->code == errRedirect && pFollowRedirects )
     {
-      static const std::string root = "root", xroot = "xroot", file = "file";
+      static const std::string root  = "root",  xroot  = "xroot", file = "file",
+                               roots = "roots", xroots = "xroots";
       std::string msg = status->GetErrorMessage();
-      if( !msg.compare( 0, root.size(), root ) ||
-          !msg.compare( 0, xroot.size(), xroot ) ||
-          !msg.compare( 0, file.size(), file ) )
+      if( !msg.compare( 0, root.size(),   root )  ||
+          !msg.compare( 0, xroot.size(),  xroot ) ||
+          !msg.compare( 0, file.size(),   file )  ||
+          !msg.compare( 0, roots.size(),  roots ) ||
+          !msg.compare( 0, xroots.size(), xroots ) )
       {
         OnStateRedirection( msg, message, userHandler, sendParams );
         return;

src/XrdCl/XrdClFileSystem.cc
@@ -1725,7 +1725,10 @@ namespace XrdCl
 
       for( uint32_t i = 0; i < locations->GetSize(); ++i )
       {
-        fs = new FileSystem( locations->At(i).GetAddress() );
+        URL locationURL( locations->At(i).GetAddress() );
+        // make sure the original protocol is preserved (root vs roots)
+        locationURL.SetProtocol( pImpl->pUrl->GetProtocol() );
+        fs = new FileSystem( locationURL );
         st = fs->DirList( path, flags, currentResp, timeout );
         if( !st.IsOK() )
         {

commit a3a267cb0d65756b1be15b076aecda84480da5a4

[XrdCl] Make sure on redirect protcol is not changed to file://.
Fix regression introduced in 95e2d5cb90e26cadd2546f7c9c2c4eeb3727ed56.

src/XrdCl/XrdClXRootDMsgHandler.cc
@@ -653,7 +653,9 @@ namespace XrdCl
         //----------------------------------------------------------------------
         // Make sure we don't change the protocol by accident (root vs roots)
         //----------------------------------------------------------------------
-        newUrl.SetProtocol( pUrl.GetProtocol() );
+        if( ( pUrl.GetProtocol() == "roots" || pUrl.GetProtocol() == "xroots" ) &&
+            ( newUrl.GetProtocol() == "root" || newUrl.GetProtocol() == "xroot" ) )
+          newUrl.SetProtocol( "roots" );
 
         //----------------------------------------------------------------------
         // Send the request to the new location