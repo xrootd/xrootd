2799ee5d095d98e3f112590318a7fd15207c02fc
Brian Bockelman <bbockelm@cse.unl.edu>
2020-03-15 23:35:27 -0500

    Add some comments about how the performance markers work in HTTP-TPC.

--- src/XrdTpc/XrdTpcTPC.hh
+++ src/XrdTpc/XrdTpcTPC.hh
@@ -42,6 +42,10 @@ private:
     int DetermineXferSize(CURL *curl, XrdHttpExtReq &req, TPC::State &state,
                           bool &success);
 
+    // Send a 'performance marker' back to the TPC client, informing it of our
+    // progress.  The TPC client will use this information to determine whether
+    // the transfer is making sufficient progress and/or other monitoring info
+    // (such as whether the transfer is happening over IPv4, IPv6, or both).
     int SendPerfMarker(XrdHttpExtReq &req, TPC::State &state);
     int SendPerfMarker(XrdHttpExtReq &req, std::vector<State*> &state,
         off_t bytes_transferred);
