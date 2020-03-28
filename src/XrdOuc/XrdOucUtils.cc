98b8c4f9f6a1f4a1ef88371fef815997151cd820
Andrew Hanushevsky <abh@stanford.edu>
2020-03-27 22:39:45 -0700

    [Server] Correct userid sanitization code.

--- src/XrdOuc/XrdOucUtils.cc
+++ src/XrdOuc/XrdOucUtils.cc
@@ -645,6 +645,9 @@ void XrdOucUtils::Sanitize(char *str, char subc)
 //
    if (*str)
       {if (*str == '-') *str = subc;
+          else if (*str == ' ') *str = subc;
+       char *blank = rindex(str, ' ');
+       if (blank) while(*blank == ' ') *blank-- = 0;
        str++;
        while(*str)
             {if (!isalnum(*str) && index("_-.", *str) == 0) *str = subc;
