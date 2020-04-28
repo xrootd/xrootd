87bdf6285ebbc1ac937d6a2d593c8b9d1732237e
Brian Bockelman <bbockelm@cse.unl.edu>
2020-04-27 20:48:08 -0500

    Ifdef out newer OpenSSL functions.
    Also add a few comments about the usage.

--- src/XrdHttp/XrdHttpProtocol.cc
+++ src/XrdHttp/XrdHttpProtocol.cc
@@ -638,9 +638,17 @@ int XrdHttpProtocol::Process(XrdLink *lp) // We ignore the argument here
           BIO_set_nbio(sbio, 1);
           ssl = SSL_new(sslctx);
 
+          // On newer versions of OpenSSL, we use the periodically
+          // updated store `verify_store` with the `x509_store_lock`
+          // held; older OpenSSL versions are missing `SSL_set1_*_cert_store`,
+          // so we simply rely on the global one in SSL_CTX.  The latter
+          // doesn't refresh the verify store, which causes issues with some
+          // plugins.
+#if OPENSSL_VERSION_NUMBER >= 0x010100000L
           XrdSysRWLockHelper scopedLock(x509_store_lock, true);
           SSL_set1_verify_cert_store(ssl, verify_store);
           SSL_set1_chain_cert_store(ssl, verify_store);
+#endif
 
         }
 
@@ -1631,6 +1639,7 @@ extern "C" int verify_callback(int ok, X509_STORE_CTX * store) {
 }
 
 /// Initialization of the ssl security
+//  Only scheduled on sufficiently new OpenSSL.
 
 class XrdCertStoreJob : XrdJob
 {
@@ -1750,6 +1759,10 @@ int XrdHttpProtocol::InitSecurity() {
     }
   }
 
+  // Initialize a store for use on individual SSL objects and schedule
+  // a periodic update.  These separate stores are only usable on versions
+  // of OpenSSL 1.1.0 or later.
+#if OPENSSL_VERSION_NUMBER >= 0x010100000L
   {
     XrdSysRWLockHelper scopedLock(x509_store_lock, false);
     verify_store = PrepareStore();
@@ -1757,8 +1770,8 @@ int XrdHttpProtocol::InitSecurity() {
       exit(1);
     }
   }
-
   new XrdCertStoreJob(Sched, 3600);
+#endif
 
   // Use default cipherlist filter if none is provided
 #if OPENSSL_VERSION_NUMBER >= 0x10002000L
