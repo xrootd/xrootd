f6723e003c701dd4bcbcee250f6b5ca9a6f9c3c5
Michal Simon <michal.simon@cern.ch>
2020-05-13 14:32:02 +0200

    [XrdCl] xrdcp: don't create unwanted dir if --recursive switch was used
    and the source is a file. closes #1193

--- src/XrdCl/XrdClCopy.cc
+++ src/XrdCl/XrdClCopy.cc
@@ -707,6 +707,7 @@ int main( int argc, char **argv )
   // If we're doing remote recursive copy, chain all the files (if it's a
   // directory)
   //----------------------------------------------------------------------------
+  bool srcIsDir = config.srcFile->Protocol == XrdCpFile::isDir;
   if( config.Want( XrdCpConfig::DoRecurse ) &&
       config.srcFile->Protocol == XrdCpFile::isXroot )
   {
@@ -717,6 +718,7 @@ int main( int argc, char **argv )
     XRootDStatus st = fs->Stat( source.GetPath(), statInfo );
     if( st.IsOK() && statInfo->TestFlags( StatInfo::IsDir ) )
     {
+      srcIsDir = true;
       //------------------------------------------------------------------------
       // Recursively index the remote directory
       //------------------------------------------------------------------------
@@ -778,7 +780,7 @@ int main( int argc, char **argv )
     //--------------------------------------------------------------------------
     std::string target = dest;
     // if this is a recursive copy make sure we preserve the directory structure
-    if( config.Want( XrdCpConfig::DoRecurse ) )
+    if( config.Want( XrdCpConfig::DoRecurse ) && srcIsDir )
     {
       // get the source directory
       std::string srcDir( sourceFile->Path, sourceFile->Doff );
