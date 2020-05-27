d28e9ae3adb989edca4b896887e0cc08aaf64453
Michal Simon <michal.simon@cern.ch>
2020-05-27 17:40:41 +0200

    [XrdVoms] Rename the plug-in to XrdVoms.
    - for compatibility create a symlink XrdSecGsiVOMS

--- src/XrdVoms.cmake
+++ src/XrdVoms.cmake
@@ -5,22 +5,23 @@
 
 include_directories( ${VOMS_INCLUDE_DIR} )
 
+set( LIB_XRD_VOMS             XrdVoms-${PLUGIN_VERSION} )
 set( LIB_XRD_SEC_GSI_VOMS     XrdSecgsiVOMS-${PLUGIN_VERSION} )
 
 add_library(
-   ${LIB_XRD_SEC_GSI_VOMS}
+   ${LIB_XRD_VOMS}
    MODULE
    ${CMAKE_SOURCE_DIR}/src/XrdVoms/XrdVomsFun.cc
    ${CMAKE_SOURCE_DIR}/src/XrdVoms/XrdVomsgsi.cc
    ${CMAKE_SOURCE_DIR}/src/XrdVoms/XrdVomsHttp.cc )
 
 target_link_libraries(
-   ${LIB_XRD_SEC_GSI_VOMS}
+   ${LIB_XRD_VOMS}
    XrdCrypto
    ${VOMS_LIBRARIES} )
 
 set_target_properties(
-   ${LIB_XRD_SEC_GSI_VOMS}
+   ${LIB_XRD_VOMS}
    PROPERTIES
    INTERFACE_LINK_LIBRARIES ""
    LINK_INTERFACE_LIBRARIES "" )
@@ -29,7 +30,7 @@ set_target_properties(
 # Install
 #-------------------------------------------------------------------------------
 install(
-   TARGETS ${LIB_XRD_SEC_GSI_VOMS}
+   TARGETS ${LIB_XRD_VOMS}
    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
 
@@ -37,4 +38,10 @@ install(
   FILES
   ${PROJECT_SOURCE_DIR}/docs/man/libXrdSecgsiVOMS.1
   DESTINATION ${CMAKE_INSTALL_MANDIR}/man1 )
+
+install(
+  CODE "
+    EXECUTE_PROCESS(
+      COMMAND ln -sf ${LIB_XRD_VOMS} ${LIB_XRD_SEC_GSI_VOMS}
+      WORKING_DIRECTORY \$ENV{DESTDIR}/${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR} )" )
 
