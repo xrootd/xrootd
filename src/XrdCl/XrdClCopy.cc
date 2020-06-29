xrootd/xrootd

commit f6723e003c701dd4bcbcee250f6b5ca9a6f9c3c5

[XrdCl] xrdcp: don't create unwanted dir if --recursive switch was used
and the source is a file. closes #1193

src/XrdCl/XrdClCopy.cc
@@ -707,6 +707,7 @@ int main( int argc, char **argv )
   // If we're doing remote recursive copy, chain all the files (if it's a
   // directory)
   //----------------------------------------------------------------------------
+  bool srcIsDir = config.srcFile->Protocol == XrdCpFile::isDir;
   if( config.Want( XrdCpConfig::DoRecurse ) &&
       config.srcFile->Protocol == XrdCpFile::isXroot )
   {
@@ -717,6 +718,7 @@ int main( int argc, char **argv )
     XRootDStatus st = fs->Stat( source.GetPath(), statInfo );
     if( st.IsOK() && statInfo->TestFlags( StatInfo::IsDir ) )
     {
+      srcIsDir = true;
       //------------------------------------------------------------------------
       // Recursively index the remote directory
       //------------------------------------------------------------------------
@@ -778,7 +780,7 @@ int main( int argc, char **argv )
     //--------------------------------------------------------------------------
     std::string target = dest;
     // if this is a recursive copy make sure we preserve the directory structure
-    if( config.Want( XrdCpConfig::DoRecurse ) )
+    if( config.Want( XrdCpConfig::DoRecurse ) && srcIsDir )
     {
       // get the source directory
       std::string srcDir( sourceFile->Path, sourceFile->Doff );

commit 76234c054054789986c9e67185e0e7855057fa99

[XrdCl] Fix regression in recursive copy (introduced in f6723e00).
Fixes #1227

src/XrdCl/XrdClCopy.cc
@@ -707,7 +707,6 @@ int main( int argc, char **argv )
   // If we're doing remote recursive copy, chain all the files (if it's a
   // directory)
   //----------------------------------------------------------------------------
-  bool srcIsDir = config.srcFile->Protocol == XrdCpFile::isDir;
   if( config.Want( XrdCpConfig::DoRecurse ) &&
       config.srcFile->Protocol == XrdCpFile::isXroot )
   {
@@ -718,7 +717,6 @@ int main( int argc, char **argv )
     XRootDStatus st = fs->Stat( source.GetPath(), statInfo );
     if( st.IsOK() && statInfo->TestFlags( StatInfo::IsDir ) )
     {
-      srcIsDir = true;
       //------------------------------------------------------------------------
       // Recursively index the remote directory
       //------------------------------------------------------------------------
@@ -779,6 +777,9 @@ int main( int argc, char **argv )
     // Set up the job
     //--------------------------------------------------------------------------
     std::string target = dest;
+
+    // if the source was a file Dlen and Doff overlap
+    bool srcIsDir = std::string( sourceFile->Path ).size() == size_t( sourceFile->Doff + sourceFile->Dlen );
     // if this is a recursive copy make sure we preserve the directory structure
     if( config.Want( XrdCpConfig::DoRecurse ) && srcIsDir )
     {