c8f3acd7761c0efaf74bacae4e640052ca4e67bf
Michal Simon <michal.simon@cern.ch>
2020-11-26 21:09:51 +0100

    [XrdCl] Implement OpenFile declarative operation.

--- src/XrdCl/XrdClZipOperations.hh
+++ src/XrdCl/XrdClZipOperations.hh
@@ -137,6 +137,143 @@ namespace XrdCl
       }
   };
   typedef OpenArchiveImpl<false> OpenArchive;
+
+  //----------------------------------------------------------------------------
+  //! OpenFile operation (@see ZipOperation)
+  //----------------------------------------------------------------------------
+  template<bool HasHndl>
+  class OpenFileImpl: public ZipOperation<OpenFileImpl, HasHndl, Resp<void>,
+      Arg<std::string>>
+  {
+    public:
+
+      //------------------------------------------------------------------------
+      //! Inherit constructors from FileOperation (@see FileOperation)
+      //------------------------------------------------------------------------
+      using ZipOperation<OpenFileImpl, HasHndl, Resp<void>, Arg<std::string>>::ZipOperation;
+
+      //------------------------------------------------------------------------
+      //! Argument indexes in the args tuple
+      //------------------------------------------------------------------------
+      enum { FnArg, };
+
+      //------------------------------------------------------------------------
+      //! @return : name of the operation (@see Operation)
+      //------------------------------------------------------------------------
+      std::string ToString()
+      {
+        return "OpenFile";
+      }
+
+    protected:
+
+      //------------------------------------------------------------------------
+      //! RunImpl operation (@see Operation)
+      //!
+      //! @param params :  container with parameters forwarded from
+      //!                  previous operation
+      //! @return       :  status of the operation
+      //------------------------------------------------------------------------
+      XRootDStatus RunImpl( uint16_t pipelineTimeout )
+      {
+        try
+        {
+          std::string fn      = std::get<FnArg>( this->args ).Get();
+          uint16_t    timeout = pipelineTimeout < this->timeout ?
+                                     pipelineTimeout : this->timeout;
+          return this->zip->OpenFile( fn, OpenFlags::None, 0, 0, this->handler.get(), timeout );
+        }
+        catch( const PipelineException& ex )
+        {
+          return ex.GetError();
+        }
+        catch( const std::exception& ex )
+        {
+          return XRootDStatus( stError, ex.what() );
+        }
+      }
+  };
+
+  //----------------------------------------------------------------------------
+  //! Factory for creating OpenFileImpl objects
+  //----------------------------------------------------------------------------
+  template<typename ZIP>
+  inline OpenFileImpl<false> OpenFile( ZIP &zip, Arg<std::string> fn )
+  {
+    return OpenFileImpl<false>( zip, std::move( fn ) );
+  }
+
+  //----------------------------------------------------------------------------
+  //! OpenFile operation (@see ZipOperation)
+  //----------------------------------------------------------------------------
+  template<bool HasHndl>
+  class OpenFileImpl2: public ZipOperation<OpenFileImpl2, HasHndl, Resp<void>,
+      Arg<std::string>, Arg<OpenFlags::Flags>, Arg<uint64_t>, Arg<uint32_t>>
+  {
+    public:
+
+      //------------------------------------------------------------------------
+      //! Inherit constructors from FileOperation (@see FileOperation)
+      //------------------------------------------------------------------------
+      using ZipOperation<OpenFileImpl2, HasHndl, Resp<void>, Arg<std::string>,
+          Arg<OpenFlags::Flags>, Arg<uint64_t>, Arg<uint32_t>>::ZipOperation;
+
+      //------------------------------------------------------------------------
+      //! Argument indexes in the args tuple
+      //------------------------------------------------------------------------
+      enum { FnArg, FlagsArg, SizeArg, Crc32Arg };
+
+      //------------------------------------------------------------------------
+      //! @return : name of the operation (@see Operation)
+      //------------------------------------------------------------------------
+      std::string ToString()
+      {
+        return "OpenFile";
+      }
+
+    protected:
+
+      //------------------------------------------------------------------------
+      //! RunImpl operation (@see Operation)
+      //!
+      //! @param params :  container with parameters forwarded from
+      //!                  previous operation
+      //! @return       :  status of the operation
+      //------------------------------------------------------------------------
+      XRootDStatus RunImpl( uint16_t pipelineTimeout )
+      {
+        try
+        {
+          std::string      fn      = std::get<FnArg>( this->args ).Get();
+          OpenFlags::Flags flags   = std::get<FlagsArg>( this->args ).Get();
+          uint64_t         size    = std::get<SizeArg>( this->args ).Get();
+          uint32_t         crc32   = std::get<Crc32Arg>( this->args ).Get();
+          uint16_t         timeout = pipelineTimeout < this->timeout ?
+                                     pipelineTimeout : this->timeout;
+          return this->zip->OpenFile( fn, flags, size, crc32, this->handler.get(), timeout );
+        }
+        catch( const PipelineException& ex )
+        {
+          return ex.GetError();
+        }
+        catch( const std::exception& ex )
+        {
+          return XRootDStatus( stError, ex.what() );
+        }
+      }
+  };
+
+  //----------------------------------------------------------------------------
+  //! Factory for creating OpenFileImpl2 objects
+  //----------------------------------------------------------------------------
+  template<typename ZIP>
+  inline OpenFileImpl2<false> OpenFile( ZIP &zip, Arg<std::string> fn,
+      Arg<OpenFlags::Flags> flags, Arg<uint64_t> size, Arg<uint32_t> crc32 )
+  {
+    return OpenFileImpl2<false>( zip, std::move( fn ), std::move( flags ),
+        std::move( size ), std::move( crc32 ) );
+  }
+
 }
 
 #endif /* SRC_XRDCL_XRDCLZIPOPERATIONS_HH_ */
