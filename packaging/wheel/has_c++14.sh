e1eea3882dbc1c686976ad331ea60775f87eaebb
Michal Simon <michal.simon@cern.ch>
2021-07-29 14:09:45 +0200

    [PyPI] Adjustements to has_c++14.sh & setup.py.

--- packaging/wheel/has_c++14.sh
+++ packaging/wheel/has_c++14.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir has_c++144.tmp
-cp TestCXX14.txt has_c++144.tmp/CMakeLists.txt
-cd has_c++144.tmp
+mkdir has_c++14.tmp
+cp packaging/wheel/TestCXX14.txt has_c++14.tmp/CMakeLists.txt
+cd has_c++14.tmp
 mkdir build
 cd build
 cmake3 ..
 has_cxx14=$?
 cd ../..
-rm -rf has_c++144.tmp
-exit has_cxx14
+rm -rf has_c++14.tmp
+exit $has_cxx14
