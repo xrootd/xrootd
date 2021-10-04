a14921ecd9c5522f524fbf8dbd65f38f9e6cd3da
Michal Simon <michal.simon@cern.ch>
2021-10-04 13:05:28 +0200

    [CMake] Build openssl3 files only if WITH_OPENSSL3=TRUE.

--- src/XrdCrypto.cmake
+++ src/XrdCrypto.cmake
@@ -15,24 +15,43 @@ set( XRD_CRYPTO_SOVERSION 2 )
 set( XRD_CRYPTO_LITE_VERSION   2.0.0 )
 set( XRD_CRYPTO_LITE_SOVERSION 2 )
 
+if( WITH_OPENSSL3 )
+  set( XrdCryptoSources
+    XrdCrypto/XrdCryptoAux.cc                 XrdCrypto/XrdCryptoAux.hh
+    XrdCrypto/XrdCryptoTrace.hh
+    XrdCrypto/XrdCryptoBasic.cc               XrdCrypto/XrdCryptoBasic.hh
+    XrdCrypto/XrdCryptoCipher.cc              XrdCrypto/XrdCryptoCipher.hh
+    XrdCrypto/XrdCryptoFactory.cc             XrdCrypto/XrdCryptoFactory.hh
+    XrdCrypto/XrdCryptoMsgDigest.cc           XrdCrypto/XrdCryptoMsgDigest.hh
+    XrdCrypto/XrdCryptoRSA.cc                 XrdCrypto/XrdCryptoRSA.hh
+    XrdCrypto/XrdCryptoX509.cc                XrdCrypto/XrdCryptoX509.hh
+    XrdCrypto/openssl3/XrdCryptoX509Chain.cc  XrdCrypto/XrdCryptoX509Chain.hh
+    XrdCrypto/XrdCryptoX509Crl.cc             XrdCrypto/XrdCryptoX509Crl.hh
+    XrdCrypto/XrdCryptoX509Req.cc             XrdCrypto/XrdCryptoX509Req.hh
+    XrdCrypto/XrdCryptogsiX509Chain.cc        XrdCrypto/XrdCryptogsiX509Chain.hh )
+else()
+  set( XrdCryptoSources
+    XrdCrypto/XrdCryptoAux.cc               XrdCrypto/XrdCryptoAux.hh
+    XrdCrypto/XrdCryptoTrace.hh
+    XrdCrypto/XrdCryptoBasic.cc             XrdCrypto/XrdCryptoBasic.hh
+    XrdCrypto/XrdCryptoCipher.cc            XrdCrypto/XrdCryptoCipher.hh
+    XrdCrypto/XrdCryptoFactory.cc           XrdCrypto/XrdCryptoFactory.hh
+    XrdCrypto/XrdCryptoMsgDigest.cc         XrdCrypto/XrdCryptoMsgDigest.hh
+    XrdCrypto/XrdCryptoRSA.cc               XrdCrypto/XrdCryptoRSA.hh
+    XrdCrypto/XrdCryptoX509.cc              XrdCrypto/XrdCryptoX509.hh
+    XrdCrypto/XrdCryptoX509Chain.cc         XrdCrypto/XrdCryptoX509Chain.hh
+    XrdCrypto/XrdCryptoX509Crl.cc           XrdCrypto/XrdCryptoX509Crl.hh
+    XrdCrypto/XrdCryptoX509Req.cc           XrdCrypto/XrdCryptoX509Req.hh
+    XrdCrypto/XrdCryptogsiX509Chain.cc      XrdCrypto/XrdCryptogsiX509Chain.hh )
+endif()
+
 #-------------------------------------------------------------------------------
 # The XrdCrypto library
 #-------------------------------------------------------------------------------
 add_library(
   XrdCrypto
   SHARED
-  XrdCrypto/XrdCryptoAux.cc               XrdCrypto/XrdCryptoAux.hh
-  XrdCrypto/XrdCryptoTrace.hh
-  XrdCrypto/XrdCryptoBasic.cc             XrdCrypto/XrdCryptoBasic.hh
-  XrdCrypto/XrdCryptoCipher.cc            XrdCrypto/XrdCryptoCipher.hh
-  XrdCrypto/XrdCryptoFactory.cc           XrdCrypto/XrdCryptoFactory.hh
-  XrdCrypto/XrdCryptoMsgDigest.cc         XrdCrypto/XrdCryptoMsgDigest.hh
-  XrdCrypto/XrdCryptoRSA.cc               XrdCrypto/XrdCryptoRSA.hh
-  XrdCrypto/XrdCryptoX509.cc              XrdCrypto/XrdCryptoX509.hh
-  XrdCrypto/XrdCryptoX509Chain.cc         XrdCrypto/XrdCryptoX509Chain.hh
-  XrdCrypto/XrdCryptoX509Crl.cc           XrdCrypto/XrdCryptoX509Crl.hh
-  XrdCrypto/XrdCryptoX509Req.cc           XrdCrypto/XrdCryptoX509Req.hh
-  XrdCrypto/XrdCryptogsiX509Chain.cc      XrdCrypto/XrdCryptogsiX509Chain.hh )
+  ${XrdCryptoSources} )
 
 target_link_libraries(
   XrdCrypto
@@ -50,6 +69,17 @@ set_target_properties(
 #-------------------------------------------------------------------------------
 # The XrdCryptoLite library
 #-------------------------------------------------------------------------------
+
+if( WITH_OPENSSL3 )
+  set( XrdCryptoLiteSources
+       XrdCrypto/XrdCryptoLite.cc   XrdCrypto/XrdCryptoLite.hh
+       openssl/XrdCrypto/XrdCryptoLite_bf32.cc )
+else()
+  set( XrdCryptoLiteSources
+       XrdCrypto/XrdCryptoLite.cc   XrdCrypto/XrdCryptoLite.hh
+       XrdCrypto/XrdCryptoLite_bf32.cc )
+endif()
+
 add_library(
   XrdCryptoLite
   SHARED
@@ -81,19 +111,36 @@ set_target_properties(
 if( BUILD_CRYPTO )
   include_directories( ${OPENSSL_INCLUDE_DIR} )
 
+  if( WITH_OPENSSL3 )
+    set( XrdCryptosslSources 
+         XrdCrypto/openssl3/XrdCryptosslAux.cc       XrdCrypto/XrdCryptosslAux.hh
+         XrdCrypto/openssl3/XrdCryptosslgsiAux.cc
+         XrdCrypto/openssl3/XrdCryptosslCipher.cc    XrdCrypto/openssl3/XrdCryptosslCipher.hh
+         XrdCrypto/XrdCryptosslMsgDigest.cc          XrdCrypto/XrdCryptosslMsgDigest.hh
+         XrdCrypto/openssl3/XrdCryptosslRSA.cc       XrdCrypto/XrdCryptosslRSA.hh
+         XrdCrypto/openssl3/XrdCryptosslX509.cc      XrdCrypto/XrdCryptosslX509.hh
+         XrdCrypto/XrdCryptosslX509Crl.cc            XrdCrypto/XrdCryptosslX509Crl.hh
+         XrdCrypto/XrdCryptosslX509Req.cc            XrdCrypto/XrdCryptosslX509Req.hh
+         XrdCrypto/XrdCryptosslTrace.hh
+         XrdCrypto/openssl3/XrdCryptosslFactory.cc   XrdCrypto/XrdCryptosslFactory.hh )
+  else()
+    set( XrdCryptosslSources
+         XrdCrypto/XrdCryptosslAux.cc            XrdCrypto/XrdCryptosslAux.hh
+         XrdCrypto/XrdCryptosslgsiAux.cc
+         XrdCrypto/XrdCryptosslCipher.cc         XrdCrypto/XrdCryptosslCipher.hh
+         XrdCrypto/XrdCryptosslMsgDigest.cc      XrdCrypto/XrdCryptosslMsgDigest.hh
+         XrdCrypto/XrdCryptosslRSA.cc            XrdCrypto/XrdCryptosslRSA.hh
+         XrdCrypto/XrdCryptosslX509.cc           XrdCrypto/XrdCryptosslX509.hh
+         XrdCrypto/XrdCryptosslX509Crl.cc        XrdCrypto/XrdCryptosslX509Crl.hh
+         XrdCrypto/XrdCryptosslX509Req.cc        XrdCrypto/XrdCryptosslX509Req.hh
+         XrdCrypto/XrdCryptosslTrace.hh
+         XrdCrypto/XrdCryptosslFactory.cc        XrdCrypto/XrdCryptosslFactory.hh )
+  endif()
+
   add_library(
     ${LIB_XRD_CRYPTOSSL}
     MODULE
-    XrdCrypto/XrdCryptosslAux.cc            XrdCrypto/XrdCryptosslAux.hh
-    XrdCrypto/XrdCryptosslgsiAux.cc
-    XrdCrypto/XrdCryptosslCipher.cc         XrdCrypto/XrdCryptosslCipher.hh
-    XrdCrypto/XrdCryptosslMsgDigest.cc      XrdCrypto/XrdCryptosslMsgDigest.hh
-    XrdCrypto/XrdCryptosslRSA.cc            XrdCrypto/XrdCryptosslRSA.hh
-    XrdCrypto/XrdCryptosslX509.cc           XrdCrypto/XrdCryptosslX509.hh
-    XrdCrypto/XrdCryptosslX509Crl.cc        XrdCrypto/XrdCryptosslX509Crl.hh
-    XrdCrypto/XrdCryptosslX509Req.cc        XrdCrypto/XrdCryptosslX509Req.hh
-    XrdCrypto/XrdCryptosslTrace.hh
-    XrdCrypto/XrdCryptosslFactory.cc        XrdCrypto/XrdCryptosslFactory.hh )
+    ${XrdCryptosslSources} )
 
   target_link_libraries(
     ${LIB_XRD_CRYPTOSSL}
