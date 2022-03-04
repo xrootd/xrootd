52956e54a877390d1ee21e57b8974916d31f692e
Mattias Ellert <mattias.ellert@physics.uu.se>
2022-03-04 06:10:09 +0100

    [TLS] Fix incomplete openssl3 port

--- src/XrdTls/openssl3/XrdTlsContext.cc
+++ src/XrdTls/openssl3/XrdTlsContext.cc
@@ -521,7 +521,7 @@ int VerCB(int aOK, X509_STORE_CTX *x509P)
 
        snprintf(info, sizeof(info), "Error %d at depth %d [%s]", err, depth,
                                     X509_verify_cert_error_string(err));
-       XrdTlsGlobal::msgCB("CertVerify", info, false);
+       XrdTls::Emsg("CertVerify:", info, true);
       }
 
    return aOK;
@@ -1012,8 +1012,8 @@ bool XrdTlsContext::SetCrlRefresh(int refsec)
 // Check if there is anything that is refreshable
 //
    if (!x509Verify())
-      {XrdTlsGlobal::msgCB("CrlRefresh:",
-                           "No cert information exists to refresh!", false);
+      {XrdTls::Emsg("CrlRefresh:",
+                    "No cert information exists to refresh!", false);
        return false;
       }
 
@@ -1047,8 +1047,8 @@ bool XrdTlsContext::SetCrlRefresh(int refsec)
 // Older version are too difficult to deal with. Issue a message if this
 // feature is being enabled on an old version.
 //
-   XrdTlsGlobal::msgCB("CrlRefresh:", "Refreshing CRLs only supported in "
-                       "OpenSSL version >= 1.02; CRL refresh disabled!", false);
+   XrdTls::Emsg("CrlRefresh:", "Refreshing CRLs only supported in "
+                "OpenSSL version >= 1.02; CRL refresh disabled!", false);
    return false;
 #endif
 }
