1bcee031826d8ac6b4ded2c30788cba8b369cb30
Sergey Fedorov <vital.had@gmail.com>
2022-08-21 23:01:27 +0800

    xrootd: fix missing byteswap.h error on MacOS build with gcc

--- src/XrdOssCsi/XrdOssCsiTagstoreFile.hh
+++ src/XrdOssCsi/XrdOssCsiTagstoreFile.hh
@@ -37,7 +37,19 @@
 
 #include <memory>
 #include <mutex>
+
+#if defined(__GLIBC__) || defined(__BIONIC__) || defined(__CYGWIN__)
 #include <byteswap.h>
+#elif defined(__APPLE__)
+// Make sure that byte swap functions are not already defined.
+#if !defined(bswap_16)
+// Mac OS X / Darwin features
+#include <libkern/OSByteOrder.h>
+#define bswap_16(x) OSSwapInt16(x)
+#define bswap_32(x) OSSwapInt32(x)
+#define bswap_64(x) OSSwapInt64(x)
+#endif
+#endif
 
 class XrdOssCsiTagstoreFile : public XrdOssCsiTagstore
 {
