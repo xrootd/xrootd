14331c3fd43104b566212a615b9eb49ba63d0388
Brian Bockelman <bbockelman@morgridge.org>
2022-10-15 13:32:36 -0500

    Implement XrdSciTokensHelper interface for macaroons.
    In order to use ZTN with a token library, the library must provide
    a XrdSciTokensHelper object.  This implements the interface inside
    the Macaroons::Authz object, allowing ZTN authentication to be
    utilized with a macaroon-based token.
    
    This can be enabled with the following configuration directive:
    ```
    sec.protocol -tokenlib libXrdMacaroons-5.so
    ```

--- src/XrdMacaroons/XrdMacaroonsAuthz.hh
+++ src/XrdMacaroons/XrdMacaroonsAuthz.hh
@@ -1,5 +1,6 @@
 
 #include "XrdAcc/XrdAccAuthorize.hh"
+#include "XrdSciTokens/XrdSciTokensHelper.hh"
 #include "XrdSys/XrdSysError.hh"
 
 
@@ -8,7 +9,7 @@ class XrdSysError;
 namespace Macaroons
 {
 
-class Authz : public XrdAccAuthorize
+class Authz final : public XrdAccAuthorize, public XrdSciTokensHelper
 {
 public:
     Authz(XrdSysLogger *lp, const char *parms, XrdAccAuthorize *chain);
@@ -18,20 +19,31 @@ public:
     virtual XrdAccPrivs Access(const XrdSecEntity     *Entity,
                                const char             *path,
                                const Access_Operation  oper,
-                                     XrdOucEnv        *env);
+                                     XrdOucEnv        *env) override;
+
+    // Do a minimal validation that this is a non-expired token; used
+    // for session tokens.
+    virtual bool Validate(const char   *token,
+                          std::string  &emsg,
+                          long long    *expT,
+                          XrdSecEntity *entP) override;
 
     virtual int Audit(const int accok, const XrdSecEntity *Entity,
                       const char *path, const Access_Operation oper,
-                      XrdOucEnv *Env)
+                      XrdOucEnv *Env) override
     {
         return 0;
     }
 
     virtual int Test(const XrdAccPrivs priv,
-                     const Access_Operation oper)
+                     const Access_Operation oper) override
     {
         return 0;
     }
+
+    // Macaroons don't have a concept off an "issuers"; return an empty
+    // list.
+    virtual Issuers IssuerList() {return Issuers();}
 
 private:
     XrdAccPrivs OnMissing(const XrdSecEntity     *Entity,
