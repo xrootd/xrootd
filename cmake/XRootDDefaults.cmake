xrootd/xrootd

commit ff67c2a0e0357bea69dcb4884abca8324595fa50

[CMake] add ENABLE_ switch for scitokens and macaroons, closes #1728.

cmake/XRootDDefaults.cmake
@@ -28,5 +28,7 @@ option( ENABLE_XRDEC     "Enable erasure coding component."                     
 option( ENABLE_ASAN      "Enable adress sanitizer."                                       FALSE )
 option( ENABLE_TSAN      "Enable thread sanitizer."                                       FALSE )
 option( ENABLE_XRDCLHTTP "Enable xrdcl-http plugin."                                      TRUE )
+option( ENABLE_SCITOKENS "Enable SciTokens plugin."                                       TRUE )
+option( ENABLE_MACAROONS "Enable Macaroons plugin."                                       TRUE )
 option( FORCE_ENABLED    "Fail build if enabled components cannot be built."              FALSE )
 define_default( XRD_PYTHON_REQ_VERSION 3 )

cmake/XRootDFindLibs.cmake
@@ -119,25 +119,41 @@ check_function_exists( curl_multi_wait HAVE_CURL_MULTI_WAIT )
 compiler_define_if_found( HAVE_CURL_MULTI_WAIT HAVE_CURL_MULTI_WAIT )
 endif()
 
-find_package( Macaroons )
-
-if( NOT MacOSX )
-  include(FindPkgConfig REQUIRED)
-  pkg_check_modules(JSON json-c)
-  pkg_check_modules(UUID REQUIRED uuid)
+if( ENABLE_MACAROONS )
+  if( FORCE_ENABLED )
+    find_package( Macaroons REQUIRED )
+  else()
+    find_package( Macaroons )
+  endif()
+  if( NOT MacOSX )
+    include(FindPkgConfig REQUIRED)
+    if( FORCE_ENABLED )
+      pkg_check_modules(JSON REQUIRED json-c)
+      pkg_check_modules(UUID REQUIRED uuid)
+    else()
+      pkg_check_modules(JSON json-c)
+      pkg_check_modules(UUID uuid)
+    endif()
+  endif()
+  if( MACAROONS_FOUND AND JSON_FOUND AND UUID_FOUND )
+    set( BUILD_MACAROONS TRUE )
+  else()
+    set( BUILD_MACAROONS FALSE )
+  endif()
 endif()
 
-if( MACAROONS_FOUND AND JSON_FOUND AND UUID_FOUND )
-  set( BUILD_MACAROONS TRUE )
-else()
-  set( BUILD_MACAROONS FALSE )
-endif()
 
-find_package( SciTokensCpp )
-if( SCITOKENSCPP_FOUND )
-  set( BUILD_SCITOKENS TRUE )
-else()
-  set( BUILD_SCITOKENS FALSE )
+if( ENABLE_SCITOKENS )
+  if( FORCE_ENABLED )
+    find_package( SciTokensCpp REQUIRED )
+  else()
+    find_package( SciTokensCpp )
+  endif()
+  if( SCITOKENSCPP_FOUND )
+    set( BUILD_SCITOKENS TRUE )
+  else()
+    set( BUILD_SCITOKENS FALSE )
+  endif()
 endif()
 
 if( ENABLE_XRDEC )

commit 3a099833344363cdd23e23f9e4e3499a3cc3c570

[CMake] Make switches for server-only options dependent on XRCL_ONLY=FALSE
Fixes: ff67c2a0
Closes: #1804

cmake/XRootDDefaults.cmake
@@ -9,6 +9,8 @@ if( "${CMAKE_BUILD_TYPE}" STREQUAL "" )
   endif()
 endif()
 
+include( CMakeDependentOption )
+
 define_default( PLUGIN_VERSION    5 )
 option( ENABLE_FUSE      "Enable the fuse filesystem driver if possible."                 TRUE )
 option( ENABLE_CRYPTO    "Enable the OpenSSL cryprography support."                       TRUE )
@@ -28,7 +30,7 @@ option( ENABLE_XRDEC     "Enable erasure coding component."                     
 option( ENABLE_ASAN      "Enable adress sanitizer."                                       FALSE )
 option( ENABLE_TSAN      "Enable thread sanitizer."                                       FALSE )
 option( ENABLE_XRDCLHTTP "Enable xrdcl-http plugin."                                      TRUE )
-option( ENABLE_SCITOKENS "Enable SciTokens plugin."                                       TRUE )
-option( ENABLE_MACAROONS "Enable Macaroons plugin."                                       TRUE )
+cmake_dependent_option( ENABLE_SCITOKENS "Enable SciTokens plugin." TRUE "NOT XRDCL_ONLY" FALSE )
+cmake_dependent_option( ENABLE_MACAROONS "Enable Macaroons plugin." TRUE "NOT XRDCL_ONLY" FALSE )
 option( FORCE_ENABLED    "Fail build if enabled components cannot be built."              FALSE )
 define_default( XRD_PYTHON_REQ_VERSION 3 )