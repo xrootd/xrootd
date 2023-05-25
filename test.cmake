f2a058d510e3cfaaf462fac82c16cc9bc2c9799a
Guilherme Amadio <amadio@cern.ch>
2023-05-25 17:06:00 +0200

    [CMake] Add scripts to build/test XRootD with CTest
    The test.cmake script is meant to automate some of the standard
    configure, build, test, install cycle during development, but also
    for continuous integration. The script can be generically called as
    
    ctest -VV -S test.cmake
    
    from the top directory of the repository. There are several options
    to customize the build, the main ones are:
    
    -DCOVERAGE=1         Enables test coverage analysis with gcov
    -DMEMCHECK=1         Enables memory checking with valgrind
    -DSTATIC_ANALYSIS=1  Enables static analysis with clang-tidy
    -DINSTALL=1          Enables an extra step to call make install
    
    When enabling coverage, a report is generated by default in the html/
    directory inside the build directory. The results can be viewed by
    opening the file html/coverage_details.html. This step can be disabled
    by passing -DGCOVR=0 to ctest. It is recommended to use a debug build
    to generate the coverage analysis.
    
    The configuration can also be specified directly on the command line
    via the -C option. For example, to run a coverage build in debug mode,
    with less verbose output, and showing test output when a test failure
    happens, one can run:
    
    ctest -V --output-on-failure -C Debug -DCOVERAGE=1 -S test.cmake
    
    Some environment variables can also influence the behavior of the
    script, like CC, CXX, CMAKE_GENERATOR, CTEST_CONFIGURATION_TYPE,
    CMAKE_BUILD_PARALLEL_LEVEL, CTEST_PARALLEL_LEVEL, and CMAKE_ARGS.
    These are mostly self-explanatory and can be used to override the
    provided defaults. For example, to build with the clang compiler
    and use Ninja as CMake generator, one can run:
    
    env CC=clang CXX=clang++ CMAKE_GENERATOR=Ninja ctest -V -S test.cmake
    
    Finally, the script tries to load configuration files from the .ci
    subdirectory in the source directory. The default configuration is
    used if no specific configuration is found for the detected OS. For
    example, on Ubuntu, a file named ubuntu.cmake will be used if present
    instead of config.cmake. The script also tries to detect a version,
    so, for example, on Alma, one could use almalinux8.cmake which would
    have higher precedence than almalinux.cmake. The default config.cmake
    tries to enable as many options as possible without failing if the
    dependencies are not installed.

---
+++ test.cmake
@@ -0,0 +1,176 @@
+cmake_minimum_required(VERSION 3.16)
+
+set(ENV{LANG} "C")
+set(ENV{LC_ALL} "C")
+
+site_name(CTEST_SITE)
+
+if(EXISTS "/etc/os-release")
+  file(STRINGS "/etc/os-release" OS_NAME REGEX "^ID=.*$")
+  string(REGEX REPLACE "ID=[\"']?([^\"']*)[\"']?$" "\\1" OS_NAME "${OS_NAME}")
+  file(STRINGS "/etc/os-release" OS_VERSION REGEX "^VERSION_ID=.*$")
+  string(REGEX REPLACE "VERSION_ID=[\"']?([^\"'.]*).*$" "\\1" OS_VERSION "${OS_VERSION}")
+  file(STRINGS "/etc/os-release" OS_FULL_NAME REGEX "^PRETTY_NAME=.*$")
+  string(REGEX REPLACE "PRETTY_NAME=[\"']?([^\"']*)[\"']?$" "\\1" OS_FULL_NAME "${OS_FULL_NAME}")
+elseif(APPLE)
+  set(OS_NAME "macOS")
+  execute_process(COMMAND sw_vers -productVersion
+    OUTPUT_VARIABLE OS_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(OS_FULL_NAME "${OS_NAME} ${OS_VERSION}")
+else()
+  cmake_host_system_information(RESULT OS_NAME QUERY OS_NAME)
+  cmake_host_system_information(RESULT OS_VERSION QUERY OS_VERSION)
+  set(OS_FULL_NAME "${OS_NAME} ${OS_VERSION}")
+endif()
+
+string(APPEND CTEST_SITE " (${OS_FULL_NAME} - ${CMAKE_SYSTEM_PROCESSOR})")
+
+cmake_host_system_information(RESULT
+  NCORES QUERY NUMBER_OF_PHYSICAL_CORES)
+cmake_host_system_information(RESULT
+  NTHREADS QUERY NUMBER_OF_LOGICAL_CORES)
+
+if(NOT DEFINED ENV{CMAKE_BUILD_PARALLEL_LEVEL})
+  set(ENV{CMAKE_BUILD_PARALLEL_LEVEL} ${NTHREADS})
+endif()
+
+if(NOT DEFINED ENV{CTEST_PARALLEL_LEVEL})
+  set(ENV{CTEST_PARALLEL_LEVEL} ${NCORES})
+endif()
+
+if(NOT DEFINED CTEST_CONFIGURATION_TYPE)
+  if(DEFINED ENV{CMAKE_BUILD_TYPE})
+    set(CTEST_CONFIGURATION_TYPE $ENV{CMAKE_BUILD_TYPE})
+  else()
+    set(CTEST_CONFIGURATION_TYPE RelWithDebInfo)
+  endif()
+endif()
+
+set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}")
+
+execute_process(COMMAND ${CMAKE_COMMAND} --system-information
+  OUTPUT_VARIABLE CMAKE_SYSTEM_INFORMATION ERROR_VARIABLE ERROR)
+
+if(ERROR)
+  message(FATAL_ERROR "Cannot detect system information")
+endif()
+
+string(REGEX REPLACE ".+CMAKE_CXX_COMPILER_ID \"([-0-9A-Za-z ]+)\".*$" "\\1"
+  COMPILER_ID "${CMAKE_SYSTEM_INFORMATION}")
+string(REPLACE "GNU" "GCC" COMPILER_ID "${COMPILER_ID}")
+
+string(REGEX REPLACE ".+CMAKE_CXX_COMPILER_VERSION \"([^\"]+)\".*$" "\\1"
+  COMPILER_VERSION "${CMAKE_SYSTEM_INFORMATION}")
+
+string(APPEND CTEST_BUILD_NAME " ${COMPILER_ID} ${COMPILER_VERSION}")
+string(APPEND CTEST_BUILD_NAME " ${CTEST_CONFIGURATION_TYPE}")
+
+if(DEFINED ENV{CMAKE_GENERATOR})
+  set(CTEST_CMAKE_GENERATOR $ENV{CMAKE_GENERATOR})
+else()
+  string(REGEX REPLACE ".+CMAKE_GENERATOR \"([-0-9A-Za-z ]+)\".*$" "\\1"
+    CTEST_CMAKE_GENERATOR "${CMAKE_SYSTEM_INFORMATION}")
+endif()
+
+if(NOT CTEST_CMAKE_GENERATOR MATCHES "Makefile")
+  string(APPEND CTEST_BUILD_NAME " ${CTEST_CMAKE_GENERATOR}")
+endif()
+
+if(NOT DEFINED CTEST_SOURCE_DIRECTORY)
+  set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
+endif()
+
+if(NOT DEFINED CTEST_BINARY_DIRECTORY)
+  get_filename_component(CTEST_BINARY_DIRECTORY "$ENV{PWD}/build" REALPATH)
+endif()
+
+find_program(CTEST_GIT_COMMAND NAMES git)
+
+if(EXISTS ${CTEST_GIT_COMMAND})
+  if(DEFINED ENV{GIT_PREVIOUS_COMMIT} AND DEFINED ENV{GIT_COMMIT})
+    set(CTEST_CHECKOUT_COMMAND
+      "${CTEST_GIT_COMMAND} -C ${CTEST_SOURCE_DIRECTORY} checkout -f $ENV{GIT_PREVIOUS_COMMIT}")
+    set(CTEST_GIT_UPDATE_CUSTOM
+      ${CTEST_GIT_COMMAND} -C ${CTEST_SOURCE_DIRECTORY} checkout -f $ENV{GIT_COMMIT})
+  elseif(DEFINED ENV{GIT_COMMIT})
+    set(CTEST_CHECKOUT_COMMAND
+      "${CTEST_GIT_COMMAND} -C ${CTEST_SOURCE_DIRECTORY} checkout -f $ENV{GIT_COMMIT}")
+    set(CTEST_GIT_UPDATE_CUSTOM
+      ${CTEST_GIT_COMMAND} -C ${CTEST_SOURCE_DIRECTORY} checkout -f $ENV{GIT_COMMIT})
+  else()
+    set(CTEST_GIT_UPDATE_CUSTOM ${CTEST_GIT_COMMAND} -C ${CTEST_SOURCE_DIRECTORY} diff HEAD)
+  endif()
+endif()
+
+set(CMAKE_ARGS $ENV{CMAKE_ARGS} ${CMAKE_ARGS})
+
+if(COVERAGE)
+  find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+  list(PREPEND CMAKE_ARGS "-DCMAKE_CXX_FLAGS=--coverage")
+endif()
+
+if(MEMCHECK)
+  find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+endif()
+
+if(STATIC_ANALYSIS)
+  find_program(CMAKE_CXX_CLANG_TIDY NAMES clang-tidy)
+  list(PREPEND CMAKE_ARGS "-DCMAKE_CXX_CLANG_TIDY=${CMAKE_CXX_CLANG_TIDY}")
+endif()
+
+foreach(FILENAME ${OS_NAME}${OS_VERSION}.cmake ${OS_NAME}.cmake config.cmake)
+  if(EXISTS "${CTEST_SOURCE_DIRECTORY}/.ci/${FILENAME}")
+    message(STATUS "Using CMake cache file ${FILENAME}")
+    list(PREPEND CMAKE_ARGS -C ${CTEST_SOURCE_DIRECTORY}/.ci/${FILENAME})
+    list(APPEND CTEST_NOTES_FILES ${CTEST_SOURCE_DIRECTORY}/.ci/${FILENAME})
+    break()
+  endif()
+endforeach()
+
+if(NOT DEFINED MODEL)
+  if(DEFINED CTEST_SCRIPT_ARG)
+    set(MODEL ${CTEST_SCRIPT_ARG})
+  else()
+    set(MODEL Experimental)
+  endif()
+endif()
+
+if(IS_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
+  ctest_empty_binary_directory("${CTEST_BINARY_DIRECTORY}")
+endif()
+
+ctest_read_custom_files("${CTEST_SOURCE_DIRECTORY}")
+
+ctest_start(${MODEL})
+ctest_update()
+
+ctest_configure(OPTIONS "${CMAKE_ARGS}")
+
+ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
+list(APPEND CTEST_NOTES_FILES ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt)
+
+ctest_build()
+
+if(INSTALL)
+  set(ENV{DESTDIR} "${CTEST_BINARY_DIRECTORY}/install")
+  ctest_build(TARGET install)
+endif()
+
+ctest_test()
+
+if(DEFINED CTEST_COVERAGE_COMMAND)
+  find_program(GCOVR NAMES gcovr)
+  if(EXISTS ${GCOVR})
+    execute_process(COMMAND
+      ${GCOVR} -r ${CTEST_SOURCE_DIRECTORY}/src ${CTEST_BINARY_DIRECTORY}
+        --html-details ${CTEST_BINARY_DIRECTORY}/html/ ERROR_VARIABLE ERROR)
+    if(ERROR)
+      message(FATAL_ERROR "Failed to generate coverage report")
+    endif()
+  endif()
+  ctest_coverage()
+endif()
+
+if(DEFINED CTEST_MEMORYCHECK_COMMAND)
+  ctest_memcheck()
+endif()
