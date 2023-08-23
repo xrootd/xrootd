e53d4c8dd48b497d1f2fcba7238077bde41331b2
Guilherme Amadio <amadio@cern.ch>
2023-08-23 10:10:56 +0200

    [CMake] Update Findlibuuid.cmake to use correct include paths
    We need to use target_include_directories() instead of set_property() on
    the uuid::uuid target, since setting the property directly does not make
    those directories used during compilation. This is needed for CMS-SW, as it
    uses a specific version of libuuid installed in a non-standard location.
    However, on macOS, using target_include_directories() causes kernel header
    paths to be added in the compilation ahead of the C++ compiler's own paths,
    which causes compilation failures with clang from Xcode. This is why we
    were using set_property() up to now. For target_include_directories() to
    work in both cases (macOS and Linux with non-standard install path), we
    need to set CMAKE_FIND_FRAMEWORKS to LAST before looking for the uuid.h
    header. This way the kernel framework header paths are skipped, and uuid.h
    is found in one of the regular macOS SDK header paths instead.
    
    We now also perform a search for the uuid library using find_library, to
    avoid unnecessary calls to pkg-config. We keep it only as a last resort to
    find the library, as in some cases pkg-config works where we'd have to set
    CMAKE_PREFIX_PATH for find_library to work. Nevertheless, when we do need
    to fallback to pkg-config, the include directory set by the uuid.pc file is
    "wrong", i.e. it reports <prefix>/include/uuid, not just <prefix>/include
    as we expect, so we need to fix it by stripping the last component of the
    include directory in that case.
    
    Finally, in order to let pkg-config work, we were clearing CMake cache
    variables in the beginning of the Findlibuuid module, which had the
    unfortunate side effect of preventing users from setting UUID_INCLUDE_DIR
    and UUID_LIBRARY in the CMake command line to pick up a specific version of
    libuuid. We moved this section within the block where pkg-config is used to
    correct this problem.

--- cmake/Findlibuuid.cmake
+++ cmake/Findlibuuid.cmake
@@ -34,54 +34,71 @@
 # ``UUID_INCLUDE_DIR``
 #   where to find the uuid/uuid.h header (same as UUID_INCLUDE_DIRS).
 
-foreach(var FOUND INCLUDE_DIR INCLUDE_DIRS LIBRARY LIBRARIES)
-  unset(UUID_${var} CACHE)
-endforeach()
+include(CheckCXXSymbolExists)
 
 if(NOT UUID_INCLUDE_DIR)
+  set(CMAKE_FIND_FRAMEWORK LAST)
   find_path(UUID_INCLUDE_DIR uuid/uuid.h)
 endif()
 
 if(IS_DIRECTORY "${UUID_INCLUDE_DIR}")
-  include(CheckCXXSymbolExists)
-  set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
-  set(CMAKE_REQUIRED_INCLUDES ${UUID_INCLUDE_DIRS})
+  set(CMAKE_REQUIRED_INCLUDES ${UUID_INCLUDE_DIR})
   check_cxx_symbol_exists("uuid_generate_random" "uuid/uuid.h" _uuid_header_only)
   unset(CMAKE_REQUIRED_INCLUDES)
+endif()
+
+if(NOT UUID_LIBRARY AND NOT _uuid_header_only)
+  find_library(UUID_LIBRARY NAMES uuid)
+
+  if(UUID_LIBRARY)
+    set(CMAKE_REQUIRED_INCLUDES ${UUID_INCLUDE_DIR})
+    set(CMAKE_REQUIRED_LIBRARIES ${UUID_LIBRARY})
+    check_cxx_symbol_exists("uuid_generate_random" "uuid/uuid.h" _have_libuuid)
+    unset(CMAKE_REQUIRED_INCLUDES)
+    unset(CMAKE_REQUIRED_LIBRARIES)
+  endif()
+
+  if(NOT _have_libuuid)
+    find_package(PkgConfig QUIET)
+    if(PKG_CONFIG_FOUND)
+      # We need to clear cache variables set above, which pkg-config may set,
+      # otherwise the call to pkg_check_modules will have no effect, as it does
+      # not override cache variables.
+      foreach(var FOUND INCLUDE_DIR INCLUDE_DIRS LIBRARY LIBRARIES)
+        unset(UUID_${var} CACHE)
+      endforeach()
+      if(${libuuid_FIND_REQUIRED})
+        set(libuuid_REQUIRED REQUIRED)
+      endif()
+      pkg_check_modules(UUID ${libuuid_REQUIRED} uuid)
+
+      # The include directory returned by pkg-config is <prefix>/include/uuid,
+      # while we expect just <prefix>/include, so strip the last component to
+      # allow #include <uuid/uuid.h> to actually work.
+      get_filename_component(UUID_INCLUDE_DIR ${UUID_INCLUDE_DIRS} DIRECTORY)
+
+      set(UUID_INCLUDE_DIR ${UUID_INCLUDE_DIR} CACHE PATH "")
+      set(UUID_LIBRARY ${UUID_LDFLAGS} CACHE STRING "")
+      unset(UUID_INCLUDE_DIRS CACHE)
+      unset(UUID_LIBRARIES CACHE)
+    endif()
+  endif()
 endif()
 
 if(_uuid_header_only)
   find_package_handle_standard_args(libuuid DEFAULT_MSG UUID_INCLUDE_DIR)
 else()
-  if(NOT UUID_LIBRARY)
-    include(CheckLibraryExists)
-    check_library_exists("uuid" "uuid_generate_random" "" _have_libuuid)
-
-    if(_have_libuuid)
-      set(UUID_LIBRARY "uuid")
-      set(UUID_LIBRARIES ${UUID_LIBRARY})
-    else()
-      find_package(PkgConfig)
-      if(PKG_CONFIG_FOUND)
-        if(${libuuid_FIND_REQUIRED})
-          set(libuuid_REQUIRED REQUIRED)
-        endif()
-        pkg_check_modules(UUID ${libuuid_REQUIRED} uuid)
-        set(UUID_LIBRARIES ${UUID_LDFLAGS})
-        set(UUID_LIBRARY ${UUID_LIBRARIES})
-        set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIRS})
-        set(UUID_INCLUDE_DIR ${UUID_INCLUDE_DIRS})
-      endif()
-    endif()
-    unset(_have_libuuid)
-  endif()
   find_package_handle_standard_args(libuuid DEFAULT_MSG UUID_INCLUDE_DIR UUID_LIBRARY)
 endif()
 
-if(LIBUUID_FOUND AND NOT TARGET uuid::uuid)
-  add_library(uuid::uuid INTERFACE IMPORTED)
-  set_property(TARGET uuid::uuid PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${UUID_INCLUDE_DIRS}")
-  set_property(TARGET uuid::uuid PROPERTY INTERFACE_LINK_LIBRARIES "${UUID_LIBRARIES}")
+if(LIBUUID_FOUND)
+  set(UUID_INCLUDE_DIRS ${UUID_INCLUDE_DIR})
+  set(UUID_LIBRARIES ${UUID_LIBRARY})
+  if(NOT TARGET uuid::uuid)
+    add_library(uuid::uuid INTERFACE IMPORTED)
+    target_include_directories(uuid::uuid SYSTEM INTERFACE "${UUID_INCLUDE_DIRS}")
+    target_link_libraries(uuid::uuid INTERFACE "${UUID_LIBRARIES}")
+  endif()
 endif()
 
 mark_as_advanced(UUID_INCLUDE_DIR UUID_LIBRARY)
