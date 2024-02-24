xrootd/xrootd

commit be89ee0cb3aed0b8735fb65b3aacb365db9e9a7b

[XrdCl] Only claim to be TLS capable if TLS initialization succeeds

src/XrdCl/XrdClXRootDTransport.cc
@@ -1905,25 +1905,24 @@ namespace XrdCl
     request->flags     = ClientProtocolRequest::kXR_secreqs |
                          ClientProtocolRequest::kXR_bifreqs;
 
+    int notlsok = DefaultNoTlsOK;
+    int tlsnodata = DefaultTlsNoData;
+
     XrdCl::Env *env = XrdCl::DefaultEnv::GetEnv();
 
-    int notlsok = DefaultNoTlsOK;
     env->GetInt( "NoTlsOK", notlsok );
 
-    if (info->encrypted || !notlsok)
+    if (expect & ClientProtocolRequest::kXR_ExpBind)
+      env->GetInt( "TlsNoData", tlsnodata );
+
+    if (info->encrypted || InitTLS())
       request->flags |= ClientProtocolRequest::kXR_ableTLS;
 
-    bool nodata = false;
-    if( expect & ClientProtocolRequest::kXR_ExpBind )
-    {
-      int value = DefaultTlsNoData;
-      env->GetInt( "TlsNoData", value );
-      nodata = bool( value );
-    }
-
-    if( info->encrypted && !nodata )
+    if (info->encrypted || !(notlsok || tlsnodata))
       request->flags |= ClientProtocolRequest::kXR_wantTLS;
+
     request->expect = expect;
+
     //--------------------------------------------------------------------------
     // If we are in the curse of establishing a connection in the context of
     // TPC update the expect! (this will be never followed be a bind)

commit 609f065a91c029f982061d019c7820dc594f1a0e

[XrdCl] Fix logic error when upgrading connections to TLS
Fixes: be89ee0cb3aed0b8735fb65b3aacb365db9e9a7b

src/XrdCl/XrdClXRootDTransport.cc
@@ -1918,7 +1918,7 @@ namespace XrdCl
     if (info->encrypted || InitTLS())
       request->flags |= ClientProtocolRequest::kXR_ableTLS;
 
-    if (info->encrypted || !(notlsok || tlsnodata))
+    if (info->encrypted && !(notlsok || tlsnodata))
       request->flags |= ClientProtocolRequest::kXR_wantTLS;
 
     request->expect = expect;