5591c525d1f03f91e5b80fa8f56f8ee9b88b2073
Brian Bockelman <bbockelman@morgridge.org>
2024-03-23 11:26:42 +0100

    Adjust CMake so macaroons can build with json-c
    Without the patch, Mac OS X is missing the macros for the json-c
    library.

--- src/XrdMacaroons.cmake
+++ src/XrdMacaroons.cmake
@@ -34,6 +34,12 @@ if( BUILD_MACAROONS )
     PRIVATE ${MACAROONS_INCLUDES} ${JSON_INCLUDE_DIRS})
 
   if( MacOSX )
+    # Note: as of CMake 3.26.1 on Mac OS X, the pkg_config module that detects json-c
+    # will return only the library name in ${JSON_LIBRARIES}, not the full path.  Per
+    # the CMake documentation (https://cmake.org/cmake/help/latest/command/target_link_directories.html),
+    # this is incorrect.  Until it is fixed, we also set the link directory here.
+    target_link_directories(${LIB_XRD_MACAROONS} PRIVATE "${JSON_LIBRARY_DIRS}")
+
     SET( MACAROONS_LINK_FLAGS "-Wl")
   else()
     SET( MACAROONS_LINK_FLAGS "-Wl,--version-script=${CMAKE_SOURCE_DIR}/src/XrdMacaroons/export-lib-symbols" )
