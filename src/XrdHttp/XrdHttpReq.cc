923253593c5f25d5e383e1d72083a54190dd124a
Brian Bockelman <bbockelman@morgridge.org>
2024-07-26 10:16:30 -0500

    Add support for "Bad Gateway" HTTP status code
    Map various connection-related errors in XrdCl in XCache all the
    way through to the HTTP layer.
    
    This allows a failure to connect to the origin to be represented
    with the correct status code ("Bad Gateway"); currently, a down
    origin results in a 500 ("Internal Server Error") which is misleading
    for users.

--- src/XrdHttp/XrdHttpReq.cc
+++ src/XrdHttp/XrdHttpReq.cc
@@ -903,6 +903,9 @@ void XrdHttpReq::mapXrdErrorToHttpStatus() {
       case kXR_InvalidRequest:
         httpStatusCode = 405; httpStatusText = "Method is not allowed";
         break;
+      case kXR_noserver:
+        httpStatusCode = 502; httpStatusText = "Bad Gateway";
+        break;
       case kXR_TimerExpired:
         httpStatusCode = 504; httpStatusText = "Gateway timeout";
         break;
