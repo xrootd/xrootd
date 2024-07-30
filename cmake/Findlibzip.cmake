ba1e06c5ff96563a84a56115c68ed4d89682807c
Guilherme Amadio <amadio@cern.ch>
2024-07-30 08:46:34 +0200

    [CMake] Use pkg-config in custom Findlibzip.cmake
    Although the libzip package itself exports CMake targets,
    it does so only on Alma 9, and the CMake files refer to
    binaries which require extra dependencies to be installed,
    since they are packaged separately on some distributions.

--- cmake/Findlibzip.cmake
+++ cmake/Findlibzip.cmake
@@ -1,36 +1,69 @@
-foreach(var LIBZIP_FOUND LIBZIP_INCLUDE_DIR LIBZIP_LIBZIP_LIBRARY LIBZIP_LIBRARIES)
-  unset(${var} CACHE)
+#.rst:
+# Findlibzip
+# -------
+#
+# Find libzip library for file management over HTTP-based protocols.
+#
+# Imported Targets
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines :prop_tgt:`IMPORTED` target:
+#
+# ``libzip::zip``
+#   The libzip library, if found.
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project:
+#
+# ``LIBZIP_FOUND``
+#   True if libzip has been found.
+# ``LIBZIP_INCLUDE_DIRS``
+#   Where to find libzip.hpp, etc.
+# ``LIBZIP_LIBRARIES``
+#   The libraries to link against to use libzip.
+# ``LIBZIP_VERSION``
+#   The version of the libzip library found (e.g. 1.9.2).
+#
+# Obsolete variables
+# ^^^^^^^^^^^^^^^^^^
+#
+# The following variables may also be set, for backwards compatibility:
+#
+# ``LIBZIP_LIBRARY``
+#   where to find the LIBZIP library.
+# ``LIBZIP_INCLUDE_DIR``
+#   where to find the LIBZIP headers (same as LIBZIP_INCLUDE_DIRS)
+#
+
+foreach(var FOUND INCLUDE_DIR INCLUDE_DIRS LIBRARY LIBRARIES)
+  unset(LIBZIP_${var} CACHE)
 endforeach()
 
-include( FindPackageHandleStandardArgs )
+find_package(PkgConfig)
 
-if( LIBZIP_INCLUDE_DIRS AND LIBZIP_LIBRARIES )
-  set( LIBZIP_FOUND TRUE )
-else()
+if(PKG_CONFIG_FOUND)
+  if(${libzip_FIND_REQUIRED})
+    set(libzip_REQUIRED REQUIRED)
+  endif()
 
-  find_path(
-    LIBZIP_INCLUDE_DIR
-    NAMES zip.h zipconf.h
-    HINTS
-    ${LIBZIP_ROOT_DIR}
-    PATH_SUFFIXES
-    include )
-  set( LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_DIR})
+  if(NOT DEFINED libzip_FIND_VERSION)
+    pkg_check_modules(LIBZIP ${libzip_REQUIRED} libzip)
+  else()
+    pkg_check_modules(LIBZIP ${libzip_REQUIRED} libzip>=${libzip_FIND_VERSION})
+  endif()
 
-  find_library(
-    LIBZIP_LIBRARY
-    NAMES zip
-    HINTS
-    ${LIBZIP_ROOT_DIR}
-    PATH_SUFFIXES lib64
-    ${LIBRARY_PATH_PREFIX}
-    ${LIB_SEARCH_OPTIONS})
-  set( LIBZIP_LIBRARIES ${LIBZIP_LIBRARY} )
-
-  find_package_handle_standard_args(
-    libzip
-    DEFAULT_MSG
-    LIBZIP_LIBRARY LIBZIP_INCLUDE_DIR )
-
-   mark_as_advanced( LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY )
+  set(LIBZIP_LIBRARIES ${LIBZIP_LDFLAGS})
+  set(LIBZIP_LIBRARY ${LIBZIP_LIBRARIES})
+  set(LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_DIRS})
+  set(LIBZIP_INCLUDE_DIR ${LIBZIP_INCLUDE_DIRS})
 endif()
+
+if(LIBZIP_FOUND AND NOT TARGET libzip::zip)
+  add_library(libzip::zip INTERFACE IMPORTED)
+  set_property(TARGET libzip::zip PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${LIBZIP_INCLUDE_DIRS}")
+  set_property(TARGET libzip::zip PROPERTY INTERFACE_LINK_LIBRARIES "${LIBZIP_LIBRARIES}")
+endif()
+
+mark_as_advanced(LIBZIP_INCLUDE_DIR LIBZIP_LIBRARY)
