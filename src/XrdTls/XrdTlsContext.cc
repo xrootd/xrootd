xrootd/xrootd

commit 8bbc07f4f39072add91ecc2c97347c49f004703a

[XrdTls] Restrict renegotiation for TLSv1.2 and earlier
Fixes: #1689

src/XrdTls/XrdTlsContext.cc
@@ -591,8 +591,14 @@ XrdTlsContext::XrdTlsContext(const char *cert,  const char *key,
          SSL_CTX **ctxLoc;
         } ctx_tracker(&pImpl->ctx);
 
-   static const int sslOpts = SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3
-                            | SSL_OP_NO_COMPRESSION;
+   static const int sslOpts = SSL_OP_ALL
+                            | SSL_OP_NO_SSLv2
+                            | SSL_OP_NO_SSLv3
+                            | SSL_OP_NO_COMPRESSION
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
+                            | SSL_OP_NO_RENEGOTIATION
+#endif
+                            ;
 
    std::string certFN, eText;
    const char *emsg;

commit ee13f530d7a946ecefa82f4730aca27501472f41

[XrdTls] Match type of sslOpts with SSL_CTX_set_options
Fixes: 8bbc07f4f3

src/XrdTls/XrdTlsContext.cc:602:29: warning: overflow in
conversion from 'uint64_t' {aka 'long unsigned int'} to
'int' changes value from '3254913232' to '-1040054064' [-Woverflow]
  602 |                             | SSL_OP_NO_RENEGOTIATION
      |                             ^

src/XrdTls/XrdTlsContext.cc
@@ -591,7 +591,7 @@ XrdTlsContext::XrdTlsContext(const char *cert,  const char *key,
          SSL_CTX **ctxLoc;
         } ctx_tracker(&pImpl->ctx);
 
-   static const int sslOpts = SSL_OP_ALL
+   static const uint64_t sslOpts = SSL_OP_ALL
                             | SSL_OP_NO_SSLv2
                             | SSL_OP_NO_SSLv3
                             | SSL_OP_NO_COMPRESSION