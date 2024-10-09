08c342088a8eaeb7e4838b57d57b2cb380206bee
Guilherme Amadio <amadio@cern.ch>
2024-10-09 16:17:05 +0200

    [CMake] Set CMAKE_INSTALL_RPATH in the cache to allow overrides
    Fixes: e6136c9ebaf56b5f17eda2f53ccafcbd930ac3b1
    Closes: #2350

--- cmake/XRootDOSDefs.cmake
+++ cmake/XRootDOSDefs.cmake
@@ -110,7 +110,7 @@ if( APPLE )
   
   set(CMAKE_MACOSX_RPATH TRUE)
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-  set(CMAKE_INSTALL_RPATH "@loader_path/../lib")
+  set(CMAKE_INSTALL_RPATH "@loader_path/../lib" CACHE STRING "Install RPATH")
 
   # this is here because of Apple deprecating openssl and krb5
   set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )
