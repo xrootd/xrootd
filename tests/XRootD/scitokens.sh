62249868e407d0d2ff8632767aee2401fc195d6c
Brian Bockelman <bbockelman@morgridge.org>
2024-12-01 18:52:42 -0600

    [XrdSciTokens] Add unit test coverage for upload and modify
    Add some simple unit tests for `storage.modify` and `storage.upload`.

--- tests/XRootD/scitokens.sh
+++ tests/XRootD/scitokens.sh
@@ -26,6 +26,20 @@ function setup_scitokens() {
 	fi
 	chmod 0600 "$OUTPUTDIR/token"
 
+	# Create a create-only token
+	if ! xrdscitokens-create-token issuer_pub_1.pem issuer_key_1.pem test_1 \
+		"https://localhost:7095/issuer/one" storage.create:/subdir > "$OUTPUTDIR/token_create"; then
+		echo "Failed to create 'storage.create' token"
+		exit 1
+	fi
+
+	# Create a modify token
+	if ! xrdscitokens-create-token issuer_pub_1.pem issuer_key_1.pem test_1 \
+		"https://localhost:7095/issuer/one" storage.modify:/subdir_modify > "$OUTPUTDIR/token_modify"; then
+		echo "Failed to create 'storage.modify' token"
+		exit 1
+	fi
+
 	popd || exit 1
 }
 
@@ -34,23 +48,39 @@ function execute_curl() {
 	expected_status="$2"
 	expected_contents="$3"
 	token_file="$4"
+	http_verb="$5"
 
+	token_arg=()
 	if [ -n "$token_file" ]; then
 		token_arg=(-H "Authorization: Bearer $(cat "$token_file")")
 	fi
+	verb_arg=()
+	if [ -n "$http_verb" ]; then
+		verb_arg=(-X "$http_verb" -d "$expected_contents")
+		echo -n "$http_verb $url "
+	else
+		echo -n "GET $url "
+	fi
+	token_arg=()
+	if [ -n "$token_file" ]; then
+		token_arg=(-H "Authorization: Bearer $(cat "$token_file")")
+		echo -n "with token file $token_file "
+	else
+		echo -n "with no token "
+	fi
+	echo "(expected $expected_status)"
 
 	# This does not use the "assert" helper function as we must redirect curl's output;
 	# "assert" does not allow redirects
-	echo "Downloading $url (expected $expected_status)"
-	if ! curl -s "${token_arg[@]}" -o "$TMPDIR/curl_output" -w "%{http_code}" \
+	if ! curl -s "${verb_arg[@]}" "${token_arg[@]}" -o "$TMPDIR/curl_output" -w "%{http_code}" \
 		--cacert "../issuer/tlsca.pem" "$url" > "$TMPDIR/curl_status"; then
 
-		echo "Download failed.  Retrying with verbose output enabled:"
-		curl -s "${token_arg[@]}" -v --cacert "../issuer/tlsca.pem" "$url"
+		echo "HTTP request failed.  Retrying with verbose output enabled:"
+		curl -s "${verb_arg[@]}" "${token_arg[@]}" -v --cacert "../issuer/tlsca.pem" "$url"
 		exit 1
 	fi
 	assert_eq "$expected_status" "$(cat "$TMPDIR/curl_status")"
-	if [ -n "$expected_contents" ]; then
+	if [ -n "$expected_contents" ] && [ -z "$http_verb" ]; then
 		assert_eq "$expected_contents" "$(cat "$TMPDIR/curl_output")"
 	fi
 }
@@ -75,4 +105,22 @@ function test_scitokens() {
 	execute_curl "$HOST/issuer/one/issuer.jwks" 200 "$(cat scitokens/xrootd/issuer/one/issuer.jwks)"
 	execute_curl "$HOST/protected/hello_world.txt" 403 ""
 	execute_curl "$HOST/protected/hello_world.txt" 200 "Hello, World" scitokens/token
+
+	# Downloading $HOST/protected/hello_world.txt with create-only token (expected 403)
+	execute_curl "$HOST/protected/hello_world.txt" 403 "" scitokens/token_create
+
+	# Uploading $HOST/protected/subdir/hello_world.txt with create-only token (expected 201)
+	execute_curl "$HOST/protected/subdir/hello_world.txt" 201 "hello, world" scitokens/token_create PUT
+	execute_curl "$HOST/protected/subdir/hello_world.txt" 200 "hello, world" scitokens/token
+
+	# Re-uploading $HOST/protected/subdir/hello_world.txt with create-only token (expected 403)
+	execute_curl "$HOST/protected/subdir/hello_world.txt" 403 "hello, world" scitokens/token_create PUT
+
+	# Uploading $HOST/protected/subdir_modify/hello_world.txt with modify token (expected 201)"
+	execute_curl "$HOST/protected/subdir_modify/hello_world.txt" 201 test scitokens/token_modify PUT
+	execute_curl "$HOST/protected/subdir_modify/hello_world.txt" 200 test scitokens/token
+
+	# Re-uploading $HOST/protected/subdir_modify/hello_world.txt with modify token (expected 201)
+	execute_curl "$HOST/protected/subdir_modify/hello_world.txt" 201 'hello, world' scitokens/token_modify PUT
+	execute_curl "$HOST/protected/subdir_modify/hello_world.txt" 200 'hello, world' scitokens/token
 }
