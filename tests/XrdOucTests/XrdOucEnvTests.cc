042985c947e3b71d8d0c6d825172c4f328fb1ec5
Brian Bockelman <bbockelman@morgridge.org>
2024-12-31 11:13:10 -0600

    [XrdSciTokens] Accept `access_token` CGI parameter
    On reading the definition of the `Bearer` Authentication scheme
    in RFC 6750, I stumbled upon Section 2.3 ("URI Query Parameter")
    which specifies the query parameter name that should be used when
    embedding the token in the URL; it should be `access_token`.
    
    This commit continues to accept `authz` (too much historical baggage
    to break) but adds in `access_token` alongside it.
    
    This commit includes unit test coverage for the new parameter name,
    including tests for `XrdOucEnv::TidyEnv` and fixes bugs for `TidyEnv`
    in the case the tidying resulted in the empty string.

---
+++ tests/XrdOucTests/XrdOucEnvTests.cc
@@ -0,0 +1,28 @@
+
+#include "XrdOuc/XrdOucEnv.hh"
+
+#include <gtest/gtest.h>
+
+static const std::pair<std::string, std::string> env_tests[] = {
+    {"foo=bar", "&foo=bar"},
+    {"authz=bar", "&"},
+    {"authz=bar&foo=1", "&foo=1"},
+    {"&authz=bar&authz=1", "&"},
+    {"authz=bar&authz=1", "&"},
+    {"&authz=bar", "&"},
+    {"&access_token=bar", "&"},
+    {"foo=1&authz=bar", "&foo=1"},
+    {"foo=1&authz=foo&access_token=bar", "&foo=1"},
+    {"authz=foo&access_token=bar", "&"},
+    {"authz=foo&foo=bar", "&foo=bar"},
+    {"authz=foo&foo=bar&access_token=3", "&foo=bar"},
+    {"authz=1&access_token=2&authz=3&access_token=4", "&"},
+};
+
+TEST(XrdOucEnv, EnvTidy) {
+    for (const auto &env_str : env_tests) {
+        int envlen;
+        XrdOucEnv env(env_str.first.c_str(), env_str.first.size());
+        ASSERT_STREQ(env_str.second.c_str(), env.EnvTidy(envlen)) << "Testing tidy of " << env_str.first;
+    }	    
+}
