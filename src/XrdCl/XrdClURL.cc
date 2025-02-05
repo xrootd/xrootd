df17697248308065fb4c0b3874f164b214b882d5
David Smith <david.smith@cern.ch>
2025-02-05 13:32:35 +0100

    [Cl] Also recognise IPv4 mapped addresses

--- src/XrdCl/XrdClURL.cc
+++ src/XrdCl/XrdClURL.cc
@@ -26,6 +26,7 @@
 #include "XrdCl/XrdClOptimizers.hh"
 
 #include <cstdlib>
+#include <cctype>
 #include <vector>
 #include <sstream>
 #include <algorithm>
@@ -238,13 +239,23 @@ namespace XrdCl
         // Check if we're IPv6 encoded IPv4
         //----------------------------------------------------------------------
         pos = pHostName.find( "." );
-        size_t pos2 = pHostName.find( "[::ffff" );
-        size_t pos3 = pHostName.find( "[::" );
-        if( pos != std::string::npos && pos3 != std::string::npos &&
-            pos2 == std::string::npos )
+        const size_t pos2 = pHostName.find( "[::" );
+        if( pos != std::string::npos && pos2 != std::string::npos )
         {
-          pHostName.erase( 0, 3 );
-          pHostName.erase( pHostName.length()-1, 1 );
+          std::string hl = pHostName;
+          std::transform(hl.begin(), hl.end(), hl.begin(),
+            [](unsigned char c){ return std::tolower(c); });
+          const size_t pos3 = hl.find( "[::ffff:" );
+          if ( pos3 != std::string::npos )
+          {
+            pHostName.erase( 0, 8 );
+            pHostName.erase( pHostName.length()-1, 1 );
+          }
+          else
+          {
+            pHostName.erase( 0, 3 );
+            pHostName.erase( pHostName.length()-1, 1 );
+          }
         }
       }
     }
