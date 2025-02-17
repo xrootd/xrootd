7fc7be6188ceada91360a73cc45517dbb7e66b3f
Andrew Hanushevsky <abh@slac.stanford.edu>
2025-02-17 17:36:18 +0100

    [XrdOss] Allow setting a file descriptor for an unopen file

--- src/XrdOss/XrdOss.hh
+++ src/XrdOss/XrdOss.hh
@@ -405,6 +405,8 @@ uint16_t        DFType() {return dfType;}
 //!                                Response: Pointer to XrdOucChkPnt object.
 //!                  Fctl_utimes - Set atime and mtime (no response).
 //!                                Argument: struct timeval tv[2]
+//!                  Fctl_setFD  - Set file descriptor for unopened file. 
+//!                                Argument: pointer to int file descriptor
 //! @param  alen   - Length of data pointed to by args.
 //! @param  args   - Data sent with request, zero if alen is zero.
 //! @param  resp   - Where the response is to be set. The caller must call
@@ -415,6 +417,7 @@ uint16_t        DFType() {return dfType;}
 
 static const int Fctl_ckpObj = 0;
 static const int Fctl_utimes = 1;
+static const int Fctl_setFD  = 2; // alen = sizeof(int), args->int
 
 virtual int     Fctl(int cmd, int alen, const char *args, char **resp=0);
 
