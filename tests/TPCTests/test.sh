1b8618c0bb12478cd4f55f35a760edd3232359ad
Rahul Chauhan <rahul.chauhan@cern.ch>
2025-03-14 15:16:25 +0100

    [Tests] Add configuration for SciTokens support -  Add scitokens auth to HTTP TPC
    Signed-off-by: Rahul Chauhan <rahul.chauhan@cern.ch>

--- tests/TPCTests/test.sh
+++ tests/TPCTests/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+set -Eeuxo pipefail
 
 # Skip on macOS due to missing 'declare -A' support
 if [[ $(uname) == "Darwin" ]]; then
@@ -30,6 +30,25 @@ check_commands() {
     fi
 }
 
+function error() {
+	echo "error: $*" >&2; exit 1;
+}
+
+# shellcheck disable=SC2317
+function assert() {
+	echo "$@"; "$@" || error "command \"$*\" failed";
+}
+
+# $1 is expected_value $2 is received value $3 is the error message
+function assert_eq() {
+  [[ "$1" == "$2" ]] || error "$3: expected $1 but received $2"
+}
+
+# shellcheck disable=SC2317
+function assert_failure() {
+	echo "$@"; "$@" && error "command \"$*\" did not fail";
+}
+
 check_commands "${ADLER32}" "${CRC32C}" "${XRDCP}" "${XRDFS}" "${OPENSSL}" "${CURL}"
 
 # Server host mappings
@@ -43,7 +62,18 @@ declare -A hosts_http=(
     [srv2]="https://localhost:10952"
 )
 
-cleanup_files() {
+setup_scitokens() {
+	if ! ${XRDSCITOKENS_CREATE_TOKEN} "${XRDSCITOKENS_ISSUER_DIR}"/issuer_pub_1.pem "${XRDSCITOKENS_ISSUER_DIR}"/issuer_key_1.pem test_1 \
+		"https://localhost:7095/issuer/one" "storage.modify:/ storage.create:/ storage.read:/"> "${PWD}/generated_tokens/token"; then
+		echo "Failed to create token"
+		exit 1
+	fi
+	chmod 0600 "$PWD/generated_tokens/token"
+}
+
+# Cleanup function
+# shellcheck disable=SC2317
+cleanup() {
     # Cleanup local and remote files
     for src in "${!hosts[@]}"; do
     rm "${LCLDATADIR}/${src}.dat"
@@ -66,29 +96,22 @@ cleanup_files() {
 
     rmdir "${LCLDATADIR}"
 }
-
-# Cleanup function
-# shellcheck disable=SC2317
-cleanup() {
-    echo "Error occurred. Cleaning up..."
-    set +e
-    cleanup_files
-    exit 1
-}
-trap "cleanup" ERR
+trap "cleanup" ERR EXIT
 
 # Set up directories
 RMTDATADIR="/srvdata/tpc"
 LCLDATADIR="${PWD}/localdata/tpc"
 mkdir -p "${LCLDATADIR}"
+mkdir -p "${PWD}/generated_tokens"
+mkdir -p "$XDG_CACHE_HOME/scitokens" && rm -rf "$XDG_CACHE_HOME/scitokens"/*
 
 
-## TODO: Cleanup for testing purposes
-set +e
-cleanup_files > /dev/null 2>&1
-set -e
-mkdir -p "${LCLDATADIR}"
-## Please remove the above block after testing
+# Set up scitokens
+setup_scitokens
+export BEARER_TOKEN_FILE="$PWD/generated_tokens/token"
+BEARER_TOKEN=$(cat "$BEARER_TOKEN_FILE")
+export BEARER_TOKEN
+
 
 
 generate_file() {
@@ -114,26 +137,36 @@ perform_http_tpc() {
     local src=$1
     local dst=$2
     local mode=$3
+    local token_src=$4
+    local token_dst=$5
 
     local src_file_http="${hosts_http[$src]}/${RMTDATADIR}/${src}.ref"
     local dst_file_http="${hosts_http[$dst]}/${RMTDATADIR}/${src}_to_${dst}.ref_http"
+    local http_code
 
     if [[ "$mode" == "push" ]]; then
         dst_file_http="${dst_file_http}_push"
-        ${CURL} -X COPY -L --verbose \
-        -H "Destination: ${dst_file_http}" \
-        --cacert "${BINARY_DIR}"/tests/issuer/tlsca.pem \
-        "${src_file_http}"
+        http_code=$(${CURL} -X COPY -L -s -o /dev/null -w "%{http_code}" \
+            -H "Destination: ${dst_file_http}" \
+            -H "Authorization: Bearer ${token_dst}" \
+            -H "TransferHeaderAuthorization: Bearer ${token_src}" \
+            --cacert "${BINARY_DIR}/tests/issuer/tlsca.pem" \
+            "${src_file_http}")
     elif [[ "$mode" == "pull" ]]; then
         dst_file_http="${dst_file_http}_pull"
-        ${CURL} -X COPY -L --verbose \
-        -H "Source: ${src_file_http}" \
-        --cacert "${BINARY_DIR}"/tests/issuer/tlsca.pem \
-        "${dst_file_http}"
+        http_code=$(${CURL} -X COPY -L -s -o /dev/null -w "%{http_code}" \
+            -H "Source: ${src_file_http}" \
+            -H "Authorization: Bearer ${token_src}" \
+            -H "TransferHeaderAuthorization: Bearer ${token_dst}" \
+            --cacert "${BINARY_DIR}/tests/issuer/tlsca.pem" \
+            "${dst_file_http}")
     else
-        echo "ERROR: Unsupported mode: $mode"
-        exit 1
+        echo "ERROR: Unsupported mode: $mode" >&2
+        return 1
     fi
+
+    echo "$http_code"
+    return 0
 }
 
 download_file() {
@@ -172,6 +205,7 @@ verify_checksum() {
     fi
 }
 
+
 # Generate, upload, download, and verify checksums for each host
 for host in "${!hosts[@]}"; do
     generate_file "${LCLDATADIR}/${host}.ref"
@@ -198,8 +232,8 @@ done
 for src in "${!hosts[@]}"; do
     for dst in "${!hosts[@]}"; do     
        perform_tpc "${src}" "${dst}"
-       perform_http_tpc "${src}" "${dst}" "pull"
-       perform_http_tpc "${src}" "${dst}" "push"
+       assert_eq "201" "$(perform_http_tpc "$src" "$dst" "pull" "$BEARER_TOKEN" "$BEARER_TOKEN")" "HTTP TPC pull failed"
+       assert_eq "201" "$(perform_http_tpc "$src" "$dst" "push" "$BEARER_TOKEN" "$BEARER_TOKEN")" "HTTP TPC push failed"
     done
 done
 
@@ -242,8 +276,6 @@ for src in "${!hosts[@]}"; do
         verify_checksum "adler32" "${ref_file_http}" "${new_file_http}_push" "${hosts[$dst]}" "${RMTDATADIR}/${src}_to_${dst}.ref_http_push"
     done
 done
-
-cleanup_files
 
 echo "ALL TESTS PASSED"
 exit 0
