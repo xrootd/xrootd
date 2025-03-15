b39464c363d868519a7b25f4dbcda6fa53276cff
Guilherme Amadio <amadio@cern.ch>
2025-03-15 21:17:44 +0100

    [CMake] Move compiler_define_if_found function to XRootDSystemCheck.cmake

--- cmake/XRootDUtils.cmake
+++ cmake/XRootDUtils.cmake
@@ -1,13 +1,4 @@
 
-#-------------------------------------------------------------------------------
-# Add a compiler define flag if a variable is defined
-#-------------------------------------------------------------------------------
-function( compiler_define_if_found predicate name )
-  if( ${predicate} )
-    add_definitions( -D${name} )
-  endif()
-endfunction()
-
 macro( define_default variable value )
   if( NOT DEFINED ${variable} )
     set( ${variable} ${value} )
