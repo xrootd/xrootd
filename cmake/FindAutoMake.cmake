bcb64f729f585ddc23bf3ded6d3005ef3313c5e5
Guilherme Amadio <amadio@cern.ch>
2025-03-20 15:53:42 +0100

    [CMake] Remove unused find modules for isa-l dependencies

--- cmake/FindAutoMake.cmake
+++
@@ -1,9 +0,0 @@
-FIND_PROGRAM( AUTOMAKE_FOUND automake
-              HINTS
-              ${AUTOMAKE_DIR}
-              /usr
-              PATH_SUFFIXES bin
-)
-
-INCLUDE( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( AutoMake DEFAULT_MSG AUTOMAKE_FOUND )
--- cmake/FindLibTool.cmake
+++
@@ -1,9 +0,0 @@
-FIND_PROGRAM( LIBTOOL_FOUND libtool
-              HINTS
-              ${LIBTOOL_DIR}
-              /usr
-              PATH_SUFFIXES bin
-)
-
-INCLUDE( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( LibTool DEFAULT_MSG LIBTOOL_FOUND )
