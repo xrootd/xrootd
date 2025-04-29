xrootd/xrootd

commit a019ae259417abec4fd89012395d391f21b6eb29

[CMake] Do not force a build type on Solaris if unset
Users can set CMAKE_CXX_FLAGS directly without using a build type.

cmake/XRootDOSDefs.cmake
@@ -2,8 +2,6 @@
 # Define the OS variables
 #-------------------------------------------------------------------------------
 
-include( CheckCXXSourceRuns )
-
 set( LINUX    FALSE )
 set( KFREEBSD FALSE )
 set( Hurd     FALSE )
@@ -161,21 +159,4 @@ if( ${CMAKE_SYSTEM_NAME} STREQUAL "SunOS" )
     set( LIB_SEARCH_OPTIONS NO_DEFAULT_PATH )
     define_default( LIBRARY_PATH_PREFIX "lib/64" )
   endif()
-
-  #-----------------------------------------------------------------------------
-  # Check if the SunCC compiler can do optimizations
-  #-----------------------------------------------------------------------------
-  check_cxx_source_runs(
-  "
-    int main()
-    {
-      #if __SUNPRO_CC > 0x5100
-      return 0;
-      #else
-      return 1;
-      #endif
-    }
-  "
-  SUNCC_CAN_DO_OPTS )
-
 endif()

cmake/XRootDDefaults.cmake
@@ -1,14 +1,6 @@
 #-------------------------------------------------------------------------------
 # Define the default build parameters
 #-------------------------------------------------------------------------------
-if( "${CMAKE_BUILD_TYPE}" STREQUAL "" )
-  if( Solaris AND NOT SUNCC_CAN_DO_OPTS )
-    set( CMAKE_BUILD_TYPE Debug )
-  else()
-    set( CMAKE_BUILD_TYPE RelWithDebInfo )
-  endif()
-endif()
-
 include( CMakeDependentOption )
 
 define_default( PLUGIN_VERSION    5 )

commit b0e427bf13100c1a00ad4f0b3181b0c1831b78be

[CMake] Set a default build type of RelWithDebInfo if not set
Inadvertently, the default build type was removed everywhere,
not just on Solaris as intended by the previous commit.

Fixes: a019ae259417abec4fd89012395d391f21b6eb29

cmake/XRootDOSDefs.cmake
@@ -4,6 +4,14 @@ macro( define_default variable value )
   endif()
 endmacro()
 
+# Set a default build type of RelWithDebInfo if not set
+if(NOT GENERATOR_IS_MULTI_CONFIG AND NOT CMAKE_BUILD_TYPE)
+  if(NOT CMAKE_C_FLAGS AND NOT CMAKE_CXX_FLAGS)
+    set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING
+      "CMake build type for single-configuration generators" FORCE)
+  endif()
+endif()
+
 add_definitions( -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 )
 define_default( LIBRARY_PATH_PREFIX "lib" )
 