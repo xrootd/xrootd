xrootd/xrootd

commit 3a957083c5737a1fc7d9e6d440351b0322bd1dfc

[Cl] Allow out-of-order message-sent confirmation and reply reception
This is needed in order to allow a planned change in the way substreams
are assigned to network pollers.

src/XrdCl/XrdClXRootDMsgHandler.cc
@@ -108,6 +108,22 @@ namespace XrdCl
   //----------------------------------------------------------------------------
   uint16_t XRootDMsgHandler::Examine( std::shared_ptr<Message> &msg )
   {
+    const int sst = pSendingState.fetch_or( kSawResp );
+
+    if( !( sst & kSendDone ) && !( sst & kSawResp ) )
+    {
+      // we must have been sent although we haven't got the OnStatusReady
+      // notification yet. Set the inflight notice.
+
+      Log *log = DefaultEnv::GetLog();
+      log->Dump( XRootDMsg, "[%s] Message %s reply received before notification "
+                 "that it was sent, assuming it was sent ok.",
+                 pUrl.GetHostId().c_str(),
+                 pRequest->GetObfuscatedDescription().c_str() );
+
+      pMsgInFly = true;
+    }
+
     //--------------------------------------------------------------------------
     // if the MsgHandler is already being used to process another request
     // (kXR_oksofar) we need to wait
@@ -898,6 +914,25 @@ namespace XrdCl
   {
     Log *log = DefaultEnv::GetLog();
 
+    const int sst = pSendingState.fetch_or( kSendDone );
+
+    if( sst & kFinalResp )
+    {
+      log->Dump( XRootDMsg, "[%s] Got late notification that outgoing message %s was "
+                 "sent, already have final response, queuing handler callback.",
+                 pUrl.GetHostId().c_str(), message->GetObfuscatedDescription().c_str() );
+      HandleRspOrQueue();
+      return;
+    }
+
+    if( sst & kSawResp )
+    {
+      log->Dump( XRootDMsg, "[%s] Got late notification that message %s has "
+                 "been successfully sent.",
+                 pUrl.GetHostId().c_str(), message->GetObfuscatedDescription().c_str() );
+      return;
+    }
+
     //--------------------------------------------------------------------------
     // We were successful, so we now need to listen for a response
     //--------------------------------------------------------------------------
@@ -905,10 +940,6 @@ namespace XrdCl
     {
       log->Dump( XRootDMsg, "[%s] Message %s has been successfully sent.",
                  pUrl.GetHostId().c_str(), message->GetObfuscatedDescription().c_str() );
-
-      log->Debug( ExDbgMsg, "[%s] Moving MsgHandler: %p (message: %s ) from out-queue to in-queue.",
-                  pUrl.GetHostId().c_str(), this,
-                  pRequest->GetObfuscatedDescription().c_str() );
 
       pMsgInFly = true;
       return;
@@ -1114,6 +1145,20 @@ namespace XrdCl
   void XRootDMsgHandler::HandleResponse()
   {
     //--------------------------------------------------------------------------
+    // Is it a final response?
+    //--------------------------------------------------------------------------
+    bool finalrsp = !( pStatus.IsOK() && pStatus.code == suContinue );
+    if( finalrsp )
+    {
+      // Do not do final processing of the response if we haven't had
+      // confirmation the original request was sent (via OnStatusReady).
+      // The final processing will be triggered when we get the confirm.
+      const int sst = pSendingState.fetch_or( kFinalResp );
+      if( !( sst & kSendDone ) )
+        return;
+    }
+
+    //--------------------------------------------------------------------------
     // Process the response and notify the listener
     //--------------------------------------------------------------------------
     XRootDTransport::UnMarshallRequest( pRequest );
@@ -1147,11 +1192,6 @@ namespace XrdCl
       pRdirEntry->status = *status;
       pRedirectTraceBack.push_back( std::move( pRdirEntry ) );
     }
-
-    //--------------------------------------------------------------------------
-    // Is it a final response?
-    //--------------------------------------------------------------------------
-    bool finalrsp = !( pStatus.IsOK() && pStatus.code == suContinue );
 
     //--------------------------------------------------------------------------
     // Release the stream id
@@ -2243,6 +2283,20 @@ namespace XrdCl
   //------------------------------------------------------------------------
   void XRootDMsgHandler::HandleRspOrQueue()
   {
+    //--------------------------------------------------------------------------
+    // Is it a final response?
+    //--------------------------------------------------------------------------
+    bool finalrsp = !( pStatus.IsOK() && pStatus.code == suContinue );
+    if( finalrsp )
+    {
+      // Do not do final processing of the response if we haven't had
+      // confirmation the original request was sent (via OnStatusReady).
+      // The final processing will be triggered when we get the confirm.
+      const int sst = pSendingState.fetch_or( kFinalResp );
+      if( !( sst & kSendDone ) )
+        return;
+    }
+
     JobManager *jobMgr = pPostMaster->GetJobManager();
     if( jobMgr->IsWorker() )
       HandleResponse();

src/XrdCl/XrdClInQueue.cc
@@ -51,12 +51,12 @@ namespace XrdCl
   //----------------------------------------------------------------------------
   // Add a listener that should be notified about incoming messages
   //----------------------------------------------------------------------------
-  void InQueue::AddMessageHandler( MsgHandler *handler, time_t expires, bool &rmMsg )
+  void InQueue::AddMessageHandler( MsgHandler *handler, bool &rmMsg )
   {
     uint16_t handlerSid = handler->GetSid();
     XrdSysMutexHelper scopedLock( pMutex );
 
-    pHandlers[handlerSid] = HandlerAndExpire( handler, expires );
+    pHandlers[handlerSid] = HandlerAndExpire( handler, 0 );
   }
 
   //----------------------------------------------------------------------------
@@ -85,6 +85,11 @@ namespace XrdCl
       Log *log = DefaultEnv::GetLog();
       handler = it->second.first;
       act     = handler->Examine( msg );
+      if( it->second.second == 0 ) {
+        it->second.second = handler->GetExpiration();
+        log->Debug( ExDbgMsg, "[handler: %p] Assigned expiration %lld.",
+                    handler, (long long)it->second.second );
+      }
       exp     = it->second.second;
       log->Debug( ExDbgMsg, "[msg: %p] Assigned MsgHandler: %p.",
                   msg.get(), handler );
@@ -167,7 +172,7 @@ namespace XrdCl
     HandlerMap::iterator it = pHandlers.begin();
     while( it != pHandlers.end() )
     {
-      if( it->second.second <= now )
+      if( it->second.second && it->second.second <= now )
       {
         uint8_t act = it->second.first->OnStreamEvent( MsgHandler::Timeout,
                                          Status( stError, errOperationExpired ) );
@@ -180,4 +185,27 @@ namespace XrdCl
         ++it;
     }
   }
+
+  //----------------------------------------------------------------------------
+  // Query the handler and extract the expiration time
+  //----------------------------------------------------------------------------
+  void InQueue::AssignTimeout( MsgHandler *handler )
+  {
+    uint16_t handlerSid = handler->GetSid();
+    XrdSysMutexHelper scopedLock( pMutex );
+    HandlerMap::iterator it = pHandlers.find( handlerSid );
+    if( it != pHandlers.end() )
+    {
+      if( it->second.second == 0 )
+      {
+        it->second.second   = handler->GetExpiration();
+
+        Log *log = DefaultEnv::GetLog();
+        log->Debug( ExDbgMsg, "[handler: %p] Assigned expiration %lld.",
+                    handler, (long long)it->second.second );
+
+      }
+    }
+  }
+
 }

src/XrdCl/XrdClInQueue.hh
@@ -37,14 +37,26 @@ namespace XrdCl
   {
     public:
       //------------------------------------------------------------------------
-      //! Add a listener that should be notified about incoming messages
+      //! Add a listener that should be notified about incoming messages.
+      //! Freshly added handlers have no expire time set and will not trigger
+      //! the timeout reporting. The expiry is added by AssignTimeout or
+      //! GetHandlerForMessage.
       //!
       //! @param handler message handler
-      //! @param expires time when the message handler expires
       //! @param rmMsg   will be set to true if a left over message matching the
       //!                request has been removed from the queue
       //------------------------------------------------------------------------
-      void AddMessageHandler( MsgHandler *handler, time_t expires, bool &rmMsg );
+      void AddMessageHandler( MsgHandler *handler, bool &rmMsg );
+
+      //------------------------------------------------------------------------
+      //! If the specified handler is in the queue but has not yet had an
+      //! expiry time assigned, query the handler for the expiry and record
+      //! it. Expiry will also be assigned by GetHandlerForMessage if not
+      //! already assigned.
+      //!
+      //! @param handler handler to check
+      //------------------------------------------------------------------------
+      void AssignTimeout( MsgHandler *handler );
 
       //------------------------------------------------------------------------
       //! Get a message handler interested in receiving message whose header

src/XrdCl/XrdClStream.cc
@@ -559,9 +559,26 @@ namespace XrdCl
     h.msg = pSubStreams[subStream]->outQueue->PopMessage( h.handler,
                                                           h.expires,
                                                           h.stateful );
+
+    log->Debug( PostMasterMsg, "[%s] Duplicating MsgHandler: %p (message: %s) "
+                "from out-queue to in-queue, starting to send outgoing.",
+                pUrl->GetHostId().c_str(), h.handler,
+                h.msg->GetObfuscatedDescription().c_str() );
+
     scopedLock.UnLock();
+
     if( h.handler )
+    {
+      bool rmMsg = false;
+      pIncomingQueue->AddMessageHandler( h.handler, rmMsg );
+      if( rmMsg )
+      {
+        Log *log = DefaultEnv::GetLog();
+        log->Warning( PostMasterMsg, "[%s] Removed a leftover msg from the in-queue.",
+                      pStreamName.c_str() );
+      }
       h.handler->OnReadyToSend( h.msg );
+    }
     return std::make_pair( h.msg, h.handler );
   }
 
@@ -591,15 +608,11 @@ namespace XrdCl
     pBytesSent += bytesSent;
     if( h.handler )
     {
+      // ensure expiration time is assigned if still in queue
+      pIncomingQueue->AssignTimeout( h.handler );
+      // OnStatusReady may cause the handler to delete itself, in
+      // which case the handler or the user callback may also delete msg
       h.handler->OnStatusReady( msg, XRootDStatus() );
-      bool rmMsg = false;
-      pIncomingQueue->AddMessageHandler( h.handler, h.handler->GetExpiration(), rmMsg );
-      if( rmMsg )
-      {
-        Log *log = DefaultEnv::GetLog();
-        log->Warning( PostMasterMsg, "[%s] Removed a leftover msg from the in-queue.",
-                      pStreamName.c_str() );
-      }
     }
     pSubStreams[subStream]->outMsgHelper.Reset();
   }
@@ -824,6 +837,7 @@ namespace XrdCl
       OutQueue::MsgHelper &h = pSubStreams[subStream]->outMsgHelper;
       pSubStreams[subStream]->outQueue->PushFront( h.msg, h.handler, h.expires,
                                                    h.stateful );
+      pIncomingQueue->RemoveMessageHandler(h.handler);
       pSubStreams[subStream]->outMsgHelper.Reset();
     }
 
@@ -932,6 +946,7 @@ namespace XrdCl
         OutQueue::MsgHelper &h = pSubStreams[substream]->outMsgHelper;
         pSubStreams[substream]->outQueue->PushFront( h.msg, h.handler, h.expires,
                                                      h.stateful );
+        pIncomingQueue->RemoveMessageHandler(h.handler);
         pSubStreams[substream]->outMsgHelper.Reset();
       }
 

src/XrdCl/XrdClStream.hh
@@ -366,8 +366,8 @@ namespace XrdCl
       //------------------------------------------------------------------------
       timeval                        pConnectionStarted;
       timeval                        pConnectionDone;
-      uint64_t                       pBytesSent;
-      uint64_t                       pBytesReceived;
+      std::atomic<uint64_t>          pBytesSent;
+      std::atomic<uint64_t>          pBytesReceived;
 
       //------------------------------------------------------------------------
       // Data stream on-connect handler

src/XrdCl/XrdClXRootDMsgHandler.hh
@@ -168,6 +168,7 @@ namespace XrdCl
         pAggregatedWaitTime( 0 ),
 
         pMsgInFly( false ),
+        pSendingState( 0 ),
 
         pTimeoutFence( false ),
 
@@ -233,7 +234,7 @@ namespace XrdCl
       //! @return       action type that needs to be take wrt the message and
       //!               the handler
       //------------------------------------------------------------------------
-      virtual uint16_t Examine( std::shared_ptr<Message> &msg  );
+      virtual uint16_t Examine( std::shared_ptr<Message> &msg  ) override;
 
       //------------------------------------------------------------------------
       //! Reexamine the incoming message, and decide on the action to be taken
@@ -245,21 +246,21 @@ namespace XrdCl
       //! @return       action type that needs to be take wrt the message and
       //!               the handler
       //------------------------------------------------------------------------
-      virtual uint16_t InspectStatusRsp();
+      virtual uint16_t InspectStatusRsp() override;
 
       //------------------------------------------------------------------------
       //! Get handler sid
       //!
       //! return sid of the corresponding request, otherwise 0
       //------------------------------------------------------------------------
-      virtual uint16_t GetSid() const;
+      virtual uint16_t GetSid() const override;
 
       //------------------------------------------------------------------------
       //! Process the message if it was "taken" by the examine action
       //!
       //! @param msg the message to be processed
       //------------------------------------------------------------------------
-      virtual void Process();
+      virtual void Process() override;
 
       //------------------------------------------------------------------------
       //! Read message body directly from a socket - called if Examine returns
@@ -274,7 +275,7 @@ namespace XrdCl
       //------------------------------------------------------------------------
       virtual XRootDStatus ReadMessageBody( Message  *msg,
                                             Socket   *socket,
-                                            uint32_t &bytesRead );
+                                            uint32_t &bytesRead ) override;
 
       //------------------------------------------------------------------------
       //! Handle an event other that a message arrival
@@ -283,18 +284,18 @@ namespace XrdCl
       //! @param status    status info
       //------------------------------------------------------------------------
       virtual uint8_t OnStreamEvent( StreamEvent  event,
-                                     XRootDStatus status );
+                                     XRootDStatus status ) override;
 
       //------------------------------------------------------------------------
       //! The requested action has been performed and the status is available
       //------------------------------------------------------------------------
       virtual void OnStatusReady( const Message *message,
-                                  XRootDStatus   status );
+                                  XRootDStatus   status ) override;
 
       //------------------------------------------------------------------------
       //! Are we a raw writer or not?
       //------------------------------------------------------------------------
-      virtual bool IsRaw() const;
+      virtual bool IsRaw() const override;
 
       //------------------------------------------------------------------------
       //! Write message body directly to a socket - called if IsRaw returns
@@ -307,7 +308,7 @@ namespace XrdCl
       //!                  stError on failure
       //------------------------------------------------------------------------
       XRootDStatus WriteMessageBody( Socket   *socket,
-                                     uint32_t &bytesWritten );
+                                     uint32_t &bytesWritten ) override;
 
       //------------------------------------------------------------------------
       //! Called after the wait time for kXR_wait has elapsed
@@ -327,7 +328,7 @@ namespace XrdCl
       //------------------------------------------------------------------------
       //! Get a timestamp after which we give up
       //------------------------------------------------------------------------
-      time_t GetExpiration()
+      time_t GetExpiration() override
       {
         return pExpiration;
       }
@@ -429,7 +430,17 @@ namespace XrdCl
       //------------------------------------------------------------------------
       void PartialReceived();
 
+      void OnReadyToSend( [[maybe_unused]] Message *msg ) override
+      {
+        pSendingState = 0;
+      }
+
     private:
+
+      // bit flags used with pSendingState
+      static constexpr int kSendDone   = 0x0001;
+      static constexpr int kSawResp    = 0x0002;
+      static constexpr int kFinalResp  = 0x0004;
 
       //------------------------------------------------------------------------
       //! Recover error
@@ -642,6 +653,7 @@ namespace XrdCl
       RedirectTraceBack                      pRedirectTraceBack;
 
       bool                                   pMsgInFly;
+      std::atomic<int>                       pSendingState;
 
       //------------------------------------------------------------------------
       // true if MsgHandler is both in inQueue and installed in respective

src/XrdCl/XrdClXRootDTransport.cc
@@ -1671,6 +1671,12 @@ namespace XrdCl
                                      uint32_t   bytesSent,
                                      AnyObject &channelData )
   {
+    // Called when a message has been sent. For messages that return on a
+    // different pathid (and hence may use a different poller) it is possible
+    // that the server has already replied and the reply will trigger
+    // MessageReceived() before this method has been called. However for open
+    // and close this is never the case and this method is used for tracking
+    // only those.
     XRootDChannelInfo *info = 0;
     channelData.Get( info );
     XrdSysMutexHelper scopedLock( info->mutex );

commit 679f3196ca16351e4ef77937ca9231b49cfe6da4

[Cl] Reset state when resending a message. Fixes: 3a957083
Fixes lost response when an error occurs before the resend
of a requeued request. e.g. a requeued message can happen when
the client has been redirected or told to wait and resend a
request later by kxr_wait. An error before resend could be that
the stream connect is lost or the request times-out before resend.

src/XrdCl/XrdClXRootDMsgHandler.cc
@@ -914,7 +914,9 @@ namespace XrdCl
 
   //----------------------------------------------------------------------------
   // We're here when we requested sending something over the wire
-  // and there has been a status update on this action
+  // or other status update on this action.
+  // We can be called when message is still in out-queue, with an
+  // error status indicating message will not be sent.
   //----------------------------------------------------------------------------
   void XRootDMsgHandler::OnStatusReady( const Message *message,
                                         XRootDStatus   status )
@@ -922,6 +924,18 @@ namespace XrdCl
     Log *log = DefaultEnv::GetLog();
 
     const int sst = pSendingState.fetch_or( kSendDone );
+
+    // if we have already seen a response we can not be in the out-queue
+    // anymore, so we should be getting notified of a successful send.
+    // If not log and do our best to recover.
+    if( !status.IsOK() && ( ( sst & kFinalResp ) || ( sst & kSawResp ) ) )
+    {
+      log->Error( XRootDMsg, "[%s] Unexpected error for message %s. Trying to "
+                  "recover.", pUrl.GetHostId().c_str(),
+                  message->GetObfuscatedDescription().c_str() );
+      HandleError( status );
+      return;
+    }
 
     if( sst & kFinalResp )
     {
@@ -2129,6 +2143,13 @@ namespace XrdCl
   //----------------------------------------------------------------------------
   Status XRootDMsgHandler::RetryAtServer( const URL &url, RedirectEntry::Type entryType )
   {
+    // prepare to possibly be requeued in the out-queue for a different channel.
+    // reset sendingstate; this is reset by OnReadyToSend() when our message is
+    // removed from out-queue, however OnStatusReady() may be called before that
+    // in case something happens before sending has been attempted. (e.g. stream
+    // broken or request timeout)
+    pSendingState = 0;
+
     pResponse.reset();
     Log *log = DefaultEnv::GetLog();
 