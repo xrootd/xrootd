xrootd/xrootd

commit 58b8b6acc0384abbd0e1416305786c966bfafd8a

[CMake] Display C/C++ compile flags in configuration summary

cmake/XRootDSummary.cmake
@@ -29,11 +29,16 @@ component_status( XRDCL     ENABLE_XRDCL      TRUE_VAR )
 component_status( XRDCLHTTP ENABLE_XRDCLHTTP  DAVIX_FOUND )
 component_status( XRDEC     BUILD_XRDEC       TRUE_VAR )
 
+string(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" BUILD_TYPE_FLAGS)
+
 message( STATUS "----------------------------------------" )
 message( STATUS "Installation path: " ${CMAKE_INSTALL_PREFIX} )
 message( STATUS "C Compiler:        " ${CMAKE_C_COMPILER} )
 message( STATUS "C++ Compiler:      " ${CMAKE_CXX_COMPILER} )
 message( STATUS "Build type:        " ${CMAKE_BUILD_TYPE} )
+message( STATUS "C Flags:           " ${CMAKE_C_FLAGS} )
+message( STATUS "C++ Flags:         " ${CMAKE_CXX_FLAGS} )
+message( STATUS "Build Type Flags:  " ${BUILD_TYPE_FLAGS}} )
 message( STATUS "Plug-in version:   " ${PLUGIN_VERSION} )
 message( STATUS "" )
 message( STATUS "Ceph support:      " ${STATUS_CEPH} )

commit ac69d74e98936479808a41e45059a4159d2101cd

[CMake] Fix typo in BUILD_TYPE_FLAGS message
Fixes: 58b8b6acc0384abbd0e1416305786c966bfafd8a

cmake/XRootDSummary.cmake
@@ -38,7 +38,7 @@ message( STATUS "C++ Compiler:      " ${CMAKE_CXX_COMPILER} )
 message( STATUS "Build type:        " ${CMAKE_BUILD_TYPE} )
 message( STATUS "C Flags:           " ${CMAKE_C_FLAGS} )
 message( STATUS "C++ Flags:         " ${CMAKE_CXX_FLAGS} )
-message( STATUS "Build Type Flags:  " ${BUILD_TYPE_FLAGS}} )
+message( STATUS "Build Type Flags:  " ${${BUILD_TYPE_FLAGS}} )
 message( STATUS "Plug-in version:   " ${PLUGIN_VERSION} )
 message( STATUS "" )
 message( STATUS "Ceph support:      " ${STATUS_CEPH} )