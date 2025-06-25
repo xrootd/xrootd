xrootd/xrootd

commit b72ea25e480280973959492ae7ad21b33ea36db6

[SSS] Update sss protocol for new SecEntity object. [SSS] Fix some nagging keyid issues fixes #592 #590

src/XrdSecsss/XrdSecsssKT.cc
@@ -29,6 +29,7 @@
 /******************************************************************************/
 
 #include <fcntl.h>
+#include <iostream>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -43,7 +44,6 @@
 #include "XrdOuc/XrdOucStream.hh"
 #include "XrdOuc/XrdOucUtils.hh"
 #include "XrdSys/XrdSysE2T.hh"
-#include "XrdSys/XrdSysHeaders.hh"
   
 /******************************************************************************/
 /*                    S t a t i c   D e f i n i t i o n s                     */
@@ -200,7 +200,7 @@ int XrdSecsssKT::delKey(ktEnt &ktDel)
 /*                                g e t K e y                                 */
 /******************************************************************************/
   
-int XrdSecsssKT::getKey(ktEnt &theEnt)
+int XrdSecsssKT::getKey(ktEnt &theEnt, bool andKeyID)
 {
    ktEnt *ktP, *ktN;
 
@@ -209,19 +209,23 @@ int XrdSecsssKT::getKey(ktEnt &theEnt)
    myMutex.Lock();
    ktP = ktList;
 
-// Find first key by key name (used normally by clients) or by keyID
+// Find first key by key name (used normally by clients), by keyID, or both
 //
-   if (!*theEnt.Data.Name)
-      {if (theEnt.Data.ID >= 0) 
-          while(ktP && ktP->Data.ID != theEnt.Data.ID)  ktP = ktP->Next;
-      }
-      else {while(ktP && strcmp(ktP->Data.Name,theEnt.Data.Name)) ktP=ktP->Next;
-            while(ktP && ktP->Data.Exp <= time(0))
-                 {if (!(ktN=ktP->Next) 
-                  ||  strcmp(ktN->Data.Name,theEnt.Data.Name)) break;
-                  ktP = ktN;
-                 }
-           }
+     if (!*theEnt.Data.Name)
+        {if (theEnt.Data.ID >= 0)
+            while(ktP && ktP->Data.ID != theEnt.Data.ID)  ktP = ktP->Next;
+        }
+   else if (andKeyID)
+        {while((ktP && ktP->Data.ID != theEnt.Data.ID)
+            || strcmp(ktP->Data.Name,theEnt.Data.Name)) ktP=ktP->Next;
+        }
+   else {while(ktP && strcmp(ktP->Data.Name,theEnt.Data.Name)) ktP=ktP->Next;
+         while(ktP && ktP->Data.Exp <= time(0))
+              {if (!(ktN=ktP->Next) 
+               ||  strcmp(ktN->Data.Name,theEnt.Data.Name)) break;
+               ktP = ktN;
+              }
+        }
 
 // If we found a match, export it
 //
@@ -399,13 +403,13 @@ int XrdSecsssKT::eMsg(const char *epname, int rc,
                       const char *txt1, const char *txt2,
                       const char *txt3, const char *txt4)
 {
-              cerr <<"Secsss (" << epname <<"): ";
-              cerr <<txt1;
-   if (txt2)  cerr <<txt2;
-   if (txt3)  cerr <<txt3;
-   if (txt4)  cerr <<txt4;
-  {if (rc>0) {cerr <<"; " <<XrdSysE2T(rc);}}
-              cerr <<endl;
+              std::cerr <<"Secsss (" << epname <<"): ";
+              std::cerr <<txt1;
+   if (txt2)  std::cerr <<txt2;
+   if (txt3)  std::cerr <<txt3;
+   if (txt4)  std::cerr <<txt4;
+  {if (rc>0) {std::cerr <<"; " <<XrdSysE2T(rc);}}
+              std::cerr <<"\n" <<std::endl;
 
    return (rc ? (rc < 0 ? rc : -rc) : -1);
 }
@@ -682,6 +686,8 @@ while((tp = kTab.GetToken()) && !Prob)
               ktNew->Data.Opts|=ktEnt::usrGRP;
    if (!strcmp(ktNew->Data.User, "anybody"))
       ktNew->Data.Opts|=ktEnt::anyUSR;
+      else if (!strcmp(ktNew->Data.User, "allusers"))
+              ktNew->Data.Opts|=ktEnt::allUSR;
 
 // All done
 //

src/XrdOuc/XrdOucUtils.cc
@@ -341,6 +341,40 @@ int XrdOucUtils::genPath(char *buff, int blen, const char *path, const char *psf
 }
 
 /******************************************************************************/
+/*                                g e t G I D                                 */
+/******************************************************************************/
+  
+bool XrdOucUtils::getGID(const char *gName, gid_t &gID)
+{
+   struct group Grp, *result;
+   char buff[65536];
+
+   getgrnam_r(gName, &Grp, buff, sizeof(buff), &result);
+   if (!result) return false;
+
+   gID = Grp.gr_gid;
+   return true;
+}
+
+/******************************************************************************/
+/*                                g e t U I D                                 */
+/******************************************************************************/
+  
+bool XrdOucUtils::getUID(const char *uName, uid_t &uID, gid_t *gID)
+{
+   struct passwd pwd, *result;
+   char buff[16384];
+
+   getpwnam_r(uName, &pwd, buff, sizeof(buff), &result);
+   if (!result) return false;
+
+   uID = pwd.pw_uid;
+   if (gID) *gID = pwd.pw_gid;
+
+   return true;
+}
+  
+/******************************************************************************/
 /*                               G i d N a m e                                */
 /******************************************************************************/
   

src/XrdOuc/XrdOucUtils.hh
@@ -56,6 +56,10 @@ static char *genPath(const char *path, const char *inst, const char *psfx=0);
 
 static int   genPath(char *buff, int blen, const char *path, const char *psfx=0);
 
+static bool  getGID(const char *gName, gid_t &gID);
+
+static bool  getUID(const char *uName, uid_t &uID, gid_t *gID=0);
+
 static int   GidName(gid_t gID, char *gName, int gNsz, time_t keepT=0);
 
 static int   GroupName(gid_t gID, char *gName, int gNsz);

src/XrdSec/XrdSecEntity.cc
@@ -199,6 +199,7 @@ void XrdSecEntity::Reset(bool isnew, const char *spV)
    ueid  = 0;
    addrInfo = 0;
    tident = 0;
+   pident = 0;
    sessvar = 0;
    uid     = 0;
    gid     = 0;

src/XrdSec/XrdSecEntity.hh
@@ -80,6 +80,7 @@ public:
 unsigned int     ueid;                    //!< Unique ID of entity instance
 XrdNetAddrInfo  *addrInfo;                //!< Entity's connection details
 const    char   *tident;                  //!< Trace identifier always preset
+const    char   *pident;                  //!< Trace identifier (originator)
          void   *sessvar;                 //!< Plugin settable storage pointer,
                                           //!< now deprecated. Use settable
                                           //!< attribute objects instead.
@@ -172,7 +173,6 @@ std::vector<std::string> Keys();
 //! Constructor.
 //!
 //! @param  spName  - The name of the security protocol.
-//! @param  dpName  - The name of the data     protocol.
 //------------------------------------------------------------------------------
 
          XrdSecEntity(const char *spName=0) {Reset(true, spName);}

src/XrdSecsss/XrdSecProtocolsss.hh
@@ -39,6 +39,8 @@
 
 class XrdOucErrInfo;
 
+struct XrdSecsssEnt;
+
 class XrdSecProtocolsss : public XrdSecProtocol
 {
 public:
@@ -69,11 +71,10 @@ static  char *Load_Client(XrdOucErrInfo *erp, const char *Parms);
 
 static  char *Load_Server(XrdOucErrInfo *erp, const char *Parms);
 
-static  void  setOpts(int opts) {options = opts;}
-
         XrdSecProtocolsss(const char *hname, XrdNetAddrInfo &endPoint)
                          : XrdSecProtocol("sss"),
-                           keyTab(0), Crypto(0), idBuff(0), Sequence(0)
+                           keyTab(0), Crypto(0), idBuff(0), dataOpts(0),
+                           Sequence(0), v2EndPnt(false)
                          {urName = strdup(hname); setIP(endPoint);}
 
 struct Crypto {const char *cName; char cType;};
@@ -82,12 +83,16 @@ private:
        ~XrdSecProtocolsss() {} // Delete() does it all
 
 int                Decode(XrdOucErrInfo *error, XrdSecsssKT::ktEnt &decKey,
-                          char *iBuff, XrdSecsssRR_Data *rrData, int iSize);
+                          char *iBuff, XrdSecsssRR_DataHdr *rrDHdr, int iSize);
 XrdSecCredentials *Encode(XrdOucErrInfo *error, XrdSecsssKT::ktEnt &encKey,
-                          XrdSecsssRR_Hdr *rrHdr, XrdSecsssRR_Data *rrData,
+                          XrdSecsssRR_Hdr *rrHdr, XrdSecsssRR_DataHdr *rrDHdr,
                           int dLen);
-int            getCred(XrdOucErrInfo *, XrdSecsssRR_Data &);
-int            getCred(XrdOucErrInfo *, XrdSecsssRR_Data &, XrdSecParameters *);
+
+int            getCred(XrdOucErrInfo *, XrdSecsssRR_DataHdr *&,
+                       const char    *, const char *);
+int            getCred(XrdOucErrInfo *, XrdSecsssRR_DataHdr *&,
+                       const char    *, const char *, XrdSecParameters *);
+
 char          *getLID(char *buff, int blen);
 static
 XrdCryptoLite *Load_Crypto(XrdOucErrInfo *erp, const char *eN);
@@ -100,15 +105,14 @@ void           setIP(XrdNetAddrInfo &endPoint);
 static struct Crypto  CryptoTab[];
 
 static const char    *myName;
-static int            myNLen;
        char          *urName;
        char           urIP[48];  // New format
        char           urIQ[48];  // Old format
-static int            options;
-static int            isMutual;
 static int            deltaTime;
-static int            ktFixed;
-       XrdNetAddrInfo epAddr;
+static bool           isMutual;
+static bool           isMapped;
+static bool           ktFixed;
+       XrdNetAddrInfo *epAddr;
 
 static XrdSecsssKT   *ktObject;  // Both:   Default Key Table object
        XrdSecsssKT   *keyTab;    // Both:   Active  Key Table
@@ -117,9 +121,12 @@ static XrdCryptoLite *CryptObj;  // Both:   Default Cryptogrophy object
        XrdCryptoLite *Crypto;    // Both:   Active  Cryptogrophy object
 
 static XrdSecsssID   *idMap;     // Client: Registry
+static char          *aProts;    // Server: Allowable cloned auth protocols
        char          *idBuff;    // Server: Underlying buffer for XrdSecEntity
-static char          *staticID;  // Client: Static identity
-static int            staticIDsz;// Client: Static identity length
-       int            Sequence;  // Client: Check for sequencing
+static XrdSecsssEnt  *staticID;  // Client: Static identity
+       int            dataOpts;  // Client: idMap Find() options
+       char           Sequence;  // Client: Check for sequencing
+       bool           v2EndPnt;  // Server: Client is version 2
+                                 // Client: Server is version 2
 };
 #endif

src/XrdSecsss/XrdSecsssAdmin.cc
@@ -29,6 +29,7 @@
 /******************************************************************************/
   
 #include <ctype.h>
+#include <iostream>
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -40,7 +41,6 @@
 
 #include "XrdOuc/XrdOucErrInfo.hh"
 #include "XrdSys/XrdSysE2T.hh"
-#include "XrdSys/XrdSysHeaders.hh"
 #include "XrdSys/XrdSysPlatform.hh"
 #include "XrdSys/XrdSysTimer.hh"
 
@@ -50,7 +50,7 @@
 /*                               D e f i n e s                                */
 /******************************************************************************/
   
-#define eMsg(x) cerr <<XrdpgmName <<": " <<x << endl
+#define eMsg(x) std::cerr <<XrdpgmName <<": " <<x << std::endl
 
 struct XrdsecsssAdmin_Opts
       {XrdSecsssKT *kTab;
@@ -235,8 +235,8 @@ int isNo(int dflt, const char *Msg1, const char *Msg2, const char *Msg3)
 {
    char Answer[8];
 
-   cerr <<XrdpgmName <<": " <<Msg1 <<Msg2 <<Msg3;
-   cin.getline(Answer, sizeof(Answer));
+   std::cerr <<XrdpgmName <<": " <<Msg1 <<Msg2 <<Msg3;
+   std::cin.getline(Answer, sizeof(Answer));
    if (!*Answer) return dflt;
 
    if (!strcmp("y",Answer) || !strcmp("ye",Answer) || !strcmp("yes",Answer))
@@ -257,10 +257,10 @@ void Usage(int rc, const char *opn, const char *opv)
           else  eMsg(opn <<" argument not specified.");
       }
 
-cerr <<"\nUsage:   " <<XrdpgmName <<" [options] action\n";
-cerr <<"\nOptions: [-d] [-g grpname] [-h hold] [-k keyname] [-l keylen] [-n keynum]";
-cerr <<"\n         [-s {c|g|k|n|u|x}] [-u usrname] [-x {days | mm/dd/yy}]" <<endl;
-cerr <<"\nActions: {add | del | install | list} [keyfn]" <<endl;
+std::cerr <<"\nUsage:   " <<XrdpgmName <<" [options] action\n";
+std::cerr <<"\nOptions: [-d] [-g grpname] [-h hold] [-k keyname] [-l keylen] [-n keynum]";
+std::cerr <<"\n         [-s {c|g|k|n|u|x}] [-u usrname] [-x {days | mm/dd/yy}]" <<std::endl;
+std::cerr <<"\nActions: {add | del | install | list} [keyfn]" <<std::endl;
 exit(rc);
 }
 
@@ -507,13 +507,13 @@ int  XrdSecsssAdmin_lstKey(XrdsecsssAdmin_Opts &Opt)
    ktP = ktSort;
    while(ktP)
         {if (XrdSecsssAdmin_isKey(Opt, ktP))
-            {if (pHdr) {cout <<Hdr1 <<Hdr2; pHdr = 0;}
+            {if (pHdr) {std::cout <<Hdr1 <<Hdr2; pHdr = 0;}
              sprintf(buff, "%11lld %3d ", (ktP->Data.ID & 0x7fffffff), ktP->Data.Len);
              strftime(crbuff, sizeof(crbuff), crfmt, localtime(&ktP->Data.Crt));
              if (!ktP->Data.Exp) strcpy(exbuff, "--------");
                 else strftime(exbuff,sizeof(exbuff),exfmt,localtime(&ktP->Data.Exp));
-             cout <<buff <<crbuff <<' ' <<exbuff <<' ' <<ktP->Data.Name <<' '
-                  <<ktP->Data.User <<' ' <<ktP->Data.Grup <<endl;
+             std::cout <<buff <<crbuff <<' ' <<exbuff <<' ' <<ktP->Data.Name <<' '
+                       <<ktP->Data.User <<' ' <<ktP->Data.Grup <<std::endl;
             }
          ktP = ktP->Next;
         }

commit c96e3b5cdb45deceb8d711128f57dd69c0951df9

[XrdSecsss] Fix null pointer dereference in XrdSecsssKT::getKey()
The while loop tests for ktP != null only for the first half of the
condition, so strcmp(ktP->Data.Name,theEnt.Data.Name) causes a crash
when XrdSecsssKT::getKey() is called with a SecEntity such that
theEnt->Next == nullptr and bool andKeyID == true:

Stack trace of thread 1646722:
 0  __strcmp_avx2 (libc.so.6 + 0x13eeeb)
 1  XrdSecsssKT::getKey(XrdSecsssKT::ktEnt&, bool) (libXrdUtils.so.3 + 0xfee0e)
 2  XrdSecProtocolsss::Decode(XrdOucErrInfo*, XrdSecsssKT::ktEnt&, char*, XrdSecsssRR_DataHdr*, int) (libXrdSecsss-5.so + 0x617c)
 3  XrdSecProtocolsss::Authenticate(XrdSecBuffer*, XrdSecBuffer**, XrdOucErrInfo*) (libXrdSecsss-5.so + 0x6b90)
 4  XrdXrootdProtocol::do_Auth() (libXrdServer.so.3 + 0xd352f)
 5  XrdXrootdProtocol::Process2() (libXrdServer.so.3 + 0xc2443)
 6  XrdXrootdProtocol::Process(XrdLink*) (libXrdServer.so.3 + 0xc29a3)
 7  XrdLinkXeq::DoIt() (libXrdUtils.so.3 + 0x76bc6)
 8  XrdLink::setProtocol(XrdProtocol*, bool, bool) (libXrdUtils.so.3 + 0x7325b)
 9  XrdMain::DoIt() (/usr/bin/xrootd + 0x1166c)
 10 XrdScheduler::Run() (libXrdUtils.so.3 + 0x7a8fd)
 11 XrdStartWorking(void*) (libXrdUtils.so.3 + 0x7a97e)
 12 XrdSysThread_Xeq (libXrdUtils.so.3 + 0xbaeef)
 13 start_thread (libc.so.6 + 0x83d91)
 14 __clone3 (libc.so.6 + 0xec4ec)

Issue: #1683
Fixes: b72ea25e480280973959492ae7ad21b33ea36db6

src/XrdSecsss/XrdSecsssKT.cc
@@ -220,8 +220,8 @@ int XrdSecsssKT::getKey(ktEnt &theEnt, bool andKeyID)
             while(ktP && ktP->Data.ID != theEnt.Data.ID)  ktP = ktP->Next;
         }
    else if (andKeyID)
-        {while((ktP && ktP->Data.ID != theEnt.Data.ID)
-            || strcmp(ktP->Data.Name,theEnt.Data.Name)) ktP=ktP->Next;
+        {while(ktP && ((ktP->Data.ID != theEnt.Data.ID)
+            || strcmp(ktP->Data.Name,theEnt.Data.Name))) ktP=ktP->Next;
         }
    else {while(ktP && strcmp(ktP->Data.Name,theEnt.Data.Name)) ktP=ktP->Next;
          while(ktP && ktP->Data.Exp <= time(0))