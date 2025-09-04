fc61a5b5b22bc7819c99e5629e1654b3157f5aad
alja <amraktadel@ucsd.edu>
2025-09-03 20:36:48 -0700

    [Pfc] Use XrdPosixExtra::FSctl for etag cache validation.

--- src/XrdPfc/XrdPfcFile.cc
+++ src/XrdPfc/XrdPfcFile.cc
@@ -27,7 +27,10 @@
 #include "XrdSys/XrdSysTimer.hh"
 #include "XrdOss/XrdOss.hh"
 #include "XrdOuc/XrdOucEnv.hh"
+#include "XrdOuc/XrdOucJson.hh"
 #include "XrdSfs/XrdSfsInterface.hh"
+
+#include "XrdCl/XrdClFileStateHandler.hh"
 
 #include <cstdio>
 #include <sstream>
@@ -135,10 +138,10 @@ void File::Close()
 
 //------------------------------------------------------------------------------
 
-File* File::FileOpen(const std::string &path, long long offset, long long fileSize)
+File* File::FileOpen(const std::string &path, long long offset, long long fileSize, XrdOucCacheIO *inputIO)
 {
    File *file = new File(path, offset, fileSize);
-   if ( ! file->Open())
+   if ( ! file->Open(inputIO))
    {
       delete file;
       file = 0;
@@ -420,7 +423,7 @@ void File::RemoveIO(IO *io)
 
 //------------------------------------------------------------------------------
 
-bool File::Open()
+bool File::Open(XrdOucCacheIO *inputIO)
 {
    // Sets errno accordingly.
 
@@ -531,6 +534,30 @@ bool File::Open()
       m_cfi.Write(m_info_file, ifn.c_str());
       m_info_file->Fsync();
       cache()->WriteFileSizeXAttr(m_info_file->getFD(), m_file_size);
+
+      // access and write cache-control attributes
+      std::string  responseFctl;
+      int resFctl = inputIO->Fcntl(XrdOucCacheOp::Code::QFinfo, "head", responseFctl);
+      if (resFctl == 0)
+      {
+         std::string cc_str = responseFctl;
+         nlohmann::json cc_json =  nlohmann::json::parse(cc_str);
+         if (cc_json.contains("max-age"))
+         {
+            time_t ma = cc_json["max-age"];
+            ma += time(NULL);
+            cc_json["expire"] = ma;
+            cc_str = cc_json.dump();
+         }
+         TRACE(Error, "GetFile() XrdCl::File::Fcntl value " << cc_str);
+         cache()->WriteCacheControlXAttr(m_info_file->getFD(), nullptr, cc_str);
+      }
+      else if (resFctl != kXR_Unsupported)
+      {
+         // Query XrdCl::QueryCode::Head is optional, print error only if informatin is supported
+         TRACE(Error, "GetFile() XrdCl::File::Fcntl query XrdCl::QueryCode::FInfo failed " << inputIO->Path());
+      }
+
       TRACEF(Debug, tpfx << "Creating new file info, data size = " <<  m_file_size << " num blocks = "  << m_cfi.GetNBlocks());
    }
    else
