xrootd/xrootd

commit 41724cad1f5a31ce5d0bd7d5077e64287d6e164e

Remove Davix-based XrdClHttp and rename XrdClCurl to XrdClHttp

cmake/XRootDFindLibs.cmake
@@ -78,19 +78,6 @@ if( ENABLE_PYTHON OR PYPI_BUILD )
   endif()
 endif()
 
-if( ENABLE_XRDCLHTTP )
-  if( FORCE_ENABLED )
-    find_package( Davix REQUIRED )
-  else()
-    find_package( Davix )
-  endif()
-  if( DAVIX_FOUND )
-    set( BUILD_XRDCLHTTP TRUE )
-  else()
-    set( BUILD_XRDCLHTTP FALSE )
-  endif()
-endif()
-
 if( ENABLE_XRDOSSARC )
   if( FORCE_ENABLED )
     find_package( libzip REQUIRED )

commit 00261ac8fae3b2241c8d84ef6abd5eef1975575f

[CMake] Fix condition to enable HTTP server tests
This was missed missed during renaming of XrdClCurl to XrdClHttp.

Fixes: 41724cad1f5a31ce5d0bd7d5077e64287d6e164e

cmake/XRootDFindLibs.cmake
@@ -106,7 +106,7 @@ if( ENABLE_TESTS )
     #  set(ENABLE_FUSE_TESTS TRUE CACHE BOOL "Enable FUSE tests")
     #endif()
 
-    if( BUILD_HTTP AND BUILD_XRDCLHTTP )
+    if( BUILD_HTTP )
       set(ENABLE_HTTP_TESTS TRUE CACHE BOOL "Enable HTTP tests")
     endif()
   endif()