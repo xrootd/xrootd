5a4a011259170539aa315cf48679ef9f919bbbf4
Cedric Caffy <cedric.caffy@cern.ch>
2026-03-04 17:58:48 +0100

    Better use of HAVE_STATX and remove autoStat implementation in XrdSsi

--- src/XrdSsi/XrdSsiDir.cc
+++ src/XrdSsi/XrdSsiDir.cc
@@ -157,15 +157,6 @@ int XrdSsiDir::autoStat(struct stat *buf)
    return XrdSsiUtils::Emsg(epname, EBADF, epname, "???", error);
 }
 
-int XrdSsiDir::autoStat(XrdSysStatx *buf, unsigned int mask) {
-   struct stat statbuf;
-   int retc = autoStat(&statbuf);
-   if (retc == SFS_OK) {
-      XrdSysStatxHelpers::Stat2Statx(statbuf,*buf);
-   }
-   return retc;
-}
-
 /******************************************************************************/
 /*                                 F N a m e                                  */
 /******************************************************************************/
