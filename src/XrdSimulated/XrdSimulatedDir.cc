35cec66d94f60e935bda0e1979cd71d14580ec4e
Diogo Mattioli <diogo.mattioli@cern.ch>
2026-04-22 15:12:00 +0200

    chore: adds dummy logging

--- src/XrdSimulated/XrdSimulatedDir.cc
+++ src/XrdSimulated/XrdSimulatedDir.cc
@@ -1,21 +1,31 @@
 #include "XrdSimulatedDir.hh"
+#include "XrdSys/XrdSysError.hh"
+
+namespace XrdGlobal
+{
+    extern XrdSysError Log;
+}
 
 int XrdSimulatedDir::Opendir(const char *path, XrdOucEnv &env)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedDir::Readdir(char *buff, int blen)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedDir::StatRet(struct stat *buff)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedDir::Close(long long *retsz)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
--- src/XrdSimulated/XrdSimulatedFile.cc
+++ src/XrdSimulated/XrdSimulatedFile.cc
@@ -1,111 +1,139 @@
 #include "XrdSimulatedFile.hh"
+#include "XrdSys/XrdSysError.hh"
+
+namespace XrdGlobal
+{
+    extern XrdSysError Log;
+}
 
 int XrdSimulatedFile::StatRet(struct stat *buff)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Clone(XrdOssDF& srcFile)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Clone(const std::vector<XrdOucCloneSeg> &cVec)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Fchmod(mode_t mode)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 void XrdSimulatedFile::Flush() 
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
 }
 
 int XrdSimulatedFile::Fstat(struct stat *buf)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Fsync()
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Fsync(XrdSfsAio *aiop)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Ftruncate(unsigned long long flen)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 off_t XrdSimulatedFile::getMmap(void **addr)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     *addr = 0; 
     return 0;
 }
 
 int XrdSimulatedFile::Open(const char *path, int Oflag, mode_t Mode, XrdOucEnv &env)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::pgRead (void* buffer, off_t offset, size_t rdlen, uint32_t* csvec, uint64_t opts)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::pgRead (XrdSfsAio* aioparm, uint64_t opts)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::pgWrite(void* buffer, off_t offset, size_t wrlen, uint32_t* csvec, uint64_t opts)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::pgWrite(XrdSfsAio* aioparm, uint64_t opts)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::Read(off_t offset, size_t size)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::Read(void *buffer, off_t offset, size_t size)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Read(XrdSfsAio *aiop)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::ReadRaw(void *buffer, off_t offset, size_t size)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 ssize_t XrdSimulatedFile::Write(const void *buffer, off_t offset, size_t size)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Write(XrdSfsAio *aiop)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulatedFile::Close(long long *retsz)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
