bc81727788d823c2e12f4d70527de987b729b4cc
Diogo Mattioli <diogo.mattioli@cern.ch>
2026-04-22 15:12:00 +0200

    [XrdSimulated] adds dummy logging

--- src/XrdSimulated/XrdSimulated.cc
+++ src/XrdSimulated/XrdSimulated.cc
@@ -1,6 +1,12 @@
 #include "XrdSimulated.hh"
-
 #include "XrdVersion.hh"
+
+#include "XrdSys/XrdSysError.hh"
+
+namespace XrdGlobal
+{
+    extern XrdSysError Log;
+}
 
 XrdVERSIONINFO(XrdOssGetStorageSystem, XrdSimulated);
 
@@ -14,55 +20,66 @@ extern "C"
 
 XrdOssDF *XrdSimulated::newDir(const char *tident)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return nullptr;
 }
 
 XrdOssDF *XrdSimulated::newFile(const char *tident)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return nullptr;
 }
 
 int XrdSimulated::Chmod(const char * path, mode_t mode, XrdOucEnv *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Create(const char *tid, const char *path, mode_t mode, XrdOucEnv &env, int opts)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Init(XrdSysLogger *lp, const char *cfn)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Mkdir(const char *path, mode_t mode, int mkpath, XrdOucEnv  *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Remdir(const char *path, int Opts, XrdOucEnv *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Rename(const char *oPath, const char *nPath, XrdOucEnv  *oEnvP, XrdOucEnv *nEnvP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Stat(const char *path, struct stat *buff, int opts, XrdOucEnv *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Truncate(const char *path, unsigned long long fsize, XrdOucEnv *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
 
 int XrdSimulated::Unlink(const char *path, int Opts, XrdOucEnv *envP)
 {
+    XrdGlobal::Log.Say(__PRETTY_FUNCTION__);
     return 0;
 }
