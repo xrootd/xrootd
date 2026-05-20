xrootd/xrootd

commit f481c34b60509eb03e2673c55a733d902f71c1ca

[XrdMacaroons] Fix leak of fd file descriptor during configuration
Unlike what the comment says, fp is not closed unless BIO_new_fp is
passed in the BIO_CLOSE is used on open. Use BIO_new_file instead so
ownership is clearer and ensure it's always freed at the end.

src/XrdMacaroons/XrdMacaroonsConfigure.cc
@@ -180,39 +180,27 @@ bool Handler::xsitename(XrdOucStream &config_obj, XrdSysError *log, std::string 
 bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string &secret)
 {
   char *val = config_obj.GetWord();
-  if (!val || !val[0])
-  {
+
+  if (!val || !val[0]) {
     log->Emsg("Config", "Shared secret key not specified");
     return false;
   }
 
-  FILE *fp = fopen(val, "rb");
-
-  if (fp == nullptr) {
-    log->Emsg("Config", errno, "open shared secret key file", val);
+  BIO *bio = BIO_new_file(val, "rb");
+  if (!bio) {
+    log->Emsg("Config", "Failed to open shared secret key file", val);
     return false;
   }
 
-  BIO *bio, *b64, *bio_out;
-  char inbuf[512];
-  int inlen;
-
-  b64 = BIO_new(BIO_f_base64());
-  if (!b64)
-  {
+  BIO *b64 = BIO_new(BIO_f_base64());
+  if (!b64) {
+    BIO_free_all(bio);
     log->Emsg("Config", "Failed to allocate base64 filter");
     return false;
   }
-  bio = BIO_new_fp(fp, 0); // fp will be closed when BIO is freed.
-  if (!bio)
-  {
-    BIO_free_all(b64);
-    log->Emsg("Config", "Failed to allocate BIO filter");
-    return false;
-  }
-  bio_out = BIO_new(BIO_s_mem());
-  if (!bio_out)
-  {
+
+  BIO *bio_out = BIO_new(BIO_s_mem());
+  if (!bio_out) {
     BIO_free_all(b64);
     BIO_free_all(bio);
     log->Emsg("Config", "Failed to allocate BIO output");
@@ -220,8 +208,11 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
   }
 
   BIO_push(b64, bio);
-  while ((inlen = BIO_read(b64, inbuf, 512)) > 0)
-  {
+
+  int inlen;
+  char inbuf[512];
+
+  while ((inlen = BIO_read(b64, inbuf, 512)) > 0) {
     if (inlen < 0) {
       if (errno == EINTR) continue;
       break;
@@ -229,26 +220,31 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
       BIO_write(bio_out, inbuf, inlen);
     }
   }
+
   if (inlen < 0) {
-    BIO_free_all(b64);
     BIO_free_all(bio_out);
+    BIO_free_all(b64);
+    BIO_free_all(bio);
     log->Emsg("Config", errno, "read secret key.");
     return false;
   }
+
   if (!BIO_flush(bio_out)) {
-    BIO_free_all(b64);
     BIO_free_all(bio_out);
+    BIO_free_all(b64);
+    BIO_free_all(bio);
     log->Emsg("Config", errno, "flush secret key.");
     return false;
   }
 
   char *decoded;
   long data_len = BIO_get_mem_data(bio_out, &decoded);
-  BIO_free_all(b64);
 
   secret = std::string(decoded, data_len);
 
   BIO_free_all(bio_out);
+  BIO_free_all(b64);
+  BIO_free_all(bio);
 
   if (secret.size() < 32) {
     log->Emsg("Config", "Secret key is too short; must be 32 bytes long.  Try running 'openssl rand -base64 -out", val, "64' to generate a new key");

commit db9ee1c3329986b105e013c7750e5bc629f3ae13

[XrdMacaroons] Fix double free bug when reading macaroon secret
The call BIO_push(b64, bio) links the two BIOs, so calling
BIO_free_all() on both of them later causes a double free with
some versions of OpenSSL.

Fixes: f481c34b60509eb03e2673c55a733d902f71c1ca
Closes: #2797

src/XrdMacaroons/XrdMacaroonsConfigure.cc
@@ -194,15 +194,15 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
 
   BIO *b64 = BIO_new(BIO_f_base64());
   if (!b64) {
-    BIO_free_all(bio);
+    BIO_free(bio);
     log->Emsg("Config", "Failed to allocate base64 filter");
     return false;
   }
 
   BIO *bio_out = BIO_new(BIO_s_mem());
   if (!bio_out) {
-    BIO_free_all(b64);
-    BIO_free_all(bio);
+    BIO_free(b64);
+    BIO_free(bio);
     log->Emsg("Config", "Failed to allocate BIO output");
     return false;
   }
@@ -224,7 +224,6 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
   if (inlen < 0) {
     BIO_free_all(bio_out);
     BIO_free_all(b64);
-    BIO_free_all(bio);
     log->Emsg("Config", errno, "read secret key.");
     return false;
   }
@@ -232,7 +231,6 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
   if (!BIO_flush(bio_out)) {
     BIO_free_all(bio_out);
     BIO_free_all(b64);
-    BIO_free_all(bio);
     log->Emsg("Config", errno, "flush secret key.");
     return false;
   }
@@ -244,7 +242,6 @@ bool Handler::xsecretkey(XrdOucStream &config_obj, XrdSysError *log, std::string
 
   BIO_free_all(bio_out);
   BIO_free_all(b64);
-  BIO_free_all(bio);
 
   if (secret.size() < 32) {
     log->Emsg("Config", "Secret key is too short; must be 32 bytes long.  Try running 'openssl rand -base64 -out", val, "64' to generate a new key");