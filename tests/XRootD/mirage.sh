ab80e081eee43b804592b82bb5507833ef4bbe72
Diogo Mattioli <diogomattioli@gmail.com>
2026-05-26 14:43:08 +0000

    test: initial testing

--- tests/XRootD/mirage.sh
+++ tests/XRootD/mirage.sh
@@ -15,9 +15,41 @@ function test_mirage() {
 	ORIGIN_HOST="root://localhost:6543"
 
 	assert xrdcp -f "${SOURCE_DIR}"/mirage.cfg "${ORIGIN_HOST}//mirage.cfg"
-	assert xrdcp -f "${ORIGIN_HOST}"//mirage.cfg mirage.origin.cfg
-	assert diff -u "${SOURCE_DIR}"/mirage.cfg mirage.origin.cfg
 
-	assert xrdcp -f "${HOST}"//mirage.cfg host.mirage.cfg
-	assert diff -u "${SOURCE_DIR}"/mirage.cfg host.mirage.cfg
+	# Local and remote size match
+	local_size=$(stat -c '%s' "${SOURCE_DIR}"/mirage.cfg)
+	remote_size=$(xrdcp -f "${ORIGIN_HOST}"//mirage.cfg - | wc -c)
+	assert [ "$local_size" = "$remote_size" ]
+
+	# Stat returns correct file size
+	local_size=$(stat -c '%s' "${SOURCE_DIR}"/mirage.cfg)
+	remote_size=$(xrdfs "${ORIGIN_HOST}"/ stat /mirage.cfg | grep -i size | awk '{print $2}')
+	assert [ "$local_size" = "$remote_size" ]
+
+	# Truncate file to 1000 bytes
+	assert xrdfs "${ORIGIN_HOST}"/ truncate /mirage.cfg 1000
+	remote_size=$(xrdfs "${ORIGIN_HOST}"/ stat /mirage.cfg | grep -i size | awk '{print $2}')
+	assert [ "$remote_size" -eq 1000 ]
+
+	# Single char pattern
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg set pattern='a'
+	occurrences=$(xrdcp -f "${ORIGIN_HOST}"//mirage.cfg - | grep -o a | wc -l)
+	assert [ "$occurrences" -eq 1000 ]
+
+	# String pattern
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg set pattern='abcde'
+	occurrences=$(xrdcp -f "${ORIGIN_HOST}"//mirage.cfg - | grep -o abcde | wc -l)
+	assert [ "$occurrences" -eq 200 ]
+
+	# Open return code
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg set open.return_code=12
+	output=$(xrdcp -f "${ORIGIN_HOST}"//mirage.cfg - 2>&1)
+	[[ "$output" == *"cannot allocate memory"* ]]
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg del open.return_code
+
+	# Read return code
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg set read.return_code=12
+	output=$(xrdcp -f "${ORIGIN_HOST}"//mirage.cfg - 2>&1)
+	[[ "$output" == *"cannot allocate memory"* ]]
+	assert xrdfs "${ORIGIN_HOST}"/ xattr /mirage.cfg del read.return_code
 }
