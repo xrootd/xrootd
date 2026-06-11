9387d5f30ba0cd59bcb9fc69952400a6f6a8ca23
Diogo Mattioli <diogo.mattioli@cern.ch>
2026-06-11 11:05:36 +0200

    tests: expect instead of assert to let tests run until the end

--- tests/XrdOssMirageTests/XrdOssMirageXAttrTests.cc
+++ tests/XrdOssMirageTests/XrdOssMirageXAttrTests.cc
@@ -10,12 +10,12 @@ class XrdOssMirageXAttrFixture : public XrdOssMirageFixture
 
 TEST_F(XrdOssMirageXAttrFixture, SetPropertiesReturnsOk)
 {
-    ASSERT_EQ(0, xattr.Set("U.open.return_code", "1", 1, "/dummy", 0, 0));
-    ASSERT_EQ(0, xattr.Set("U.read.return_code", "2", 1, "/dummy", 0, 0));
-    ASSERT_EQ(0, xattr.Set("U.read.return_position", "3", 1, "/dummy", 0, 0));
-    ASSERT_EQ(0, xattr.Set("U.write.return_code", "4", 1, "/dummy", 0, 0));
-    ASSERT_EQ(0, xattr.Set("U.write.return_position", "5", 1, "/dummy", 0, 0));
-    ASSERT_EQ(0, xattr.Set("U.pattern", "6", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.open.return_code", "1", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.read.return_code", "2", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.read.return_position", "3", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.write.return_code", "4", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.write.return_position", "5", 1, "/dummy", 0, 0));
+    EXPECT_EQ(0, xattr.Set("U.pattern", "6", 1, "/dummy", 0, 0));
 }
 
 TEST_F(XrdOssMirageXAttrFixture, SetPropertiesChangesTheirValues)
@@ -29,12 +29,12 @@ TEST_F(XrdOssMirageXAttrFixture, SetPropertiesChangesTheirValues)
 
     auto entry = oss.get_entry_read("/dummy").value();
 
-    ASSERT_EQ(1, entry.open.return_code);
-    ASSERT_EQ(2, entry.read.return_code);
-    ASSERT_EQ(3, entry.read.return_position);
-    ASSERT_EQ(4, entry.write.return_code);
-    ASSERT_EQ(5, entry.write.return_position);
-    ASSERT_EQ("6", entry.pattern);
+    EXPECT_EQ(1, entry.open.return_code);
+    EXPECT_EQ(2, entry.read.return_code);
+    EXPECT_EQ(3, entry.read.return_position);
+    EXPECT_EQ(4, entry.write.return_code);
+    EXPECT_EQ(5, entry.write.return_position);
+    EXPECT_EQ("6", entry.pattern);
 }
 
 TEST_F(XrdOssMirageXAttrFixture, SetInvalidPropertyReturnsINVALError)
@@ -73,11 +73,11 @@ TEST_F(XrdOssMirageXAttrFixture, SetPropertiesWithValuesOutOfRangeDoesNotChangeT
 
     auto entry = oss.get_entry_read("/dummy").value();
 
-    ASSERT_EQ(0, entry.open.return_code);
-    ASSERT_EQ(0, entry.read.return_code);
-    ASSERT_EQ(0, entry.read.return_position);
-    ASSERT_EQ(0, entry.write.return_code);
-    ASSERT_EQ(0, entry.write.return_position);
+    EXPECT_EQ(0, entry.open.return_code);
+    EXPECT_EQ(0, entry.read.return_code);
+    EXPECT_EQ(0, entry.read.return_position);
+    EXPECT_EQ(0, entry.write.return_code);
+    EXPECT_EQ(0, entry.write.return_position);
 }
 
 TEST_F(XrdOssMirageXAttrFixture, GetPropertiesReturnsNumberOfReadBytes)
@@ -93,12 +93,12 @@ TEST_F(XrdOssMirageXAttrFixture, GetPropertiesReturnsNumberOfReadBytes)
     }
 
     char value = 0;
-    ASSERT_EQ(1, xattr.Get("U.open.return_code", &value, 1, "/dummy", 0));
-    ASSERT_EQ(1, xattr.Get("U.read.return_code", &value, 1, "/dummy", 0));
-    ASSERT_EQ(1, xattr.Get("U.read.return_position", &value, 1, "/dummy", 0));
-    ASSERT_EQ(1, xattr.Get("U.write.return_code", &value, 1, "/dummy", 0));
-    ASSERT_EQ(1, xattr.Get("U.write.return_position", &value, 1, "/dummy", 0));
-    ASSERT_EQ(1, xattr.Get("U.pattern", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.open.return_code", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.read.return_code", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.read.return_position", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.write.return_code", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.write.return_position", &value, 1, "/dummy", 0));
+    EXPECT_EQ(1, xattr.Get("U.pattern", &value, 1, "/dummy", 0));
 }
 
 TEST_F(XrdOssMirageXAttrFixture, GetPropertiesReturnsTheirChangedValues)
@@ -115,22 +115,22 @@ TEST_F(XrdOssMirageXAttrFixture, GetPropertiesReturnsTheirChangedValues)
 
     char value = 0;
     xattr.Get("U.open.return_code", &value, 1, "/dummy", 0);
-    ASSERT_EQ('1', value);
+    EXPECT_EQ('1', value);
 
     xattr.Get("U.read.return_code", &value, 1, "/dummy", 0);
-    ASSERT_EQ('2', value);
+    EXPECT_EQ('2', value);
 
     xattr.Get("U.read.return_position", &value, 1, "/dummy", 0);
-    ASSERT_EQ('3', value);
+    EXPECT_EQ('3', value);
 
     xattr.Get("U.write.return_code", &value, 1, "/dummy", 0);
-    ASSERT_EQ('4', value);
+    EXPECT_EQ('4', value);
 
     xattr.Get("U.write.return_position", &value, 1, "/dummy", 0);
-    ASSERT_EQ('5', value);
+    EXPECT_EQ('5', value);
 
     xattr.Get("U.pattern", &value, 1, "/dummy", 0);
-    ASSERT_EQ('6', value);
+    EXPECT_EQ('6', value);
 }
 
 TEST_F(XrdOssMirageXAttrFixture, GetInvalidPropertyReturnsINVALError)
@@ -152,12 +152,12 @@ TEST_F(XrdOssMirageXAttrFixture, GetPropertyOfAFileThatIsBeingWrittenReturnsINVA
 
 TEST_F(XrdOssMirageXAttrFixture, DeletePropertiesReturnsOk)
 {
-    ASSERT_EQ(0, xattr.Del("U.open.return_code", "/dummy", 0));
-    ASSERT_EQ(0, xattr.Del("U.read.return_code", "/dummy", 0));
-    ASSERT_EQ(0, xattr.Del("U.read.return_position", "/dummy", 0));
-    ASSERT_EQ(0, xattr.Del("U.write.return_code", "/dummy", 0));
-    ASSERT_EQ(0, xattr.Del("U.write.return_position", "/dummy", 0));
-    ASSERT_EQ(0, xattr.Del("U.pattern", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.open.return_code", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.read.return_code", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.read.return_position", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.write.return_code", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.write.return_position", "/dummy", 0));
+    EXPECT_EQ(0, xattr.Del("U.pattern", "/dummy", 0));
 }
 
 TEST_F(XrdOssMirageXAttrFixture, DeletePropertiesResetsTheirValues)
@@ -181,12 +181,12 @@ TEST_F(XrdOssMirageXAttrFixture, DeletePropertiesResetsTheirValues)
 
     auto entry = oss.get_entry_read("/dummy").value();
 
-    ASSERT_EQ(0, entry.open.return_code);
-    ASSERT_EQ(0, entry.read.return_code);
-    ASSERT_EQ(0, entry.read.return_position);
-    ASSERT_EQ(0, entry.write.return_code);
-    ASSERT_EQ(0, entry.write.return_position);
-    ASSERT_EQ("", entry.pattern);
+    EXPECT_EQ(0, entry.open.return_code);
+    EXPECT_EQ(0, entry.read.return_code);
+    EXPECT_EQ(0, entry.read.return_position);
+    EXPECT_EQ(0, entry.write.return_code);
+    EXPECT_EQ(0, entry.write.return_position);
+    EXPECT_EQ("", entry.pattern);
 }
 
 TEST_F(XrdOssMirageXAttrFixture, DeleteInvalidPropertyReturnsINVALError)
