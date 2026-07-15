2806bdd37be1c7008872f89b4a54bfd942adb448
Jyothish Thomas <jyothish.thomas@stfc.ac.uk>
2026-07-15 11:11:22 +0000

    [XrdCeph] implement filename limits to avoid crashes
    allow max pathlimit of 1020, with each path section limit of 255.
    The 255 limit reflects the common limit used by most filesystems, 1020 is used to match a buffer limit in the fstream monitoring plugin

--- src/XrdCeph/XrdCephPosix.cc
+++ src/XrdCeph/XrdCephPosix.cc
@@ -647,6 +647,18 @@ void ceph_posix_set_logfunc(void (*logfunc) (char *, va_list argp)) {
   g_logfunc = logfunc;
 };
 
+int maxSubstringLength(const std::string& path, char delimiter) {
+    std::stringstream ss(path);
+    std::string token;
+    size_t maxLength = 0;
+
+    // Split the string by the specified delimiter
+    while (std::getline(ss, token, delimiter)) {
+        maxLength = std::max(maxLength, token.length());
+    }
+    return maxLength;
+};
+
 static int ceph_posix_internal_truncate(const CephFile &file, unsigned long long size);
 
 /**
@@ -664,7 +676,17 @@ static int ceph_posix_internal_truncate(const CephFile &file, unsigned long long
  * */
 
 int ceph_posix_open(XrdOucEnv* env, const char *pathname, int flags, mode_t mode){
-
+  if (std::strlen(pathname)>1020){
+  logwrapper((char*)"path name too long");
+  return -ENAMETOOLONG;
+  }
+  else{
+  std::string pathstr(pathname);
+  if (maxSubstringLength(pathstr, '/') > 254){
+    logwrapper((char*)"file/subpath name too long");
+    return -ENAMETOOLONG;
+    }
+  }
   CephFileRef fr = getCephFileRef(pathname, env, flags, mode, 0);
 
   struct stat buf;
@@ -1217,7 +1239,7 @@ ssize_t ceph_aio_read(int fd, XrdSfsAio *aiop, AioCB *cb) {
 int ceph_posix_fstat(int fd, struct stat *buf) {
   CephFileRef* fr = getFileRef(fd);
   if (fr) {
-    logwrapper((char*)__FUNCTION__,": fd %d", fd);
+    logwrapper((char*)"%s: fd %d", __FUNCTION__, fd);
     // minimal stat : only size and times are filled
     // atime, mtime and ctime are set all to the same value
     // mode is set arbitrarily to 0666 | S_IFREG
